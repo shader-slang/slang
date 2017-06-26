ccf76b861d3b2e86c7fd40f0baa4a333a1551ca4
Tim Foley <tfoley@nvidia.com>
2017-06-26 09:07:07 -0700

    Include imported code when generating reflection data
    - The basic idea is simple: be sure to enumerate code in `__import`ed modules when generating reflection info
    
    - Note that we don't currently allow an entry point to appear in an imported module, so we only consider globlal-scope parameters
    
    - Although there isn't currently a real implementation of namespacing, I went ahead and ensured that parameters in imported modules are treated as distinct from parameters in the user's code, even if they have the same name.

---
+++ tests/reflection/reflect-imported-code.hlsl
@@ -0,0 +1,21 @@
+//TEST:SIMPLE:-profile ps_4_0  -target reflection-json
+
+// Confirm that shader parameters in imported modules get reflected properly.
+
+__import reflect_imported_code;
+
+Texture2D 		t;
+SamplerState 	s;
+
+cbuffer C
+{
+	float c;
+}
+
+float4 main() : SV_Target
+{
+	return use(t,s_i)
+	     + use(c)
+	     + use(t_i, s)
+	     + use(c_i);
+}
