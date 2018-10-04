757b4ab7a33362aebd47bf375a1ff32edead619b
Jonathan Small <jsmall@nvidia.com>
2018-10-04 14:58:08 -0400

    * More tests and perf improvements for byte encoding. * Mechanism to detect processor and processor features in main slang header.

--- slang.h
+++ slang.h
@@ -163,6 +163,91 @@ Any platforms not detected by the above logic are now now explicitly zeroed out.
 #define SLANG_APPLE_FAMILY (SLANG_IOS || SLANG_OSX)                  /* equivalent to #if __APPLE__ */
 #define SLANG_UNIX_FAMILY (SLANG_LINUX_FAMILY || SLANG_APPLE_FAMILY) /* shortcut for unix/posix platforms */
 
+/* Macros for detecting processor */
+#if defined(_M_ARM) || defined(__ARM_EABI__)
+// This is special case for nVidia tegra
+#   define SLANG_PROCESSOR_ARM 1
+#elif defined(__i386__) || defined(_M_IX86)
+#   define SLANG_PROCESSOR_X86 1
+#elif defined(_M_AMD64) || defined(_M_X64) || defined(__amd64) || defined(__x86_64)
+#   define SLANG_PROCESSOR_X86_64 1
+#elif defined(_PPC_) || defined(__ppc__) || defined(__POWERPC__) || defined(_M_PPC)
+#   if defined(__powerpc64__) || defined(__ppc64__) || defined(__PPC64__) || defined(__64BIT__) || defined(_LP64) || defined(__LP64__)
+#       define SLANG_PROCESSOR_POWER_PC_64 1
+#   else
+#       define SLANG_PROCESSOR_POWER_PC 1
+#   endif
+#elif defined(__arm__)
+#   define SLANG_PROCESSOR_ARM 1
+#elif defined(__aarch64__)
+#   define SLANG_PROCESSOR_ARM_64 1
+#endif 
+
+#ifndef SLANG_PROCESSOR_ARM
+#   define SLANG_PROCESSOR_ARM 0
+#endif
+
+#ifndef SLANG_PROCESSOR_ARM_64
+#   define SLANG_PROCESSOR_ARM_64 0
+#endif
+
+#ifndef SLANG_PROCESSOR_X86
+#   define SLANG_PROCESSOR_X86 0
+#endif
+
+#ifndef SLANG_PROCESSOR_X86_64
+#   define SLANG_PROCESSOR_X86_64 0
+#endif
+
+#ifndef SLANG_PROCESSOR_POWER_PC
+#   define SLANG_PROCESSOR_POWER_PC 0
+#endif
+
+#ifndef SLANG_PROCESSOR_POWER_PC_64
+#   define SLANG_PROCESSOR_POWER_PC_64 0
+#endif
+
+// Processor families
+
+#define SLANG_PROCESSOR_FAMILY_X86 (SLANG_PROCESSOR_X86_64 | SLANG_PROCESSOR_X86)
+#define SLANG_PROCESSOR_FAMILY_ARM (SLANG_PROCESSOR_ARM | SLANG_PROCESSOR_ARM_64)
+#define SLANG_PROCESSOR_FAMILY_POWER_PC (SLANG_PROCESSOR_POWER_PC_64 | SLANG_PROCESSOR_POWER_PC)
+
+#define SLANG_PTR_IS_64 (SLANG_PROCESSOR_ARM_64 | SLANG_PROCESSOR_X86_64 | SLANG_PROCESSOR_POWER_PC_64)
+#define SLANG_PTR_IS_32 (SLANG_PTR_IS_64 ^ 1)
+
+// Processor features
+#if SLANG_PROCESSOR_FAMILY_X86
+#   define SLANG_LITTLE_ENDIAN 1
+#   define SLANG_UNALIGNED_ACCESS 1
+#elif SLANG_PROCESSOR_FAMILY_ARM
+#   if defined(__ARMEB__)
+#       define SLANG_BIG_ENDIAN 1
+#   else
+#       define SLANG_LITTLE_ENDIAN 1
+#   endif
+#elif SLANG_PROCESSOR_FAMILY_POWER_PC
+#       define SLANG_BIG_ENDIAN 1
+#endif
+
+#ifndef SLANG_LITTLE_ENDIAN
+#   define SLANG_LITTLE_ENDIAN 0
+#endif
+
+#ifndef SLANG_BIG_ENDIAN
+#   define SLANG_BIG_ENDIAN 0
+#endif
+
+#ifndef SLANG_UNALIGNED_ACCESS
+#   define SLANG_UNALIGNED_ACCESS 0
+#endif
+
+// One endianess must be set
+#if ((SLANG_BIG_ENDIAN | SLANG_LITTLE_ENDIAN) == 0)
+#   error "Couldn't determine endianess"
+#endif
+
+
 /* Macro for declaring if a method is no throw. Should be set before the return parameter. */
 #ifndef SLANG_NO_THROW
 #   if SLANG_WINDOWS_FAMILY && !defined(SLANG_DISABLE_EXCEPTIONS)
--- source/core/slang-byte-encode-util.h
+++ source/core/slang-byte-encode-util.h
@@ -3,7 +3,6 @@
 
 #include "list.h"
 
-
 namespace Slang {
 
 struct ByteEncodeUtil
@@ -12,6 +11,34 @@ struct ByteEncodeUtil
     {
         kMaxLiteEncodeUInt32 = 5,                   /// One byte for prefix, the remaining 4 bytes hold the value
     };
+
+        /** Find the most significant bit for 8 bits 
+        @param v The value to find most significant bit on 
+        @return The most significant bit, or -1 if no bits are set
+        */
+    SLANG_FORCE_INLINE static int calcMsb8(uint32_t v);
+    
+        /** Find the most significant bit for 32 bits 
+        @param v The value to find most significant bit on 
+        @return The most significant bit, or -1 if no bits are set
+        */
+    SLANG_FORCE_INLINE static int calcMsb32(uint32_t v);
+    
+        /** Calculates the 'most significant' byte ie the highest bytes that is non zero. 
+         Note return value is *undefined* if in is 0.
+         @param in Value - cannot be 0. 
+         @return The byte index of the highest byte that is non zero.
+         */
+    SLANG_FORCE_INLINE static int calcNonZeroMsByte32(uint32_t in);
+
+        /** Calculates the 'most significant' byte ie the highest bytes that is non zero. 
+        @param in Value - cannot be 0. 
+        @return The byte index of the highest byte that is non zero.
+        */
+    SLANG_FORCE_INLINE static int calcMsByte32(uint32_t in);
+
+        /// Calculate the size of encoding bytes
+    static size_t calcEncodeLiteSizeUInt32(const uint32_t* in, size_t num);
 
         /** Encodes a uint32_t as an integer
          @return the number of bytes needed to encode */
@@ -22,7 +49,115 @@ struct ByteEncodeUtil
          @param out Value constructed
         @return number of bytes on in consumed */
     static int decodeLiteUInt32(const uint8_t* in, uint32_t* out);
+
+        /** Encode an array of uint32_t
+        @param in The values to encode
+        @param num The amount of values to encode
+        @param encodeOut The buffer to hold the encoded value. MUST be large enough to hold the encoding
+        @return The size of the encoding in bytes
+        */
+    static size_t encodeLiteUInt32(const uint32_t* in, size_t num, uint8_t* encodeOut);
+
+        /** Encode an array of uint32_t
+        @param encodeIn The encoded values
+        @param numValues The amount of values to be decoded (NOTE! This is the number of valuesOut, not encodeIn)
+        @param valuesOut The buffer to hold the encoded value. MUST be large enough to hold the encoding
+        @return The amount of bytes decoded
+        */
+    static size_t decodeLiteUInt32(const uint8_t* encodeIn, size_t numValues, uint32_t* valuesOut); 
+
+        /// Table that maps 8 bits to it's most significant bit. If 0 returns -1.
+    static const int8_t s_msb8[256];
 };
+
+#if SLANG_VC 
+// Work on ARM and x86/64 on visual studio compiler
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE int ByteEncodeUtil::calcNonZeroMsByte32(uint32_t in)
+{
+    SLANG_ASSERT(in != 0);
+    // Can use intrinsic
+    // https://msdn.microsoft.com/en-us/library/fbxyd7zd.aspx
+    unsigned long index;
+    _BitScanReverse(&index, in);
+    return index >> 3;
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE int ByteEncodeUtil::calcMsByte32(uint32_t in)
+{
+    if (in == 0)
+    {   
+        return -1;
+    }
+    // Can use intrinsic
+    // https://msdn.microsoft.com/en-us/library/fbxyd7zd.aspx
+    unsigned long index;
+    _BitScanReverse(&index, in);
+    return index >> 3;
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcMsb8(uint32_t v)
+{
+    SLANG_ASSERT((v & 0xffffff00) == 0);
+    if (v == 0)
+    {
+        return -1;
+    }
+    unsigned long index;
+    _BitScanReverse(&index, v);
+    return index;
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcMsb32(uint32_t v)
+{
+    if (v == 0)
+    {
+        return -1;
+    }
+    unsigned long index;
+    _BitScanReverse(&index, v);
+    return index;
+}
+
+#else
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcNonZeroMsByte32(uint32_t in)
+{
+    return (in & 0xffff0000) ? 
+        ((in & 0xff000000) ? 3 : 2) :
+        ((in & 0x0000ff00) ? 1 : 0);
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcMsByte32(uint32_t in)
+{
+    return (in & 0xffff0000) ?
+        ((in & 0xff000000) ? 3 : 2) :
+        ((in & 0x0000ff00) ? 1 : 0);
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcMsb8(uint32_t v) 
+{ 
+    SLANG_ASSERT((v & 0xffffff00) == 0); 
+    return s_msb8[v]; 
+}
+
+// ---------------------------------------------------------------------------
+SLANG_FORCE_INLINE /* static */int ByteEncodeUtil::calcMsb32(uint32_t v)
+{
+    return (v & 0xffff0000) ? 
+        ((v & 0xff000000) ? s_msb8[v >> 24] + 24 : s_msb8[v >> 16] + 16) :
+        ((v & 0x0000ff00) ? s_msb8[v >>  8] +  8 : s_msb8[v]);
+}
+
+#endif
+
 
 } // namespace Slang
 
