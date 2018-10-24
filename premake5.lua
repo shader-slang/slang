eb27a5201054ff7a7dc97ca66526da786f4cd071
Jonathan Small <jsmall@nvidia.com>
2018-10-24 16:13:45 -0400

    Re-add location for linux.

--- premake5.lua
+++ premake5.lua
@@ -53,6 +53,8 @@ workspace "slang"
     configurations { "Debug", "Release" }
     platforms { "x86", "x64" }
 
+    filter { "system:linux" }
+        location("build.linux")
     
     -- The output binary directory will be derived from the OS
     -- and configuration options, e.g. `bin/windows-x64/debug/`
