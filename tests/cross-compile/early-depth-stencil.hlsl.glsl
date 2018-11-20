84b06ec3bb683fb6845cac2bf0bde1b91f3eb906
Jonathan Small <jsmall@nvidia.com>
2018-11-20 16:42:41 -0500

    Use cross compilation to test [earlydepthstencil] on glsl.

--- tests/cross-compile/early-depth-stencil.hlsl.glsl
+++ tests/cross-compile/early-depth-stencil.hlsl.glsl
@@ -1,7 +1,4 @@
-result code = 0
-standard error = {
-}
-standard output = {
+//TEST_IGNORE_FILE:
 #version 450
 layout(row_major) uniform;
 layout(row_major) buffer;
@@ -10,13 +7,10 @@ layout(row_major) buffer;
 layout(location = 0)
 out vec4 _S1;
 
-
 #line 5
 layout(early_fragment_tests) in;
 void main()
 {
     _S1 = vec4(float(1), float(0), float(0), float(1));
     return;
-}
-
 }
--- tests/render/early-depth-stencil.hlsl
+++
@@ -1,9 +0,0 @@
-//TEST:SIMPLE: -profile ps_4_0 -entry main -target hlsl
-//TEST:SIMPLE: -profile ps_4_0 -entry main -target glsl
-
-[earlydepthstencil]
-float4 main(): SV_Target
-{
-    return float4(1, 0, 0, 1); 
-}
-
