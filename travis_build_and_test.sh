983746cf0468eeb36366e1d7477a6c7ad91bbbd7
jsmall-nvidia <jsmall@nvidia.com>
2018-12-05 16:44:16 -0500

    Try to make premake choose the correct tooling.

--- travis_build_and_test.sh
+++ travis_build_and_test.sh
@@ -3,7 +3,7 @@
 # Get premake
 wget https://github.com/shader-slang/slang-binaries/blob/master/premake/premake-5.0.0-alpha13/bin/linux-64/premake5?raw=true -O premake5
 chmod u+x premake5
-./premake5 gmake 
+./premake5 gmake --cc=${CC}
 make config=${CONFIGURATION}_x64
 
 # Setup to run release tests
