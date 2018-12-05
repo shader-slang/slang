8be000faf97c05753bf6f0d000aec57a04135c03
jsmall-nvidia <jsmall@nvidia.com>
2018-12-05 16:10:06 -0500

    Remove the build.linux directory - but doing so will overwrite Makefile.

--- travis_build_and_test.sh
+++ travis_build_and_test.sh
@@ -3,10 +3,8 @@
 # Get premake
 wget https://github.com/shader-slang/slang-binaries/blob/master/premake/premake-5.0.0-alpha13/bin/linux-64/premake5?raw=true -O premake5
 chmod u+x premake5
-./premake5 gmake -build-location=build.linux
-cd build.linux        
+./premake5 gmake 
 make config=${CONFIGURATION}_x64
-cd ..
 
 # Setup to run release tests
 source ./travis_test.sh
