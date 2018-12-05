8c8bb452600862c4da8b424af02b26cf819b8400
jsmall-nvidia <jsmall@nvidia.com>
2018-12-05 15:33:14 -0500

    Try moving build out into script.

---
+++ travis_build_and_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get premake
+wget https://github.com/shader-slang/slang-binaries/blob/master/premake/premake-5.0.0-alpha13/bin/linux-64/premake5?raw=true -O premake5
+chmod u+x premake5
+./premake5 gmake -build-location=build.linux
+cd build.linux        
+make config=release_x64
+make config=debug_x64
+cd ..
+
+# Setup to run release tests
+./travis_test.sh release smoke
+
--- travis_test.sh
+++ travis_test.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #!/usr/bin/env bash
 
 CONFIGURATION="${1}"
