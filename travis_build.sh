f4b4bdb5beb12ff909112d141446601ea98d0fa5
Jonathan Small <jsmall@nvidia.com>
2018-12-11 11:55:20 -0500

    Split scripts into travis_build.sh travis_test.sh

--- travis_build.sh
+++ travis_build.sh
@@ -3,9 +3,11 @@
 # Get premake
 wget https://github.com/shader-slang/slang-binaries/blob/master/premake/premake-5.0.0-alpha13/bin/linux-64/premake5?raw=true -O premake5
 chmod u+x premake5
+
+# Create the makefile
 ./premake5 gmake --cc=${CC}
+
+# Build the configuration
 make config=${CONFIGURATION}_x64
 
-# Setup to run release tests
-source ./travis_test.sh
 
