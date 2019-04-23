7a71b86aadda83d16dadf809be137c7551f31464
jsmall-nvidia <jsmall@nvidia.com>
2019-04-23 12:38:40 -0400

    Fix 'unreachable code' stopping compilation on VS2017. (#949)

--- source/slang/object-meta-begin.h
+++ source/slang/object-meta-begin.h
@@ -34,3 +34,10 @@
 
 #define SIMPLE_SYNTAX_CLASS(NAME, BASE) SYNTAX_CLASS(NAME, BASE) END_SYNTAX_CLASS()
 
+// Hack to remove 'warning C4702: unreachable code' on VS2017, blocking compilation
+// Note! This is matched in object-meta-end.h 
+#if _MSC_VER >= 1910
+#pragma warning(push)
+#pragma warning(disable: 4702)
+#endif
+
