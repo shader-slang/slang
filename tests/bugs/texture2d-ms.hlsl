1e20703f28ac3578db469db3786b40525adeb002
Robert Stepinski <rstepinski@nvidia.com>
2019-08-16 01:04:19 -0400

    Fix a typo in core.meta.slang which was causing an assert when compiling shaders that used texture2DMS Load() operations

---
+++ tests/bugs/texture2d-ms.hlsl
@@ -0,0 +1,10 @@
+//TEST:CROSS_COMPILE: -profile lib_6_3 -entry main -stage compute -target spirv-assembly
+
+[[vk::binding(0, 0)]]
+Texture2DMS tex : register(t1);
+
+[numthreads(4, 4, 1)]
+void main(uint3 groupId : SV_GroupID)
+{
+    tex.Load(groupId.xy, 0);
+}
