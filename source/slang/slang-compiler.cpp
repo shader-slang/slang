d7b8365c41e7b3ac72d3a909b7392531f8d8b557
jsmall-nvidia <jsmall@nvidia.com>
2019-09-16 17:19:55 -0400

    Fix problem with adding \n for externa diagnostic - to do it if there isn't a \n at the end. Change the ouput order (put result before) so last value is diagnostic string.

--- source/slang/slang-compiler.cpp
+++ source/slang/slang-compiler.cpp
@@ -789,11 +789,6 @@ namespace Slang
             builder << compilerName << ": ";
         }
 
-        if (diagnostic.size() > 0)
-        {
-            builder.Append(diagnostic);
-        }
-
         if (SLANG_FAILED(res) && res != SLANG_FAIL)
         {
             {
@@ -805,7 +800,14 @@ namespace Slang
             PlatformUtil::appendResult(res, builder);
         }
 
-        builder << "\n";
+        if (diagnostic.size() > 0)
+        {
+            builder.Append(diagnostic);
+            if (!diagnostic.endsWith("\n"))
+            {
+                builder.Append("\n");
+            }
+        }
 
         sink->diagnoseRaw(severity, builder.getUnownedSlice());
     }
