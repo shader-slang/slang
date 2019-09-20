9733d0e75ec2a204229cb225dc20529744cd5b51
jsmall-nvidia <jsmall@nvidia.com>
2019-09-20 16:01:14 -0400

    Improved documentation around cpu-hello-world. Added information about C++/CPU targets to README.md Referenced cpu-target.

--- examples/cpu-hello-world/main.cpp
+++ examples/cpu-hello-world/main.cpp
@@ -5,36 +5,35 @@
 // This file implements an extremely simple example of loading and
 // executing a Slang shader program on the CPU. 
 //
+// More information about generation C++ or CPU code can be found in docs/cpu-target.md
+//
+// NOTE! This test will only run on a system correctly where slang can find a suitable
+// C++ compiler - such as clang/gcc/visual studio
+//
 // The comments in the file will attempt to explain concepts as
 // they are introduced.
 //
 // Of course, in order to use the Slang API, we need to include
 // its header. We have set up the build options for this project
 // so that it is as simple as:
-//
-// NOTE! This test will only run on a system correctly where slang can find a suitable
-// C++ compiler - such as clang/gcc/visual studio
-//
-// If slang cannot find the compiler there is a mechanism to specify the compiler
-// location with the API call 'setDownstreamCompilerPath'.
-//
-// By default Slang will try to use the compiler *closest* to the compiler it
-// was compiled with.
 #include <slang.h>
 
+// Allows use of ComPtr - which we can use to scope any 'com-like' pointers easily
 #include <slang-com-ptr.h>
+// Provides macros for handling SlangResult values easily 
 #include <slang-com-helper.h>
 
+// This includes a useful small function for setting up the prelude (described more further below).
 #include "../../source/core/slang-test-tool-util.h"
-#include "../../source/core/slang-list.h"
 
 // Slang namespace is used for elements support code (like core) which we use here
-// for List<>, ComPtr<> and TestToolUtil
+// for ComPtr<> and TestToolUtil
 using namespace Slang;
 
 // Slang source is converted into C++ code which is compiled by a backend compiler.
 // That process uses a 'prelude' which defines types and functions that are needed
 // for everything else to work.
+// 
 // We include the prelude here, so we can directly use the types as were used by the
 // compiled code. It is not necessary to include the prelude, as long as memory is
 // laid out in the manner that the generated slang code expects. 
@@ -78,7 +77,7 @@ static SlangResult _innerMain(int argc, char** argv)
 
     // We would like to request a CPU code that can be executed directly on the host -
     // which is the 'SLANG_HOST_CALLABLE' target. 
-    // If we wanted a shared library/dll, we could have used SLANG_SHARED_LIBRARY.
+    // If we wanted a just a shared library/dll, we could have used SLANG_SHARED_LIBRARY.
     int targetIndex = spAddCodeGenTarget(slangRequest, SLANG_HOST_CALLABLE);
 
     // A compile request can include one or more "translation units," which more or
