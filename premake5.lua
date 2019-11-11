43229594cd49579789d8c19f46e7ccf06832b000
Tim Foley <tfoley@nvidia.com>
2019-11-11 15:00:54 -0800

    Enable use of pre-built glslang binaries
    This change uses an updated version of the `slang-binaries` submodule that includes pre-built versions of `slang-glslang.dll` and `libslang-glslang.so`, and enables the build of the main Slang project to rely on these binaries instead of building them from source.
    
    An option to the premake build file can be used to generate the appropriate project files for `slang-glslang`, which should enable us to build updated binaries as needed. The default option is to *not* build those projects, so that we can reduce build times in the common case (and on CI).

--- premake5.lua
+++ premake5.lua
@@ -76,9 +76,18 @@ newoption {
    allowed     = { {"cygwin"}, {"mingw"} }
 }
 
+newoption {
+   trigger     = "build-glslang",
+   description = "(Optional) If true glslang and spirv-opt will be built",
+   value       = "bool",
+   default     = "false",
+   allowed     = { { "true", "True"}, { "false", "False" } }
+}
+
 buildLocation = _OPTIONS["build-location"]
 executeBinary = (_OPTIONS["execute-binary"] == "true")
 targetDetail = _OPTIONS["target-detail"]
+buildGlslang = (_OPTIONS["build-glslang"] == "true")
 
 -- Is true when the target is really windows (ie not something on top of windows like cygwin)
 local isTargetWindows = (os.target() == "windows") and not (targetDetail == "mingw" or targetDetail == "cygwin")
@@ -165,6 +174,8 @@ workspace "slang"
     		
     filter { "system:linux" }
         linkoptions{  "-Wl,-rpath,'$$ORIGIN',--no-as-needed", "-ldl"}
+
+    filter { "platforms:x64 or x86"}
         
             
 function dump(o)
@@ -581,7 +592,7 @@ standardProject "slangc"
     uuid "D56CBCEB-1EB5-4CA8-AEC4-48EA35ED61C7"
     kind "ConsoleApp"
     links { "core", "slang" }
-    
+
 --
 -- TODO: Slang's current `Makefile` build does some careful incantations
 -- to make sure that the binaries it generates use a "relative `RPATH`"
@@ -616,6 +627,8 @@ standardProject "slang"
     -- `slang.h` header in this prject, so we do that manually here.
     files { "slang.h" }
 
+    files { "source/core/core.natvis" }
+
     -- The most challenging part of building `slang` is that we need
     -- to invoke the `slang-generate` tool to generate the version
     -- of the Slang standard library that we embed into the compiler.
@@ -625,6 +638,14 @@ standardProject "slang"
     -- the projects here:
     --
     dependson { "slang-generate" }
+
+    -- If we are not building glslang from source, then be
+    -- sure to copy a binary copy over to the output directory
+    if not buildGlslang then
+        postbuildcommands {
+            "{COPY} ../../external/slang-binaries/bin/" .. targetName .. "/*slang-glslang* %{cfg.targetdir}"
+        }
+    end
 
     filter { "system:linux" }
 	-- might be able to do pic(true)
@@ -672,6 +693,8 @@ standardProject "slang"
             --
             buildinputs { "%{cfg.targetdir}/slang-generate" .. executableSuffix }
     end
+
+if buildGlslang then
 
 --
 -- The single most complicated part of our build is our custom version of glslang.
@@ -750,3 +773,5 @@ standardProject "slang-glslang"
 -- * Packaging up binaries
 -- * "Installing" Slang on a user's machine
 --
+
+end
