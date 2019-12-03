9c498cb0541c507f90c4af5b95dfb549db3583e0
jsmall-nvidia <jsmall@nvidia.com>
2019-12-03 13:28:21 -0500

    Fix ArrayView compiling issue on gcc/clang.

--- source/core/slang-array-view.h
+++ source/core/slang-array-view.h
@@ -113,6 +113,9 @@ namespace Slang
 	public:
         typedef ConstArrayView<T> Super;
 
+        using Super::m_buffer;
+        using Super::m_count;
+
         using Super::begin;
         T* begin() { return m_buffer; }
 
