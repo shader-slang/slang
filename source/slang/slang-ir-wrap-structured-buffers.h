fc24aaffac48b070e587530c20426de98fba3f03
Tim Foley <tfoley@nvidia.com>
2020-01-27 11:34:11 -0800

    Fix layout for structured buffers of matrices
    When using row-major layout (via command-line or API option), the following sort of declaration:
    
    ```hlsl
    StructuredBuffer<float4x4> gBuffer;
    
    ... gBuffer[i] ...
    ```
    
    Generates unexpected results when compiled to DXBC via fxc or DXIL via dxc, because the fxc/dxc compilers do not respect the matrix layout mode in this specific case (a structured buffer of matrices). Instead, they always use column-major layout, even if row-major was requested by the user.
    
    A user can work around this behavior by wrapping the matrix in a `struct`:
    
    ```hlsl
    struct Wrapper { float4x4 wrapped; }
    SturcturedBuffer<Wrapper> gBuffer;
    
    ... gBuffer[i].wrapped ...
    ```
    
    This change simply automates that workaround when compiling for an HLSL-based downstream compiler, so that we get the same behavior across all our backends.
    
    The change adds a test case to confirm the behavior across multiple targets, but it turns out we also had a test checked in that confirmed the buggy (or at least surprising) fxc/dxc behavior, so that one had its baselines changed and can work as a regression test for this fix as well.

---
+++ source/slang/slang-ir-wrap-structured-buffers.h
@@ -0,0 +1,15 @@
+// slang-ir-wrap-structured-buffers.h
+#pragma once
+
+namespace Slang
+{
+    struct IRModule;
+
+        /// Wrap structured buffer types of matrices for
+        /// targets that compute incorrect layouts for such
+        /// types.
+        ///
+    void wrapStructuredBuffersOfMatrices(
+        IRModule*                           module);
+}
+
