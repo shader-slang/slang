42c5525b3a980b3f7aed3da77b702d0e1e7e100b
Tim Foley <tfoley@nvidia.com>
2020-03-06 11:37:19 -0800

    Yet more definitions moved into the stdlib
    The only big catch that I ran into with this batch was that I found the `float.getPi()` function was being emitted to the output GLSL even when that function wasn't being used. This seems to have been a latent problem in the earlier PR, but was only surfaced in the tests once a Slang->GLSL test started using another intrinsic that led to the `float : __BuiltinFloatingPointType` witness table being live in the IR.
    
    The fix for the gotcha here was to add a late IR pass that basically empties out all witness tables in the IR, so that functions that are only referenced by witness tables can then be removed as dead code. This pass is something we should *not* apply if/when we start supporting real dynamic dispatch through witness tables, but that is a problem to be solved on another day.
    
    The remaining tricky pieces of this change were:
    
    * Needed to remember to mark functions as target intrinsics on HLSL and/or GLSL as appropriate (hopefully I caught all the cases) so they don't get emitted as source there.
    
    * The `msad4` function in HLSL is very poorly documented, so filling in its definition was tricky. I made my best effort based on how it is described on MSDN, but it is likely that if anybody wants to rely on this function they will need us to vet our results with some tests.

---
+++ source/slang/slang-ir-strip-witness-tables.h
@@ -0,0 +1,10 @@
+// slang-ir-strip-witness-tables.cpp
+#pragma once
+
+namespace Slang
+{
+struct IRModule;
+
+    /// Strip the contents of all witness table instructions from the given IR `module`
+void stripWitnessTables(IRModule* module);
+}
