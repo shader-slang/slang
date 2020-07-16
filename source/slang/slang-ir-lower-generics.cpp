5cf78cd694e595fcb989e82d0c76496fb55b5ccf
Yong He <yonghe@outlook.com>
2020-07-15 18:27:34 -0700

    Run SSA pass to clean up generic temporary variables during lowering.

--- source/slang/slang-ir-lower-generics.cpp
+++ source/slang/slang-ir-lower-generics.cpp
@@ -6,6 +6,8 @@
 #include "slang-ir-lower-generic-call.h"
 #include "slang-ir-lower-generic-var.h"
 #include "slang-ir-witness-table-wrapper.h"
+#include "slang-ir-ssa.h"
+#include "slang-ir-dce.h"
 
 namespace Slang
 {
@@ -16,7 +18,14 @@ namespace Slang
         sharedContext.module = module;
         lowerGenericFunctions(&sharedContext);
         lowerGenericCalls(&sharedContext);
+        // We might have generated new temporary variables during lowering.
+        // An SSA pass can clean up unncessary load/stores.
+        constructSSA(module);
+        eliminateDeadCode(module);
         lowerGenericVar(&sharedContext);
+        // After lowerGenericVar, there could be some unused `undef` values.
+        // We eliminate them in a DCE pass.
+        eliminateDeadCode(module);
         generateWitnessTableWrapperFunctions(&sharedContext);
     }
 } // namespace Slang
