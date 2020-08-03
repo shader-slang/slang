067d19041dbf71e793b264c8c7bae06ddf823526
jsmall-nvidia <jsmall@nvidia.com>
2020-08-03 14:24:35 -0400

    Use SLANG_RESOURCE_EXT_SHAPE_MASK again

--- source/slang/slang-type-system-shared.cpp
+++ source/slang/slang-type-system-shared.cpp
@@ -13,7 +13,7 @@ namespace Slang
 
     TextureFlavor TextureFlavor::create(SlangResourceShape shape, SlangResourceAccess access, int flags)
     {
-        SLANG_ASSERT((flags & ~int(SLANG_TEXTURE_FLAG_MASK)) == 0);
+        SLANG_ASSERT((flags & ~int(SLANG_RESOURCE_EXT_SHAPE_MASK)) == 0);
         TextureFlavor rs;
         rs.flavor = uint16_t(shape | (access << 8) | flags);
         return rs;
