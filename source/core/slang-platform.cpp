ee028ea8e7243e1f1a5fa3cd5871cb4d2b27c8ca
Yong He <yonghe@outlook.com>
2020-09-01 11:09:24 -0700

    More fixes.

--- source/core/slang-platform.cpp
+++ source/core/slang-platform.cpp
@@ -173,10 +173,10 @@ SLANG_COMPILE_TIME_ASSERT(E_OUTOFMEMORY == SLANG_E_OUT_OF_MEMORY);
 	dlclose(handle);
 }
 
-/* static */SharedLibrary::FuncPtr SharedLibrary::findSymbolAddressByName(Handle handle, char const* name)
+/* static */void* SharedLibrary::findSymbolAddressByName(Handle handle, char const* name)
 {
     SLANG_ASSERT(handle);
-	return (FuncPtr)dlsym((void*)handle, name);
+	return dlsym((void*)handle, name);
 }
 
 /* static */void SharedLibrary::appendPlatformFileName(const UnownedStringSlice& name, StringBuilder& dst)
