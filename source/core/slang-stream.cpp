e012ee829c07d9fa32b4bdcf0cdaf8ee12c8eae4
jsmall-nvidia <jsmall@nvidia.com>
2020-10-19 12:13:21 -0400

    Convert tabs to spaces.

--- source/core/slang-stream.cpp
+++ source/core/slang-stream.cpp
@@ -12,12 +12,12 @@ namespace Slang
 FileStream::FileStream(const String& fileName, FileMode fileMode)
 {
     const FileAccess access = (fileMode == FileMode::Open) ? FileAccess::Read : FileAccess::Write;
-	_init(fileName, fileMode, access, FileShare::None);
+    _init(fileName, fileMode, access, FileShare::None);
 }
 
 FileStream::FileStream(const String& fileName, FileMode fileMode, FileAccess access, FileShare share)
 {
-	_init(fileName, fileMode, access, share);
+    _init(fileName, fileMode, access, share);
 }
 
 void FileStream::_init(const String& fileName, FileMode fileMode, FileAccess access, FileShare share)
@@ -30,66 +30,66 @@ void FileStream::_init(const String& fileName, FileMode fileMode, FileAccess acc
     // Default to no access, until stream is fully constructed
     m_fileAccess = FileAccess::None;
 
-	const char* mode = "rt";
-	switch (fileMode)
-	{
-	case FileMode::Create:
-		if (access == FileAccess::Read)
-			throw ArgumentException("Read-only access is incompatible with Create mode.");
-		else if (access == FileAccess::ReadWrite)
-		{
-			mode = "w+b";
-		}
-		else
-		{
-			mode = "wb";
-		}
-		break;
-	case FileMode::Open:
-		if (access == FileAccess::Read)
-		{
-			mode = "rb";
-		}
-		else if (access == FileAccess::ReadWrite)
-		{
-			mode = "r+b";
-		}
-		else
-		{
-			mode = "wb";
-		}
-		break;
-	case FileMode::CreateNew:
-		if (File::exists(fileName))
-		{
-			throw IOException("Failed opening '" + fileName + "', file already exists.");
-		}
-		if (access == FileAccess::Read)
-			throw ArgumentException("Read-only access is incompatible with Create mode.");
-		else if (access == FileAccess::ReadWrite)
-		{
-			mode = "w+b";
-		}
-		else
-		{
-			mode = "wb";
-		}
-		break;
-	case FileMode::Append:
-		if (access == FileAccess::Read)
-			throw ArgumentException("Read-only access is incompatible with Append mode.");
-		else if (access == FileAccess::ReadWrite)
-		{
-			mode = "a+b";
-		}
-		else
-		{
-			mode = "ab";
-		}
-		break;
-	default:
-		break;
-	}
+    const char* mode = "rt";
+    switch (fileMode)
+    {
+    case FileMode::Create:
+        if (access == FileAccess::Read)
+            throw ArgumentException("Read-only access is incompatible with Create mode.");
+        else if (access == FileAccess::ReadWrite)
+        {
+            mode = "w+b";
+        }
+        else
+        {
+            mode = "wb";
+        }
+        break;
+    case FileMode::Open:
+        if (access == FileAccess::Read)
+        {
+            mode = "rb";
+        }
+        else if (access == FileAccess::ReadWrite)
+        {
+            mode = "r+b";
+        }
+        else
+        {
+            mode = "wb";
+        }
+        break;
+    case FileMode::CreateNew:
+        if (File::exists(fileName))
+        {
+            throw IOException("Failed opening '" + fileName + "', file already exists.");
+        }
+        if (access == FileAccess::Read)
+            throw ArgumentException("Read-only access is incompatible with Create mode.");
+        else if (access == FileAccess::ReadWrite)
+        {
+            mode = "w+b";
+        }
+        else
+        {
+            mode = "wb";
+        }
+        break;
+    case FileMode::Append:
+        if (access == FileAccess::Read)
+            throw ArgumentException("Read-only access is incompatible with Append mode.");
+        else if (access == FileAccess::ReadWrite)
+        {
+            mode = "a+b";
+        }
+        else
+        {
+            mode = "ab";
+        }
+        break;
+    default:
+        break;
+    }
 #ifdef _WIN32
 
     // NOTE! This works because we know all the characters in the mode
@@ -108,35 +108,35 @@ void FileStream::_init(const String& fileName, FileMode fileMode, FileAccess acc
    
     int shFlag = _SH_DENYRW;
     switch (share)
-	{
-	case FileShare::None:
-		shFlag = _SH_DENYRW;
-		break;
-	case FileShare::ReadOnly:
-		shFlag = _SH_DENYWR;
-		break;
-	case FileShare::WriteOnly:
-		shFlag = _SH_DENYRD;
-		break;
-	case FileShare::ReadWrite:
-		shFlag = _SH_DENYNO;
-		break;
-	default:
-		throw ArgumentException("Invalid file share mode.");
-		break;
-	}
+    {
+    case FileShare::None:
+        shFlag = _SH_DENYRW;
+        break;
+    case FileShare::ReadOnly:
+        shFlag = _SH_DENYWR;
+        break;
+    case FileShare::WriteOnly:
+        shFlag = _SH_DENYRD;
+        break;
+    case FileShare::ReadWrite:
+        shFlag = _SH_DENYNO;
+        break;
+    default:
+        throw ArgumentException("Invalid file share mode.");
+        break;
+    }
     if (share == FileShare::None)
 #pragma warning(suppress:4996)
         m_handle = _wfopen(fileName.toWString(), wideMode);
     else
-		m_handle = _wfsopen(fileName.toWString(), wideMode, shFlag);
+        m_handle = _wfsopen(fileName.toWString(), wideMode, shFlag);
 #else
-	m_handle = fopen(fileName.getBuffer(), mode);
+    m_handle = fopen(fileName.getBuffer(), mode);
 #endif
-	if (!m_handle)
-	{
-		throw IOException("Cannot open file '" + fileName + "'");
-	}
+    if (!m_handle)
+    {
+        throw IOException("Cannot open file '" + fileName + "'");
+    }
 
     // Just set the access specified
     m_fileAccess = access;
@@ -144,105 +144,105 @@ void FileStream::_init(const String& fileName, FileMode fileMode, FileAccess acc
 
 FileStream::~FileStream()
 {
-	close();
+    close();
 }
 
 Int64 FileStream::getPosition()
 {
 #if defined(_WIN32) || defined(__CYGWIN__)
-	fpos_t pos;
-	fgetpos(m_handle, &pos);
-	return pos;
+    fpos_t pos;
+    fgetpos(m_handle, &pos);
+    return pos;
 #elif defined(__APPLE__)
-	return ftell(m_handle);
+    return ftell(m_handle);
 #else 
-	fpos64_t pos;
-	fgetpos64(m_handle, &pos);
-	return *(Int64*)(&pos);
+    fpos64_t pos;
+    fgetpos64(m_handle, &pos);
+    return *(Int64*)(&pos);
 #endif
 }
 void FileStream::seek(SeekOrigin origin, Int64 offset)
 {
-	int _origin;
-	switch (origin)
-	{
-	case SeekOrigin::Start:
-		_origin = SEEK_SET;
-		m_endReached = false;
-		break;
-	case SeekOrigin::End:
-		_origin = SEEK_END;
+    int _origin;
+    switch (origin)
+    {
+    case SeekOrigin::Start:
+        _origin = SEEK_SET;
+        m_endReached = false;
+        break;
+    case SeekOrigin::End:
+        _origin = SEEK_END;
         // JS TODO: This doesn't seem right, the offset can mean it's not at the end
-		m_endReached = true;
-		break;
-	case SeekOrigin::Current:
-		_origin = SEEK_CUR;
-		m_endReached = false;
-		break;
-	default:
-		throw NotSupportedException("Unsupported seek origin.");
-		break;
-	}
+        m_endReached = true;
+        break;
+    case SeekOrigin::Current:
+        _origin = SEEK_CUR;
+        m_endReached = false;
+        break;
+    default:
+        throw NotSupportedException("Unsupported seek origin.");
+        break;
+    }
 #ifdef _WIN32
-	int rs = _fseeki64(m_handle, offset, _origin);
+    int rs = _fseeki64(m_handle, offset, _origin);
 #else
-	int rs = fseek(m_handle, (int)offset, _origin);
+    int rs = fseek(m_handle, (int)offset, _origin);
 #endif
-	if (rs != 0)
-	{
-		throw IOException("FileStream seek failed.");
-	}
+    if (rs != 0)
+    {
+        throw IOException("FileStream seek failed.");
+    }
 }
 
 size_t FileStream::read(void* buffer, size_t length)
 {
-	auto bytes = fread_s(buffer, length, 1, length, m_handle);
-	if (bytes == 0 && length > 0)
-	{
-		if (!feof(m_handle))
-			throw IOException("FileStream read failed.");
-		else if (m_endReached)
-			throw EndOfStreamException("End of file is reached.");
-		m_endReached = true;
-	}
-	return bytes;
+    auto bytes = fread_s(buffer, length, 1, length, m_handle);
+    if (bytes == 0 && length > 0)
+    {
+        if (!feof(m_handle))
+            throw IOException("FileStream read failed.");
+        else if (m_endReached)
+            throw EndOfStreamException("End of file is reached.");
+        m_endReached = true;
+    }
+    return bytes;
 }
 
 size_t FileStream::write(const void* buffer, size_t length)
 {
-	auto bytes = fwrite(buffer, 1, length, m_handle);
-	if (bytes < length)
-	{
-		throw IOException("FileStream write failed.");
-	}
-	return bytes;
+    auto bytes = fwrite(buffer, 1, length, m_handle);
+    if (bytes < length)
+    {
+        throw IOException("FileStream write failed.");
+    }
+    return bytes;
 }
 
 bool FileStream::canRead()
 {
-	return ((int)m_fileAccess & (int)FileAccess::Read) != 0;
+    return ((int)m_fileAccess & (int)FileAccess::Read) != 0;
 }
 
 bool FileStream::canWrite()
 {
-	return ((int)m_fileAccess & (int)FileAccess::Write) != 0;
+    return ((int)m_fileAccess & (int)FileAccess::Write) != 0;
 }
 
 void FileStream::close()
 {
-	if (m_handle)
-	{
-		fclose(m_handle);
-		m_handle = 0;
+    if (m_handle)
+    {
+        fclose(m_handle);
+        m_handle = 0;
 
         // If closed, can neither read or write
         m_fileAccess = FileAccess::None;
-	}
+    }
 }
 
 bool FileStream::isEnd()
 {
-	return m_endReached;
+    return m_endReached;
 }
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MemoryStreamBase !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
