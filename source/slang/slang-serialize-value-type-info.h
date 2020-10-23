5e105c7c8ee2b34c040a5c3fffd31ec391d0e546
jsmall-nvidia <jsmall@nvidia.com>
2020-10-23 14:20:46 -0400

    Value types with inheritence.

--- source/slang/slang-serialize-value-type-info.h
+++ source/slang/slang-serialize-value-type-info.h
@@ -24,9 +24,14 @@ namespace Slang {
 #define SLANG_VALUE_FIELD_TO_SERIAL(FIELD_NAME, TYPE, param) SerialTypeInfo<SLANG_VALUE_GET_TYPE TYPE>::toSerial(writer, &src->FIELD_NAME, &dst->FIELD_NAME);
 #define SLANG_VALUE_FIELD_TO_NATIVE(FIELD_NAME, TYPE, param) SerialTypeInfo<SLANG_VALUE_GET_TYPE TYPE>::toNative(reader, &src->FIELD_NAME, &dst->FIELD_NAME);
 
+#define SLANG_IF_HAS_SUPER_BASE(x)
+#define SLANG_IF_HAS_SUPER_INNER(x) x
+#define SLANG_IF_HAS_SUPER_LEAF(x) x
+
 #define SLANG_VALUE_TO_SERIAL(NAME, SUPER, ORIGIN, LAST, MARKER, TYPE, param) \
 static void toSerial(SerialWriter* writer, const void* native, void* serial) \
 { \
+    SLANG_IF_HAS_SUPER_##TYPE(SerialTypeInfo<SUPER>::toSerial(writer, native, serial);) \
     auto dst = (SerialType*)serial; \
     auto src = (const NativeType*)native; \
     SLANG_FIELDS_Value_##NAME(SLANG_VALUE_FIELD_TO_SERIAL, param) \
@@ -35,6 +40,7 @@ static void toSerial(SerialWriter* writer, const void* native, void* serial) \
 #define SLANG_VALUE_TO_NATIVE(NAME, SUPER, ORIGIN, LAST, MARKER, TYPE, param) \
 static void toNative(SerialReader* reader, const void* serial, void* native) \
 { \
+    SLANG_IF_HAS_SUPER_##TYPE(SerialTypeInfo<SUPER>::toSerial(reader, serial, native);) \
     auto src = (const SerialType*)serial; \
     auto dst = (NativeType*)native; \
     SLANG_FIELDS_Value_##NAME(SLANG_VALUE_FIELD_TO_NATIVE, param) \
@@ -43,11 +49,10 @@ static void toNative(SerialReader* reader, const void* serial, void* native) \
 #define SLANG_VALUE_SERIAL_FIELD(FIELD_NAME, TYPE, param) SerialTypeInfo<SLANG_VALUE_GET_TYPE TYPE>::SerialType FIELD_NAME;
 
 #define SLANG_VALUE_SERIAL_STRUCT(NAME, SUPER, ORIGIN, LAST, MARKER, TYPE, param) \
-struct SerialType \
+struct SerialType SLANG_IF_HAS_SUPER_##TYPE( : SerialTypeInfo<SUPER>::SerialType) \
 { \
     SLANG_FIELDS_Value_##NAME(SLANG_VALUE_SERIAL_FIELD, param) \
 }; 
-
 
 #define SLANG_VALUE_TYPE_INFO_IMPL(NAME, SUPER, ORIGIN, LAST, MARKER, TYPE, param) \
 template <> \
