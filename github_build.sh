bdc589b0b1360cd06c48f88a117c55e84198ab23
Yong He <yonghe@outlook.com>
2020-11-17 12:03:45 -0800

    Switch CI to github actions. (#1609)
    * Remove travis config files
    
    * change github build script
    
    * skip non-tag build on appveyor

--- github_build.sh
+++ github_build.sh
@@ -6,11 +6,11 @@ chmod u+x premake5
 # generate slang-tag-version.h 
 git describe --tags | sed -e "s/\(.*\)/\#define SLANG_TAG_VERSION \"\1\"/" > slang-tag-version.h
 cat slang-tag-version.h 
- 
+
 # Create the makefile
 ./premake5 gmake --cc=${CC}
 
 # Build the configuration
-make config=${CONFIGURATION}_x64 -j2
+make config=${CONFIGURATION}_x64 -j`nproc`
 
 
