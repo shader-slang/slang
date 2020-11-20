18df582f7edd707d2e6f473c72045e8b4d46aa59
jsmall-nvidia <jsmall@nvidia.com>
2020-11-20 18:01:22 -0500

    Remove warnings needed when miniz was included as a header library.

--- source/core/slang-zip-file-system.cpp
+++ source/core/slang-zip-file-system.cpp
@@ -9,19 +9,6 @@
 #include "slang-string-slice-pool.h"
 #include "slang-uint-set.h"
 
-// These match what is in amalgamate.sh - so we can just
-// use the github repro with the appropriate tag (and submodules)
-
-#ifdef _MSC_VER
-// Disable warning on VS
-// warning C4334: '<<': result of 32-bit shift implicitly converted to 64 bits (was 64-bit shift intended?)
-#   pragma warning(disable:4334)
-// warning C4100: : unreferenced formal parameter
-#   pragma warning(disable:4100)
-// warning C4127: conditional expression is constant
-#   pragma warning(disable:4127)
-#endif
-
 #include "../../external/miniz/miniz.h"
 #include "../../external/miniz/miniz_common.h"
 #include "../../external/miniz/miniz_tdef.h"
