d949f37d8ff8532a5a35b9b11d3f4e484a5da6a4
Yong He <yhe@nvidia.com>
2021-07-09 01:31:14 -0700

    Use dxc from slang-binaries

--- tools/copy-hlsl-libs.bat
+++ tools/copy-hlsl-libs.bat
@@ -7,7 +7,10 @@ set PLATFORM=%~3
 
 robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" d3dcompiler_47.dll /r:0 >nul
 
-robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxcompiler.dll    /xn /r:0 >nul
-robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxil.dll          /xn /r:0 >nul
+robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxcompiler.dll /r:0 >nul
+robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxil.dll /r:0 >nul
+
+robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxcompiler.dll    /xo /r:0 >nul
+robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxil.dll          /xo /r:0 >nul
 
 exit /b 0
