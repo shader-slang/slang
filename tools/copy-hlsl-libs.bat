487108c570cc130f91f60f7c9d2f0611cd41d3a5
Yong He <yhe@nvidia.com>
2021-07-09 03:03:07 -0700

    Fix

--- tools/copy-hlsl-libs.bat
+++ tools/copy-hlsl-libs.bat
@@ -5,10 +5,10 @@ set SOURCE_DIR=%~1
 set TARGET_DIR=%~2
 set PLATFORM=%~3
 
-robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" d3dcompiler_47.dll /r:0 >nul
+robocopy "../../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" d3dcompiler_47.dll /r:0 >nul
 
-robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxcompiler.dll /r:0 >nul
-robocopy "../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxil.dll /r:0 >nul
+robocopy "../../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxcompiler.dll /r:0 >nul
+robocopy "../../../external/slang-binaries/bin/%PLATFORM%" "%TARGET_DIR%" dxil.dll /r:0 >nul
 
 robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxcompiler.dll    /xo /r:0 >nul
 robocopy "%SOURCE_DIR%" "%TARGET_DIR%" dxil.dll          /xo /r:0 >nul
