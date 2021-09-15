40113f6dbd8009001f90b565893395e73d712aa6
Pablo Delgado Krämer <private@pablode.com>
2021-09-15 21:31:11 +0200

    Add missing Windows.h include for InterlockedIncrement

--- source/slang/slang-compiler.cpp
+++ source/slang/slang-compiler.cpp
@@ -37,6 +37,14 @@
 #include <unistd.h>
 #endif
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#undef WIN32_LEAN_AND_MEAN
+#undef NOMINMAX
+#endif
+
 #ifdef _MSC_VER
 #pragma warning(disable: 4996)
 #endif
