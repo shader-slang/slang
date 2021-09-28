87f0554abf9d2efd8d8b7231bcece3d90dcacc4e
Kai-Hwa Yao <kyao@nvidia.com>
2021-09-27 20:38:54 -0700

    Resource barrier API and VK implementation

--- slang-gfx.h
+++ slang-gfx.h
@@ -205,6 +205,7 @@ enum class PrimitiveTopology
 enum class ResourceState
 {
     Undefined,
+    PreInitialized,
     VertexBuffer,
     IndexBuffer,
     ConstantBuffer,
@@ -215,6 +216,7 @@ enum class ResourceState
     DepthRead,
     DepthWrite,
     Present,
+    IndirectArgument,
     CopySource,
     CopyDestination,
     ResolveSource,
@@ -1158,6 +1160,10 @@ public:
         IBufferResource* src,
         size_t srcOffset,
         size_t size) = 0;
+
+    virtual SLANG_NO_THROW void SLANG_MCALL textureBarrier(ITextureResource* texture, ResourceState src, ResourceState dst) = 0;
+    virtual SLANG_NO_THROW void SLANG_MCALL bufferBarrier(IBufferResource* buffer, ResourceState src, ResourceState dst) = 0;
+
     virtual SLANG_NO_THROW void SLANG_MCALL
         uploadBufferData(IBufferResource* dst, size_t offset, size_t size, void* data) = 0;
 };
