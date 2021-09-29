2ab29dd7797e7fdb204a44d1ac06e9a2e5655c87
jsmall-nvidia <jsmall@nvidia.com>
2021-09-29 18:23:55 -0400

    Fix tabbing.

--- source/core/slang-char-encode.h
+++ source/core/slang-char-encode.h
@@ -21,21 +21,21 @@ template <typename ReadByteFunc>
 Char32 getUnicodePointFromUTF8(const ReadByteFunc& get)
 {
     Char32 codePoint = 0;
-	uint32_t leading = Byte(get(0));
+    uint32_t leading = Byte(get(0));
     uint32_t mask = 0x80;
     Index count = 0;
-	while (leading & mask)
-	{
-		count++;
-		mask >>= 1;
-	}
-	codePoint = (leading & (mask - 1));
-	for (Index i = 1; i <= count - 1; i++)
-	{
-		codePoint <<= 6;
-		codePoint += (get(i) & 0x3F);
-	}
-	return codePoint;
+    while (leading & mask)
+    {
+        count++;
+        mask >>= 1;
+    }
+    codePoint = (leading & (mask - 1));
+    for (Index i = 1; i <= count - 1; i++)
+    {
+        codePoint <<= 6;
+        codePoint += (get(i) & 0x3F);
+    }
+    return codePoint;
 }
 
 template <typename ReadByteFunc>
@@ -44,15 +44,15 @@ Char32 getUnicodePointFromUTF16(const ReadByteFunc& get)
     uint32_t byte0 = Byte(get(0));
     uint32_t byte1 = Byte(get(1));
     uint32_t word0 = byte0 + (byte1 << 8);
-	if (word0 >= 0xD800 && word0 <= 0xDFFF)
-	{
+    if (word0 >= 0xD800 && word0 <= 0xDFFF)
+    {
         uint32_t byte2 = Byte(get(2));
         uint32_t byte3 = Byte(get(3));
         uint32_t word1 = byte2 + (byte3 << 8);
-		return Char32(((word0 & 0x3FF) << 10) + (word1 & 0x3FF) + 0x10000);
-	}
-	else
-		return Char32(word0);
+        return Char32(((word0 & 0x3FF) << 10) + (word1 & 0x3FF) + 0x10000);
+    }
+    else
+        return Char32(word0);
 }
 
 template <typename ReadByteFunc>
@@ -61,25 +61,25 @@ Char32 getUnicodePointFromUTF16Reversed(const ReadByteFunc& get)
     uint32_t byte0 = Byte(get(0));
     uint32_t byte1 = Byte(get(1));
     uint32_t word0 = (byte0 << 8) + byte1;
-	if (word0 >= 0xD800 && word0 <= 0xDFFF)
-	{
+    if (word0 >= 0xD800 && word0 <= 0xDFFF)
+    {
         uint32_t byte2 = Byte(get(2));
         uint32_t byte3 = Byte(get(3));
         uint32_t word1 = (byte2 << 8) + byte3;
-		return Char32(((word0 & 0x3FF) << 10) + (word1 & 0x3FF));
-	}
-	else
-		return Char32(word0);
+        return Char32(((word0 & 0x3FF) << 10) + (word1 & 0x3FF));
+    }
+    else
+        return Char32(word0);
 }
 
 template <typename ReadByteFunc>
 Char32 getUnicodePointFromUTF32(const ReadByteFunc& get)
 {
-	uint32_t byte0 = Byte(get(0));
+    uint32_t byte0 = Byte(get(0));
     uint32_t byte1 = Byte(get(1));
     uint32_t byte2 = Byte(get(2));
     uint32_t byte3 = Byte(get(3));
-	return Char32(byte0 + (byte1 << 8) + (byte2 << 16) + (byte3 << 24));
+    return Char32(byte0 + (byte1 << 8) + (byte2 << 16) + (byte3 << 24));
 }
 
 // Encode functions return the amount of elements output to the buffer
@@ -93,27 +93,27 @@ inline int encodeUnicodePointToUTF8(Char32 codePoint, char* outBuffer)
         dst[0] = char(codePoint);
         return 1;
     }
-	else if (codePoint <= 0x7FF)
-	{
+    else if (codePoint <= 0x7FF)
+    {
         dst[0] = char(0xC0 + (codePoint >> 6));
         dst[1] = char(0x80 + (codePoint & 0x3F));
         return 2;
-	}
-	else if (codePoint <= 0xFFFF)
-	{
+    }
+    else if (codePoint <= 0xFFFF)
+    {
         dst[0] = char(0xE0 + (codePoint >> 12));
         dst[1] = char(0x80 + ((codePoint >> 6) & (0x3F)));
         dst[2] = char(0x80 + (codePoint & 0x3F));
         return 3;
-	}
-	else
-	{
-		dst[0] = char(0xF0 + (codePoint >> 18));
+    }
+    else
+    {
+        dst[0] = char(0xF0 + (codePoint >> 18));
         dst[1] = char(0x80 + ((codePoint >> 12) & 0x3F));
         dst[2] = char(0x80 + ((codePoint >> 6) & 0x3F));
         dst[3] = char(0x80 + (codePoint & 0x3F));
         return 4;
-	}
+    }
 }
 
 inline int encodeUnicodePointToUTF16(Char32 codePoint, Char16* outBuffer)
@@ -124,13 +124,13 @@ inline int encodeUnicodePointToUTF16(Char32 codePoint, Char16* outBuffer)
         dst[0] = Char16(codePoint);
         return 1;
     }
-	else
-	{
-		const uint32_t sub = codePoint - 0x10000;
-		dst[0] = Char16((sub >> 10) + 0xD800);
-		dst[1] = Char16((sub & 0x3FF) + 0xDC00);
+    else
+    {
+        const uint32_t sub = codePoint - 0x10000;
+        dst[0] = Char16((sub >> 10) + 0xD800);
+        dst[1] = Char16((sub & 0x3FF) + 0xDC00);
         return 2;
-	}
+    }
 }
 
 SLANG_FORCE_INLINE Char16 reverseByteOrder(Char16 val)
@@ -146,15 +146,15 @@ inline int encodeUnicodePointToUTF16Reversed(Char32 codePoint, Char16* outBuffer
         dst[0] = reverseByteOrder(Char16(codePoint));
         return 1;
     }
-	else
-	{
+    else
+    {
         const uint32_t sub = codePoint - 0x10000;
         const uint32_t high = (sub >> 10) + 0xD800;
         const uint32_t low = (sub & 0x3FF) + 0xDC00;
-		dst[0] = reverseByteOrder(Char16(high));
-		dst[1] = reverseByteOrder(Char16(low));
+        dst[0] = reverseByteOrder(Char16(high));
+        dst[1] = reverseByteOrder(Char16(low));
         return 2;
-	}
+    }
 }
 
 static const Char16 kUTF16Header = 0xFEFF;
@@ -163,14 +163,14 @@ static const Char16 kUTF16ReversedHeader = 0xFFFE;
 class CharEncoding
 {
 public:
-	static CharEncoding* UTF8,* UTF16,* UTF16Reversed,* UTF32;
+    static CharEncoding* UTF8,* UTF16,* UTF16Reversed,* UTF32;
 
         /// Encode Utf8 held in slice append into ioBuffer
-	virtual void encode(const UnownedStringSlice& str, List<Byte>& ioBuffer) = 0;
+    virtual void encode(const UnownedStringSlice& str, List<Byte>& ioBuffer) = 0;
         /// Decode buffer into Utf8 held in ioBuffer
-	virtual void decode(const Byte* buffer, int length, List<char>& ioBuffer) = 0;
+    virtual void decode(const Byte* buffer, int length, List<char>& ioBuffer) = 0;
 
-	virtual ~CharEncoding()	{}
+    virtual ~CharEncoding()    {}
 
         /// Get the encoding type
     CharEncodeType getEncodingType() const { return m_encodingType; }
