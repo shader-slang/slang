b5b77ad5e06cac81aa780e71ba5aa85d67a99cf0
Lucy Chen <lucchen@nvidia.com>
2021-10-15 19:01:32 -0700

    All unit tests added for BC and Srgb formats

--- tools/gfx-unit-test/format-unit-tests.cpp
+++ tools/gfx-unit-test/format-unit-tests.cpp
@@ -116,39 +116,36 @@ namespace gfx_test
 
     void FormatTestsImpl(IDevice* device, UnitTestContext* context)
     {
-        // BC1 Unorm
+        // RGBA_UInt32
         {
-            uint8_t texData[] = { 31, 0, 0, 0, 0, 0, 0, 0, 31, 0, 0, 0, 0, 0, 0, 0,
-                              31, 0, 0, 0, 0, 0, 0, 0, 31, 0, 0, 0, 0, 0, 0, 0,
-                              255, 255, 255, 255, 0, 0, 0, 0 };
-            ITextureResource::SubresourceData subData[] = {
-                ITextureResource::SubresourceData {(void*)texData, 16, 0},
-                ITextureResource::SubresourceData {(void*)(texData + 32), 8, 0}
-            };
-            const int resultCount = 8;
-            float initialData[resultCount] = { 0.0f };
+            uint32_t texData[] = { 244u, 0u, 0u, 244u, 0u, 244u, 0u, 244u, 0u, 0u, 244u, 244u, 244u, 244u, 244u, 244u,
+                               184u, 0u, 0u, 244u, 0u, 184u, 0u, 244u, 0u, 0u, 184u, 244u, 184u, 184u, 184u, 244u,
+                               124u, 0u, 0u, 244u, 0u, 124u, 0u, 244u, 0u, 0u, 124u, 244u, 124u, 124u, 124u, 244u,
+                               64u, 0u, 0u, 244u, 0u, 64u, 0u, 244u, 0u, 0u, 64u, 244u, 64u, 64u, 64u, 244u };
+            ITextureResource::SubresourceData subData = {};
+            subData.data = (void*)texData;
+            subData.strideY = 4 * 4 * sizeof(uint32_t);
+            const int resultCount = 64;
+            uint32_t initialData[resultCount] = { 0u };
 
-            ITextureResource::Size size = {};
-            size.width = 8;
-            size.height = 8;
-            size.depth = 1;
+            ITextureResource::Size size = { 4, 4, 1 };
 
-            auto texView = createTexView(device, size, gfx::Format::BC1_Unorm, subData, 2);
-            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto texView = createTexView(device, size, gfx::Format::RGBA_UInt32, &subData);
+            auto outBuffer = createBuffer<uint32_t>(device, resultCount, initialData);
             auto bufferView = createBufferView(device, outBuffer);
 
-            ISamplerState::Desc samplerDesc;
-            auto sampler = device->createSamplerState(samplerDesc);
-
-            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            setUpAndRunTest(device, texView, bufferView, "copyTexUint");
 
             compareComputeResult(
                 device,
                 outBuffer,
-                Slang::makeArray<float>(0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f));
+                Slang::makeArray<uint32_t>(244u, 0u, 0u, 244u, 0u, 244u, 0u, 244u, 0u, 0u, 244u, 244u, 244u, 244u, 244u, 244u,
+                                           184u, 0u, 0u, 244u, 0u, 184u, 0u, 244u, 0u, 0u, 184u, 244u, 184u, 184u, 184u, 244u,
+                                           124u, 0u, 0u, 244u, 0u, 124u, 0u, 244u, 0u, 0u, 124u, 244u, 124u, 124u, 124u, 244u,
+                                           64u, 0u, 0u, 244u, 0u, 64u, 0u, 244u, 0u, 0u, 64u, 244u, 64u, 64u, 64u, 244u));
         }
-        
-        // RGBA8 Unorm Srgb
+
+        // RGBA_Unorm_UInt8_Srgb
         {
             uint8_t texData[] = { 0, 0, 0, 255, 127, 127, 127, 255, 255, 255, 255, 255, 0, 0, 0, 0 };
             ITextureResource::SubresourceData subData = {};
@@ -157,10 +154,7 @@ namespace gfx_test
             const int resultCount = 16;
             float initialData[resultCount] = { 0.0f };
 
-            ITextureResource::Size size = {};
-            size.width = 2;
-            size.height = 2;
-            size.depth = 1;
+            ITextureResource::Size size = { 2, 2, 1 };
 
             auto texView = createTexView(device, size, gfx::Format::RGBA_Unorm_UInt8_Srgb, &subData);
             auto outBuffer = createBuffer<float>(device, resultCount, initialData);
@@ -172,7 +166,232 @@ namespace gfx_test
                 device,
                 outBuffer,
                 Slang::makeArray<float>(0.0f, 0.0f, 0.0f, 1.0f, 0.211914062f, 0.211914062f, 0.211914062f,
-                                        1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f));
+                    1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f));
+        }
+
+        // BC1_Unorm, BC1_Unorm_Srgb - These tests also check that mipmaps are working correctly for compressed formats.
+        {
+            uint8_t texData[] = { 16, 0, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0,
+                                  16, 0, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0,
+                                  255, 255, 255, 255, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData[] = {
+                ITextureResource::SubresourceData {(void*)texData, 16, 0},
+                ITextureResource::SubresourceData {(void*)(texData + 32), 8, 0}
+            };
+            ITextureResource::Size size = { 8, 8, 1 };
+            const int resultCount = 8;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC1_Unorm, subData, 2);
+            setUpAndRunTest(device, texView, bufferView, "sampleMipsFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.517647088f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC1_Unorm_Srgb, subData, 2);
+            setUpAndRunTest(device, texView, bufferView, "sampleMipsFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.230468750f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f));
+        }
+
+        // BC2_Unorm, BC2_Unorm_Srgb
+        {
+            uint8_t texData[] = { 255, 255, 255, 255, 255, 255, 255, 255,
+                                  16, 0, 0, 0, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC2_Unorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.517647088f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC2_Unorm_Srgb, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.230468750f, 1.0f));
+        }
+
+        // BC3_Unorm, BC3_Unorm_Srgb
+        {
+            uint8_t texData[] = { 0, 255, 255, 255, 255, 255, 255, 255,
+                                  16, 0, 0, 0, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC3_Unorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.517647088f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC3_Unorm_Srgb, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0f, 0.230468750f, 1.0f));
+        }
+
+        // BC4_Unorm, BC4_Snorm
+        {
+            uint8_t texData[] = { 127, 0, 0, 0, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData = { texData, 8, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC4_Unorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.498039216f, 0.0f, 0.0f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC4_Snorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(1.0f, 0.0f, 0.0f, 1.0f));
+        }
+
+        // BC5_Unorm, BC5_Snorm
+        {
+            uint8_t texData[] = { 127, 0, 0, 0, 0, 0, 0, 0, 127, 0, 0, 0, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 8;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC5_Unorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.498039216f, 0.498039216f, 0.0f, 1.0f, 0.498039216f, 0.498039216f, 0.0f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC5_Snorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f));
+        }
+
+        // BC6_Unsigned
+        {
+            uint8_t texData[] = { 98, 238, 232, 77, 240, 66, 148, 31, 124, 95, 2, 224, 255, 107, 77, 250 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC6_Unsigned, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.336669922f, 0.911132812f, 2.13867188f, 1.0f));
+        }
+
+        // BC6_Signed
+        {
+            uint8_t texData[] = { 107, 238, 232, 77, 240, 71, 128, 127, 1, 0, 255, 255, 170, 218, 221, 254 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC6_Signed, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.336914062f, 0.910644531f, 2.14062500f, 1.0f));
+        }
+
+        // BC7_Unorm, BC7_Unorm_Srgb
+        {
+            uint8_t texData[] = { 104, 0, 0, 0, 64, 163, 209, 104, 0, 0, 0, 0, 0, 0, 0, 0 };
+            ITextureResource::SubresourceData subData = { texData, 16, 0 };
+            ITextureResource::Size size = { 4, 4, 1 };
+            const int resultCount = 4;
+            float initialData[resultCount] = { 0.0f };
+
+            auto outBuffer = createBuffer<float>(device, resultCount, initialData);
+            auto bufferView = createBufferView(device, outBuffer);
+
+            ISamplerState::Desc samplerDesc;
+            auto sampler = device->createSamplerState(samplerDesc);
+
+            auto texView = createTexView(device, size, gfx::Format::BC7_Unorm, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.101960786f, 0.0f, 1.0f));
+
+            texView = createTexView(device, size, gfx::Format::BC7_Unorm_Srgb, &subData);
+            setUpAndRunTest(device, texView, bufferView, "sampleTexFloat", sampler);
+            compareComputeResult(
+                device,
+                outBuffer,
+                Slang::makeArray<float>(0.0f, 0.0103149414f, 0.0f, 1.0f));
         }
     }
 
