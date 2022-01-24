61c149a374583a5c717d9f6bcb5975799bb25868
dsiher <david.siher@gmail.com>
2022-01-24 13:14:27 -0500

    Added project "heterogeneous-first-gen" to address first time build issues

--- premake5.lua
+++ premake5.lua
@@ -670,19 +670,26 @@ newoption {
  
  example "cpu-hello-world"
      kind "ConsoleApp"
- 
-example "heterogeneous-hello-world"
-    kind "ConsoleApp"
-    -- Additionally add slangc for compiling shader.cpp
-    links { "example-base", "slang", "gfx", "gfx-util", "slangc", "platform", "core" }
-    -- Generate shader.cpp from shader.slang
-    prebuildmessage "Generating shader.cpp in %{wks.location:lower()}/examples/heterogeneous-hello-world/"
-    prebuildcommands {
+
+ project "heterogeneous-first-gen"
+     kind "Utility"
+     links "slangc"
+     prebuildcommands {
         "\"%{wks.location:lower()}/bin/" .. targetName .. "/%{cfg.buildcfg:lower()}/slangc\"  \"%{wks.location:lower()}/examples/heterogeneous-hello-world/shader.slang\" -o \"%{wks.location:lower()}/examples/heterogeneous-hello-world/shader.cpp\" -heterogeneous -target cpp -target hlsl"
-    }
-    files {
-        "examples/heterogeneous-hello-world/shader.cpp"
-    }
+     }
+ 
+ example "heterogeneous-hello-world"
+     kind "ConsoleApp"
+     -- Additionally add slangc for compiling shader.cpp
+     links { "example-base", "slang", "gfx", "gfx-util", "slangc", "platform", "core", "heterogeneous-first-gen" }
+     -- Generate shader.cpp from shader.slang
+     prebuildmessage "Generating shader.cpp in %{wks.location:lower()}/examples/heterogeneous-hello-world/"
+     prebuildcommands {
+         "\"%{wks.location:lower()}/bin/" .. targetName .. "/%{cfg.buildcfg:lower()}/slangc\"  \"%{wks.location:lower()}/examples/heterogeneous-hello-world/shader.slang\" -o \"%{wks.location:lower()}/examples/heterogeneous-hello-world/shader.cpp\" -heterogeneous -target cpp -target hlsl"
+     }
+     files {
+         "examples/heterogeneous-hello-world/shader.cpp"
+     }
 
  -- Most of the other projects have more interesting configuration going
  -- on, so let's walk through them in order of increasing complexity.
