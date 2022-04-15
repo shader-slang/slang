245a5b1901bcb33fb6b56c922391e4f176bc31c3
Theresa Foley <tfoley@nvidia.com>
2022-04-15 15:21:14 -0700

    Add forwarding headers
    This change adds new `slang.h` and `slang-gfx.h` headers at the root
    of the project repository, which forward to the original headers that
    got moved under `include/slang/`.

---
+++ slang-gfx.h
@@ -0,0 +1,7 @@
+/* slang-gfx.h
+*
+* This file is a placeholder to support applications that expect the `slang-gfx.h`
+* header to be at the top level of the Slang source repository. The actual
+* definitions of the GFX API are in `include/slang/slang-gfx.h`
+*/
+#include "./include/slang/slang-gfx.h"
---
+++ slang.h
@@ -0,0 +1,7 @@
+/* slang.h
+*
+* This file is a placeholder to support applications that expect the `slang.h`
+* header to be at the top level of the Slang source repository. The actual
+* definitions of the Slang API are in `include/slang/slang.h`
+*/
+#include "./include/slang/slang.h"
