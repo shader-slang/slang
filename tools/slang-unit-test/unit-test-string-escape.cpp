2a65c2e417fa1f8456f84b5de63e89a67667d124
jsmall-nvidia <jsmall@nvidia.com>
2022-05-27 11:54:27 -0400

    Added decode test.

--- tools/slang-unit-test/unit-test-string-escape.cpp
+++ tools/slang-unit-test/unit-test-string-escape.cpp
@@ -17,6 +17,19 @@ static bool _checkConversion(StringEscapeHandler* handler, const UnownedStringSl
 	return decode == check;
 }
 
+static bool _checkDecode(const UnownedStringSlice& encoded, const UnownedStringSlice& decoded)
+{
+	auto handler = StringEscapeUtil::getHandler(StringEscapeUtil::Style::Cpp);
+
+	StringBuilder buf;
+	StringEscapeUtil::appendUnquoted(handler, encoded, buf);
+	return buf == decoded;
+}
+
+#define SLANG_ENCODED_DECODED(x) \
+		const auto encoded = toSlice(#x); \
+		const auto decoded = toSlice(x); 
+
 SLANG_UNIT_TEST(StringEscape)
 {
 	// Check greedy hex digits
@@ -40,6 +53,27 @@ SLANG_UNIT_TEST(StringEscape)
 		auto handler = StringEscapeUtil::getHandler(StringEscapeUtil::Style::Cpp);
 
 		SLANG_CHECK(_checkConversion(handler, toSlice("\0\1\2""2")));
+	}
+
+	{
+		auto handler = StringEscapeUtil::getHandler(StringEscapeUtil::Style::Cpp);
+
+		// We can't just use '\uxxxx', because it has to be translatable into an output character in MSVC (not into utf8)
+		// Can make work perhaps with something like
+		// #pragma execution_character_set("utf-8") 
+		// But for now we don't worry
+		// 
+		// Visual Studio does not appear to support '\U' by default, presumably because wchar_t is 16 bits 
+
+		{
+			SLANG_ENCODED_DECODED("\a\b\0hey~\u0023\n\0");
+			SLANG_CHECK(_checkDecode(encoded, decoded));
+		}
+
+		{
+			SLANG_ENCODED_DECODED("\n\v\b\t\1\02\003\x5z\x00007f\0");
+			SLANG_CHECK(_checkDecode(encoded, decoded));
+		}
 	}
 }
 
