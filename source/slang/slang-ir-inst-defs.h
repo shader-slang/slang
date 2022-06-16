d2a467c7a941c4453b3d825c9d5bb4d72230c8ba
Sai Praveen Bangaru <31557731+saipraveenb25@users.noreply.github.com>
2022-06-16 11:03:59 -0400

    Added a decorator to mark functions for forward-mode differentiation (#2283)

--- source/slang/slang-ir-inst-defs.h
+++ source/slang/slang-ir-inst-defs.h
@@ -661,6 +661,9 @@ INST(HighLevelDeclDecoration,               highLevelDecl,          1, 0)
 
     INST(SPIRVOpDecoration, spirvOpDecoration, 1, 0)
 
+        /// Decorated function is marked for the forward-mode differentiation pass.
+    INST(JVPDerivativeDecoration, differentiateJvp, 0, 0)
+
         /// Marks a struct type as being used as a structured buffer block.
         /// Recognized by SPIRV-emit pass so we can emit a SPIRV `BufferBlock` decoration.
     INST(SPIRVBufferBlockDecoration, spvBufferBlock, 0, 0)
