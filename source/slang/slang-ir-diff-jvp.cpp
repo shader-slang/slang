f7698b9f0e60a30819eca72179cf83afefcabba6
Sai Bangaru <31557731+saipraveenb25@users.noreply.github.com>
2022-06-24 11:49:18 -0400

    Added JVPTranscriber to handle differentiation of load, store, var, param and return instructions, as well as conversion of data and function types

--- source/slang/slang-ir-diff-jvp.cpp
+++ source/slang/slang-ir-diff-jvp.cpp
@@ -3,9 +3,219 @@
 
 #include "slang-ir.h"
 #include "slang-ir-insts.h"
+#include "slang-ir-clone.h"
 
 namespace Slang
 {
+
+struct JVPTranscriber
+{
+
+    // Stores the mapping of arbitrary 'R-value' instructions to instructions that represent
+    // their differential values.
+    Dictionary<IRInst*, IRInst*> instMapD;
+
+    // Cloning environment to hold mapping from old to new copies for the primal
+    // instructions.
+    IRCloneEnv cloneEnv;
+
+    void mapDifferentialInst(IRInst* instP, IRInst* instD)
+    {
+        instMapD.Add(instP, instD);
+    }
+
+    IRInst* getDifferentialInst(IRInst* instP)
+    {
+        return instMapD[instP];
+    }
+
+    IRFuncType* differentiateFunctionType(IRBuilder* builder, IRFuncType* funcType)
+    {
+        List<IRType*> parameterTypesD;
+        IRType* returnTypeD;
+
+        // Add all primal parameters to the list.
+        for (UIndex i = 0; i < funcType->getParamCount(); i++)
+        {   
+            parameterTypesD.add(funcType->getParamType(i));
+        }
+
+        // Add differential versions for the types we support.
+        for (UIndex i = 0; i < funcType->getParamCount(); i++)
+        {   
+            if (auto typeD = differentiateType(builder, funcType->getParamType(i)))
+                parameterTypesD.add(typeD);
+        }
+
+        // Transcribe return type. 
+        // This will be void if the primal return type is non-differentiable.
+        //
+        returnTypeD = differentiateType(builder, funcType->getResultType());
+        if (!returnTypeD)
+            returnTypeD = builder->getVoidType();
+
+        return builder->getFuncType(parameterTypesD, returnTypeD);
+    }
+
+    IRType* differentiateType(IRBuilder* builder, IRType* typeP)
+    {
+        switch (typeP->getOp())
+        {
+            case kIROp_HalfType:
+            case kIROp_FloatType:
+            case kIROp_DoubleType:
+                return builder->getType(typeP->getOp());
+            
+            default:
+                return nullptr;
+        }
+    }
+    
+    IRInst* differentiateParam(IRBuilder* builder, IRParam* paramP)
+    {
+        if (IRType* typeD = differentiateType(builder, paramP->getFullType()))
+        {
+            IRParam* paramD = builder->emitParam(typeD);
+            SLANG_ASSERT(paramD);
+            return paramD;
+        }
+        return nullptr;
+    }
+
+    IRInst* differentiateVar(IRBuilder* builder, IRVar* varP)
+    {
+        if (IRType* typeD = differentiateType(builder, varP->getDataType()->getValueType()))
+        {
+            IRVar* varD = builder->emitVar(typeD);
+            SLANG_ASSERT(varD);
+            return varD;
+        }
+        return nullptr;
+    }
+
+    IRInst* differentiateLoad(IRBuilder* builder, IRLoad* loadP)
+    {
+        if (auto varP = as<IRVar>(loadP->getPtr()))
+        {   
+            // If the loaded parameter has a differential version, 
+            // emit a load instruction for the differential parameter.
+            // Otherwise, emit nothing since there's nothing to load.
+            // 
+            if (auto varD = as<IRVar>(getDifferentialInst(varP)))
+            {
+                IRLoad* loadD = as<IRLoad>(builder->emitLoad(varD));
+                SLANG_ASSERT(loadD);
+                return loadD;
+            }
+            return nullptr;
+        }
+
+        SLANG_UNEXPECTED("Attempting to differentiate an unsupported load instruction");
+    }
+
+    IRInst* differentiateStore(IRBuilder* builder, IRStore* storeP)
+    {
+        IRInst* storeLocation = storeP->getPtr();
+        IRInst* storeVal = storeP->getVal();
+        if (auto destParam = as<IRVar>(storeLocation))
+        {   
+            // If the stored value has a differential version, 
+            // emit a store instruction for the differential parameter.
+            // Otherwise, emit nothing since there's nothing to load.
+            // 
+            IRInst* storeValD = getDifferentialInst(storeVal);
+            IRVar*  storeLocationD = as<IRVar>(getDifferentialInst(destParam));
+            if (storeValD && storeLocationD)
+            {
+                IRStore* storeD = as<IRStore>(
+                    builder->emitStore(storeLocationD, storeValD));
+                SLANG_ASSERT(storeD);
+                return storeD;
+            }
+            return nullptr;
+        }
+        
+        SLANG_UNEXPECTED("Attempting to differentiate an unsupported store instruction");
+    }
+
+    IRInst* differentiateReturn(IRBuilder* builder, IRReturn* returnP)
+    {
+        IRInst* returnVal = returnP->getVal();
+        if (auto returnValD = getDifferentialInst(returnVal))
+        {   
+            IRReturn* returnD = as<IRReturn>(builder->emitReturn(returnValD));
+            SLANG_ASSERT(returnD);
+            return returnD;
+        }
+        return nullptr;
+    }
+
+    // Logic for whether a primal instruction needs to be replicated
+    // in the differential function. For puerly functional blocks with
+    // no side-effects, it's safe to replicate everything except the
+    // return instruction.
+    //
+    bool requiresPrimalClone(IRBuilder* builder, IRInst* instP)
+    {
+        if (as<IRReturn>(instP))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    IRInst* transcribe(IRBuilder* builder, IRInst* oldInstP)
+    {
+        IRInst* instP = oldInstP;
+
+        // Clone the old instruction, but only if it's safe to do so.
+        // For instance, instructions that handle control flow 
+        // (return statements) shouldn't be replicated.
+        //
+        if (requiresPrimalClone(builder, oldInstP))
+            instP = cloneInst(&cloneEnv, builder, oldInstP);
+        SLANG_ASSERT(instP);
+
+        IRInst* instD = differentiateInst(builder, instP);
+
+        mapDifferentialInst(instP, instD);
+
+        return instD;
+    }
+
+    IRInst* differentiateInst(IRBuilder* builder, IRInst* instP)
+    {
+        switch (instP->getOp())
+        {
+        case kIROp_Param:
+            return differentiateParam(builder, as<IRParam>(instP));
+            break;
+
+        case kIROp_Var:
+            return differentiateVar(builder, as<IRVar>(instP));
+            break;
+
+        case kIROp_Load:
+            return differentiateLoad(builder, as<IRLoad>(instP));
+            break;
+
+        case kIROp_Store:
+            return differentiateStore(builder, as<IRStore>(instP));
+            break;
+
+        case kIROp_Return:
+            return differentiateReturn(builder, as<IRReturn>(instP));
+            break;
+
+        default:
+            SLANG_UNREACHABLE("Attempting to differentiate unrecognized instruction");
+            break;
+        }
+    }
+};
 
 struct JVPDerivativeContext
 {
@@ -16,7 +226,12 @@ struct JVPDerivativeContext
     IRModule*                       module;
 
     // Shared builder state for our derivative passes.
-    SharedIRBuilder                sharedBuilderStorage;
+    SharedIRBuilder                 sharedBuilderStorage;
+
+    // A transcriber object that handles the main job of 
+    // processing instructions while maintaining state.
+    //
+    JVPTranscriber                  transcriberStorage;
 
     bool processModule()
     {
@@ -31,6 +246,7 @@ struct JVPDerivativeContext
         // looking for callables.
         // Note: We're only processing global callables (IRGlobalValueWithCode)
         // for now.
+        // 
         IRBuilder builderStorage(sharedBuilderStorage);
         IRBuilder* builder = &builderStorage;
         for (auto inst : module->getGlobalInsts())
@@ -43,6 +259,8 @@ struct JVPDerivativeContext
                     SLANG_ASSERT(as<IRFunc>(callable));
                     IRFunc* jvpFunction = emitJVPFunction(&builderStorage, as<IRFunc>(callable));
                     builder->addJVPDerivativeReferenceDecoration(callable, jvpFunction);
+
+                    unmarkForJVP(callable);
                 }
             }
         }
@@ -62,10 +280,25 @@ struct JVPDerivativeContext
             {
                 return true;
             }
-            // TODO: Need to remove this decoration or check for 
-            // JVPDerivativeReferenceDecoration to avoid re-generating code.
         }
         return false;
+    }
+
+    // Removes the JVPDerivativeMarkerDecoration from the provided callable, 
+    // if it exists.
+    //
+    void unmarkForJVP(IRGlobalValueWithCode* callable)
+    {
+        for(auto decoration = callable->getFirstDecoration(); 
+            decoration;
+            decoration = decoration->getNextDecoration())
+        {
+            if (decoration->getOp() == kIROp_JVPDerivativeMarkerDecoration)
+            {
+                decoration->removeAndDeallocate();
+                return;
+            }
+        }
     }
 
     List<IRParam*> emitFuncParameters(IRBuilder* builder, IRFuncType* dataType)
@@ -81,15 +314,21 @@ struct JVPDerivativeContext
 
     // Perform forward-mode automatic differentiation on 
     // the intstructions.
+    //
     IRFunc* emitJVPFunction(IRBuilder* builder,
                             IRFunc*    primalFn)
     {
-        // Note (sai): Is this safe? Should we use setInsertInto?
+        
         builder->setInsertBefore(primalFn->getNextInst()); 
 
         auto jvpFn = builder->createFunc();
-        IRType* jvpFuncType = primalTypeToJVPType(primalFn->getFullType());
+        
+        SLANG_ASSERT(as<IRFuncType>(primalFn->getFullType()));
+        IRType* jvpFuncType = transcriberStorage.differentiateFunctionType(
+            builder,
+            as<IRFuncType>(primalFn->getFullType()));
         jvpFn->setFullType(jvpFuncType);
+
         if (auto jvpName = getJVPFuncName(builder, primalFn))
             builder->addNameHintDecoration(jvpFn, jvpName);
 
@@ -100,13 +339,7 @@ struct JVPDerivativeContext
         
         for (auto block = primalFn->getFirstBlock(); block; block = block->getNextBlock())
         {
-            IRBlock* newJVPBlock = nullptr;
-            if (block == primalFn->getFirstBlock())
-            {
-                newJVPBlock = builder->emitBlock();
-                emitFuncParameters(builder, as<IRFuncType>(jvpFuncType));
-            }
-            newJVPBlock = emitJVPBlock(builder, primalFn->getFirstBlock(), newJVPBlock);
+            emitJVPBlock(builder, primalFn->getFirstBlock());
         }
 
         return jvpFn;
@@ -138,31 +371,22 @@ struct JVPDerivativeContext
                           IRBlock*      primalBlock,
                           IRBlock*      jvpBlock = nullptr)
     {   
-        // Create if not already provided, and insert into new block.
+        // Create if not already created, and then insert into new block.
         if (!jvpBlock)
             jvpBlock = builder->emitBlock();
         else
             builder->setInsertInto(jvpBlock);
 
-        // Temporarily, we're going to just emit a single return 0 instruction.
+        // Run through every instruction and use the transcriber to generate the appropriate
+        // derivative code.
         for(auto child = primalBlock->getFirstInst(); child; child = child->getNextInst())
         {
-            if (auto returnOp = as<IRReturn>(child))
-            {
-                auto zeroVal = builder->getFloatValue(returnOp->getVal()->getDataType(), 0.0);
-                builder->emitReturn(zeroVal);
-            }
+            transcriberStorage.transcribe(builder, child);
         }
 
         return jvpBlock;
     }
 
-    IRType* primalTypeToJVPType(IRType* primalType)
-    {
-        // Temporarily, we're going to implement the identity transform.
-        // The return type is the same as the primal type.
-        return primalType;
-    }
 };
 
 // Set up context and call main process method.
