34387a85a294dae6fcceb0f08be3383006bb6a40
Lucy Chen <lucchen@nvidia.com>
2022-07-22 16:44:25 -0700

    render-cpu split, does not compile

---
+++ tools/gfx/cpu/cpu-base.h
@@ -0,0 +1,43 @@
+// cpu-base.h
+// Shared header file for CPU implementation
+#pragma once
+
+#include <chrono>
+
+#include "slang.h"
+#include "slang-com-ptr.h"
+#include "slang-com-helper.h"
+#include "core/slang-basic.h"
+#include "core/slang-blob.h"
+
+#include "../immediate-renderer-base.h"
+#include "../renderer-shared.h"
+#include "../slang-context.h"
+#include "../mutable-shader-object.h"
+#define SLANG_PRELUDE_NAMESPACE slang_prelude
+#include "prelude/slang-cpp-types.h"
+
+namespace gfx
+{
+using namespace Slang;
+
+namespace cpu
+{
+    class BufferResourceImpl;
+    class TextureResourceImpl;
+    class ResourceViewImpl;
+    class BufferResourceViewImpl;
+    class TextureResourceViewImpl;
+    class ShaderObjectLayoutImpl;
+    class EntryPointLayoutImpl;
+    class RootShaderObjectLayoutImpl;
+    class ShaderObjectImpl;
+    class MutableShaderObjectImpl;
+    class EntryPointShaderObjectImpl;
+    class RootShaderObjectImpl;
+    class ShaderProgramImpl;
+    class PipelineStateImpl;
+    class QueryPoolImpl;
+    class DeviceImpl;
+} // namespace cpu
+} // namespace gfx
---
+++ tools/gfx/cpu/cpu-pipeline-state.cpp
@@ -0,0 +1,25 @@
+// cpu-pipeline-state.cpp
+#include "cpu-pipeline-state.h"
+
+namespace gfx
+{
+using namespace Slang;
+
+namespace cpu
+{
+
+    ShaderProgramImpl* PipelineStateImpl::getProgram()
+    {
+        return static_cast<ShaderProgramImpl*>(m_program.Ptr());
+    }
+
+    void PipelineStateImpl::init(const ComputePipelineStateDesc& inDesc)
+    {
+        PipelineStateDesc pipelineDesc;
+        pipelineDesc.type = PipelineType::Compute;
+        pipelineDesc.compute = inDesc;
+        initializeBase(pipelineDesc);
+    }
+
+} // namespace cpu
+} // namespace gfx
