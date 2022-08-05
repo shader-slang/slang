7127cbca5855c935d99c0ec4a39e27d89c7b6328
jsmall-nvidia <jsmall@nvidia.com>
2022-08-05 11:10:16 -0400

    Make ISlangSharedLibrary derive from ICastable, and remain backwards compatible with slang-llvm.

--- source/core/slang-shared-library.cpp
+++ source/core/slang-shared-library.cpp
@@ -62,7 +62,7 @@ SlangResult DefaultSharedLibraryLoader::loadPlatformSharedLibrary(const char* pa
     }
 }
 
-/* !!!!!!!!!!!!!!!!!!!!!!!!!! DefaultSharedLibrary !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/* !!!!!!!!!!!!!!!!!!!!!!!!!! TemporarySharedLibrary !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
 TemporarySharedLibrary::~TemporarySharedLibrary()
 {
@@ -76,25 +76,6 @@ TemporarySharedLibrary::~TemporarySharedLibrary()
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!!! DefaultSharedLibrary !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
-SLANG_NO_THROW SlangResult SLANG_MCALL DefaultSharedLibrary::queryInterface(SlangUUID const& uuid, void** outObject)
-{
-    // Mechanism to cast to underlying type. 
-    // NOTE! Purposefully does not ref count
-    if (uuid == DefaultSharedLibrary::getTypeGuid())
-    {
-        *outObject = this;
-        return SLANG_OK;
-    }
-
-    if (uuid == ISlangUnknown::getTypeGuid() || uuid == ISlangSharedLibrary::getTypeGuid()) 
-    {
-        ++m_refCount;
-        *outObject = static_cast<ISlangSharedLibrary*>(this);
-        return SLANG_OK;
-    }
-    return SLANG_E_NO_INTERFACE;
-}
-
 DefaultSharedLibrary::~DefaultSharedLibrary()
 {
     if (m_sharedLibraryHandle)
@@ -107,6 +88,37 @@ void* DefaultSharedLibrary::findSymbolAddressByName(char const* name)
 {
     return SharedLibrary::findSymbolAddressByName(m_sharedLibraryHandle, name);
 }
+
+void* DefaultSharedLibrary::castAs(const SlangUUID& guid)
+{
+    if (auto intf = getInterface(guid))
+    {
+        return intf;
+    }
+    return getObject(guid);
+}
+
+void* DefaultSharedLibrary::getInterface(const Guid& guid)
+{
+    if (guid == ISlangUnknown::getTypeGuid() ||
+        guid == ICastable::getTypeGuid() ||
+        guid == ISlangSharedLibrary::getTypeGuid())
+    {
+        return static_cast<ISlangSharedLibrary*>(this);
+    }
+    return nullptr;
+}
+
+void* DefaultSharedLibrary::getObject(const Guid& guid)
+{
+    if (guid == DefaultSharedLibrary::getTypeGuid())
+    {
+        return this;
+    }
+    return nullptr;
+}
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!! SharedLibraryUtils !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
 String SharedLibraryUtils::getSharedLibraryFileName(void* symbolInLib)
 {
--- source/slang/slang-compiler.cpp
+++ source/slang/slang-compiler.cpp
@@ -55,6 +55,52 @@
 
 namespace Slang
 {
+    // A temporary class that adapts `ISlangSharedLibrary_Dep1` to ISlangSharedLibrary
+
+    class SharedLibraryDep1Adapter : public ComBaseObject, public ISlangSharedLibrary
+    {
+    public:
+        SLANG_COM_BASE_IUNKNOWN_ALL
+
+        // ICastable
+        virtual SLANG_NO_THROW void* SLANG_MCALL castAs(const SlangUUID& guid) SLANG_OVERRIDE;
+
+        // ISlangSharedLibrary
+        virtual SLANG_NO_THROW void* SLANG_MCALL findSymbolAddressByName(char const* name) SLANG_OVERRIDE { return m_contained->findSymbolAddressByName(name); }
+
+        SharedLibraryDep1Adapter(ISlangSharedLibrary_Dep1* dep1):
+            m_contained(dep1)
+        {
+        }
+
+    protected:
+        void* getInterface(const Guid& guid)
+        {
+            if (guid == ISlangUnknown::getTypeGuid() ||
+                guid == ICastable::getTypeGuid() ||
+                guid == ISlangSharedLibrary::getTypeGuid())
+            {
+                return static_cast<ISlangSharedLibrary*>(this);
+            }
+            return nullptr;
+        }
+        void* getObject(const Guid& guid)
+        {
+            SLANG_UNUSED(guid);
+            return nullptr;
+        }
+
+        ComPtr<ISlangSharedLibrary_Dep1> m_contained;
+    };
+
+    void* SharedLibraryDep1Adapter::castAs(const SlangUUID& guid)
+    {
+        if (auto intf = getInterface(guid))
+        {
+            return intf;
+        }
+        return getObject(guid);
+    }
 
 // !!!!!!!!!!!!!!!!!!!!!! free functions for DiagnosicSink !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -81,7 +127,24 @@ namespace Slang
     {
         if (downstreamResult)
         {
-            return downstreamResult->getHostCallableSharedLibrary(outSharedLibrary);
+            // TODO(JS): Work around for not knowing actual interface this is returning, 
+            // and needing to support deps interface
+
+            ComPtr<ISlangSharedLibrary> lib;
+            SLANG_RETURN_ON_FAIL(downstreamResult->getHostCallableSharedLibrary(lib));
+
+            if (SLANG_SUCCEEDED(lib->queryInterface(ISlangSharedLibrary::getTypeGuid(), (void**)outSharedLibrary.writeRef())))
+            {
+                return SLANG_OK;
+            }
+            
+            ComPtr<ISlangSharedLibrary_Dep1> libDep1;
+            if (SLANG_SUCCEEDED(lib->queryInterface(ISlangSharedLibrary_Dep1::getTypeGuid(), (void**)libDep1.writeRef())))
+            {
+                // Okay, we need to adapt for now
+                outSharedLibrary = new SharedLibraryDep1Adapter(libDep1);
+                return SLANG_OK;
+            }
         }
         return SLANG_FAIL;
     }
