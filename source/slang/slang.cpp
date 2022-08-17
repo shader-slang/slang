cbc7e62f42bc0770fbd9da20ed39a5191778e2af
Yong He <yonghe@outlook.com>
2022-08-16 19:31:14 -0700

    Compute canonical path for modules loaded from source. (#2365)

--- source/slang/slang.cpp
+++ source/slang/slang.cpp
@@ -1031,9 +1031,19 @@ SLANG_NO_THROW slang::IModule* SLANG_MCALL Linkage::loadModuleFromSource(
         {
             return loadedModule;
         }
+        String pathStr = path;
+        auto pathInfo = PathInfo::makeFromString(pathStr);
+        if (File::exists(pathStr))
+        {
+            String cannonicalPath;
+            if (SLANG_SUCCEEDED(Path::getCanonical(pathStr, cannonicalPath)))
+            {
+                pathInfo = PathInfo::makeNormal(pathStr, cannonicalPath);
+            }
+        }
         auto module = loadModule(
             name,
-            PathInfo::makeFromString(path),
+            pathInfo,
             source,
             SourceLoc(),
             &sink,
