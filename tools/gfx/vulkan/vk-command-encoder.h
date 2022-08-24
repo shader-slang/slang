07e931df7a9b6b8311e53d340ad3de9b7bb251d4
Lucy Chen <lucchen@nvidia.com>
2022-08-24 11:24:21 -0700

    Fixed math errors in SBT creation causing a ray tracing test to fail with a fully zero output when given a non-zero raygen shader index

--- tools/gfx/vulkan/vk-command-encoder.h
+++ tools/gfx/vulkan/vk-command-encoder.h
@@ -4,6 +4,14 @@
 #include "vk-base.h"
 #include "vk-pipeline-state.h"
 
+#ifndef ENABLE_VALIDATION_LAYER
+#    if _DEBUG
+#        define ENABLE_VALIDATION_LAYER 1
+#    else
+#        define ENABLE_VALIDATION_LAYER 0
+#    endif
+#endif
+
 namespace gfx
 {
 
