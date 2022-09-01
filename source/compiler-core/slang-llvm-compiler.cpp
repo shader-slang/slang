327258ba9424669418d16d57f3a757461b942f39
jsmall-nvidia <jsmall@nvidia.com>
2022-09-01 13:34:11 -0400

    Small tidy around includes in slang-llvm-compiler.cpp

--- source/compiler-core/slang-llvm-compiler.cpp
+++ source/compiler-core/slang-llvm-compiler.cpp
@@ -2,22 +2,6 @@
 #include "slang-llvm-compiler.h"
 
 #include "../core/slang-common.h"
-#include "../../slang-com-helper.h"
-
-#include "../core/slang-blob.h"
-
-#include "../core/slang-string-util.h"
-#include "../core/slang-string-slice-pool.h"
-
-#include "../core/slang-io.h"
-#include "../core/slang-shared-library.h"
-#include "../core/slang-semantic-version.h"
-#include "../core/slang-char-util.h"
-
-#include "slang-include-system.h"
-#include "slang-source-loc.h"
-
-#include "../core/slang-shared-library.h"
 
 namespace Slang
 {
