83ea34ef4ecef790d87df0980bee83795392fd41
Yong He <yhe@nvidia.com>
2022-09-28 17:27:03 -0700

    fix1

--- github_macos_build.sh
+++ github_macos_build.sh
@@ -24,11 +24,10 @@ rm -rf ./bin
 ARCH="arm64"
 # Create the makefile
 ./premake5 gmake --cc=${CC} --enable-xlib=false --enable-embed-stdlib=true --arch=${TARGETARCH} --deps=true --no-progress=true  --skip-source-generation=true --deploy-slang-llvm=false --deploy-slang-glslang=false
-
+make config=${CONFIGURATION}_${TARGETARCH} ARCH=arm64  -j`sysctl -n hw.ncpu`
 else
 # Create the makefile
 ./premake5 gmake --cc=${CC} --enable-xlib=false --enable-embed-stdlib=true --arch=${TARGETARCH} --deps=true --no-progress=true
-fi
-
 # Build the configuration
-make config=${CONFIGURATION}_${TARGETARCH} -j`sysctl -n hw.ncpu`
+make config=${CONFIGURATION}_${TARGETARCH}  -j`sysctl -n hw.ncpu`
+fi
