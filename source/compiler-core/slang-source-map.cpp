072a3c3abbf05c47e310255f5997d3ce38bb491f
jsmall-nvidia <jsmall@nvidia.com>
2023-03-15 17:55:42 -0400

    Fix tabs.

--- source/compiler-core/slang-source-map.cpp
+++ source/compiler-core/slang-source-map.cpp
@@ -16,11 +16,11 @@ static const StructRttiInfo _makeJSONSourceMap_Rtti()
 
     builder.addField("version", &obj.version);
     builder.addField("file", &obj.file);
-	builder.addField("sourceRoot", &obj.sourceRoot);
-	builder.addField("sources", &obj.sources);
-	builder.addField("sourcesContent", &obj.sourcesContent);
-	builder.addField("names", &obj.names);
-	builder.addField("mappings", &obj.mappings);
+    builder.addField("sourceRoot", &obj.sourceRoot);
+    builder.addField("sources", &obj.sources);
+    builder.addField("sourcesContent", &obj.sourcesContent);
+    builder.addField("names", &obj.names);
+    builder.addField("mappings", &obj.mappings);
 
     return builder.make();
 }
@@ -31,18 +31,18 @@ static const char g_vlqEncodeTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmn
 
 struct VlqDecodeTable
 {
-	VlqDecodeTable()
-	{
-		::memset(map, -1, sizeof(map));
-		for (Index i = 0; i < SLANG_COUNT_OF(g_vlqEncodeTable); ++i)
-		{
-			map[g_vlqEncodeTable[i]] = int8_t(i);
-		}
-	}
-		/// Returns a *negative* value if invalid
-	int8_t operator[](char c) const { return (c & 0x80) | map[c & 0x7f]; }
+    VlqDecodeTable()
+    {
+        ::memset(map, -1, sizeof(map));
+        for (Index i = 0; i < SLANG_COUNT_OF(g_vlqEncodeTable); ++i)
+        {
+            map[g_vlqEncodeTable[i]] = int8_t(i);
+        }
+    }
+        /// Returns a *negative* value if invalid
+    int8_t operator[](char c) const { return (c & 0x80) | map[c & 0x7f]; }
 
-	int8_t map[128];
+    int8_t map[128];
 };
 
 static const VlqDecodeTable g_vlqDecodeTable;
@@ -55,189 +55,189 @@ bit of the first digit is used as the sign bit. */
 
 static SlangResult _decode(UnownedStringSlice& ioEncoded, Index& out)
 {
-	Index v = 0;
+    Index v = 0;
 
-	Index shift = 0;
-	const char* cur = ioEncoded.begin();
-	const char* end = ioEncoded.end();
+    Index shift = 0;
+    const char* cur = ioEncoded.begin();
+    const char* end = ioEncoded.end();
 
-	// Must have some chars
-	if (cur >= end)
-	{
-		return SLANG_FAIL;
-	}
+    // Must have some chars
+    if (cur >= end)
+    {
+        return SLANG_FAIL;
+    }
 
-	for (; cur < end; ++cur)
-	{
-		const Index value = g_vlqDecodeTable[*cur];
-		if (value < 0)
-		{
-			return SLANG_FAIL;
-		}
-		
-		v += (value & 0x1f) << shift;
+    for (; cur < end; ++cur)
+    {
+        const Index value = g_vlqDecodeTable[*cur];
+        if (value < 0)
+        {
+            return SLANG_FAIL;
+        }
+        
+        v += (value & 0x1f) << shift;
 
-		// If the continuation bit is not set we are done
-		if (( value & 0x20) == 0)
-		{
-			++cur;
-			break;
-		}
+        // If the continuation bit is not set we are done
+        if (( value & 0x20) == 0)
+        {
+            ++cur;
+            break;
+        }
 
-		shift += 5;
-	}
+        shift += 5;
+    }
 
-	// Save out the remaining part
-	ioEncoded = UnownedStringSlice(cur, end);
+    // Save out the remaining part
+    ioEncoded = UnownedStringSlice(cur, end);
 
-	const Index sign = -(v & 1);
-	// Remove the least significant bit
-	v = v >> 1;
+    const Index sign = -(v & 1);
+    // Remove the least significant bit
+    v = v >> 1;
 
-	// sign will be -1 if need to make negative, 0 if not.
-	// This is just equvalent to -v if sign == -1, else just v.
-	out = (v ^ sign) - sign;
+    // sign will be -1 if need to make negative, 0 if not.
+    // This is just equvalent to -v if sign == -1, else just v.
+    out = (v ^ sign) - sign;
 
-	return SLANG_OK;
+    return SLANG_OK;
 }
 
 SlangResult SourceMap::decode(JSONContainer* container, const JSONSourceMap& src)
 {
-	m_slicePool.clear();
+    m_slicePool.clear();
 
-	m_file = src.file;
-	m_sourceRoot = src.sourceRoot;
+    m_file = src.file;
+    m_sourceRoot = src.sourceRoot;
 
-	m_lineStarts.clear();
-	m_lineEntries.clear();
+    m_lineStarts.clear();
+    m_lineEntries.clear();
 
-	const Count sourcesCount = src.sources.getCount();
-	
-	// These should all be unique, but for simplicity, we build a table
-	m_sources.setCount(sourcesCount);
-	for (Index i = 0; i < sourcesCount; ++i)
-	{
-		m_sources[i] = m_slicePool.add(src.sources[i]);
-	}
+    const Count sourcesCount = src.sources.getCount();
+    
+    // These should all be unique, but for simplicity, we build a table
+    m_sources.setCount(sourcesCount);
+    for (Index i = 0; i < sourcesCount; ++i)
+    {
+        m_sources[i] = m_slicePool.add(src.sources[i]);
+    }
 
-	Count sourcesContentCount = src.sourcesContent.getCount();
-	sourcesContentCount = std::min(sourcesContentCount, sourcesCount);
+    Count sourcesContentCount = src.sourcesContent.getCount();
+    sourcesContentCount = std::min(sourcesContentCount, sourcesCount);
 
-	m_sourcesContent.setCount(sourcesContentCount);
-	for (auto& cur : m_sourcesContent)
-	{
-		cur = StringSlicePool::kNullHandle;
-	}
+    m_sourcesContent.setCount(sourcesContentCount);
+    for (auto& cur : m_sourcesContent)
+    {
+        cur = StringSlicePool::kNullHandle;
+    }
 
-	for (Index i = 0; i < sourcesContentCount; ++i)
-	{
-		auto value = src.sourcesContent[i];
+    for (Index i = 0; i < sourcesContentCount; ++i)
+    {
+        auto value = src.sourcesContent[i];
 
-		if (value.type != JSONValue::Type::Null)
-		{
-			if (value.getKind() == JSONValue::Kind::String)
-			{
-				auto stringValue = container->getString(value);
-				m_sourcesContent[i] = m_slicePool.add(stringValue);
-			}
-		}
-	}
+        if (value.type != JSONValue::Type::Null)
+        {
+            if (value.getKind() == JSONValue::Kind::String)
+            {
+                auto stringValue = container->getString(value);
+                m_sourcesContent[i] = m_slicePool.add(stringValue);
+            }
+        }
+    }
 
-	
-	List<UnownedStringSlice> lines;
-	StringUtil::split(src.mappings, ';', lines);
-	
-	List<UnownedStringSlice> segments;
+    
+    List<UnownedStringSlice> lines;
+    StringUtil::split(src.mappings, ';', lines);
+    
+    List<UnownedStringSlice> segments;
 
-	// Index into sources 
-	Index sourceFileIndex = 0;
+    // Index into sources 
+    Index sourceFileIndex = 0;
 
-	Index sourceLine = 0;
-	Index sourceColumn = 0;
-	Index nameIndex = 0;
+    Index sourceLine = 0;
+    Index sourceColumn = 0;
+    Index nameIndex = 0;
 
-	const Count linesCount = lines.getCount();
+    const Count linesCount = lines.getCount();
 
-	m_lineStarts.setCount(linesCount + 1);
+    m_lineStarts.setCount(linesCount + 1);
 
-	for (Index generatedLine = 0; generatedLine < linesCount; ++generatedLine)
-	{
-		const auto line = lines[generatedLine];
+    for (Index generatedLine = 0; generatedLine < linesCount; ++generatedLine)
+    {
+        const auto line = lines[generatedLine];
 
-		m_lineStarts[generatedLine] = m_lineEntries.getCount();
+        m_lineStarts[generatedLine] = m_lineEntries.getCount();
 
-		// If it's empty move to next line
-		if (line.getLength() == 0)
-		{
-			continue;
-		}
+        // If it's empty move to next line
+        if (line.getLength() == 0)
+        {
+            continue;
+        }
 
-		// Split the line into segments
-		segments.clear();
-		StringUtil::split(line, ',', segments);
+        // Split the line into segments
+        segments.clear();
+        StringUtil::split(line, ',', segments);
 
-		Index generatedColumn = 0;
+        Index generatedColumn = 0;
 
-		for (auto segment : segments)
-		{
-			Index colDelta;
-			SLANG_RETURN_ON_FAIL(_decode(segment, colDelta));
+        for (auto segment : segments)
+        {
+            Index colDelta;
+            SLANG_RETURN_ON_FAIL(_decode(segment, colDelta));
 
-			generatedColumn += colDelta;
-			SLANG_ASSERT(generatedColumn >= 0);
+            generatedColumn += colDelta;
+            SLANG_ASSERT(generatedColumn >= 0);
 
-			// It can be 4 or 5 parts
-			if (segment.getLength())
-			{
-				/* If present, an zero-based index into the “sources” list. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented.
-					If present, the zero-based starting line in the original source represented. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented. Always present if there is a source field.
-					If present, the zero-based starting column of the line in the source represented. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented. Always present if there is a source field.
-					 */
+            // It can be 4 or 5 parts
+            if (segment.getLength())
+            {
+                /* If present, an zero-based index into the “sources” list. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented.
+                    If present, the zero-based starting line in the original source represented. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented. Always present if there is a source field.
+                    If present, the zero-based starting column of the line in the source represented. This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented. Always present if there is a source field.
+                     */
 
-				Index sourceFileDelta;
-				Index sourceLineDelta;
-				Index sourceColumnDelta;
+                Index sourceFileDelta;
+                Index sourceLineDelta;
+                Index sourceColumnDelta;
 
-				SLANG_RETURN_ON_FAIL(_decode(segment, sourceFileDelta));
-				SLANG_RETURN_ON_FAIL(_decode(segment, sourceLineDelta));
-				SLANG_RETURN_ON_FAIL(_decode(segment, sourceColumnDelta));
+                SLANG_RETURN_ON_FAIL(_decode(segment, sourceFileDelta));
+                SLANG_RETURN_ON_FAIL(_decode(segment, sourceLineDelta));
+                SLANG_RETURN_ON_FAIL(_decode(segment, sourceColumnDelta));
 
-				sourceFileIndex += sourceFileDelta;
-				sourceLine += sourceLineDelta;
-				sourceColumn += sourceColumnDelta;
+                sourceFileIndex += sourceFileDelta;
+                sourceLine += sourceLineDelta;
+                sourceColumn += sourceColumnDelta;
 
-				SLANG_ASSERT(sourceFileIndex >= 0);
-				SLANG_ASSERT(sourceLine >= 0);
-				SLANG_ASSERT(sourceColumn >= 0);
+                SLANG_ASSERT(sourceFileIndex >= 0);
+                SLANG_ASSERT(sourceLine >= 0);
+                SLANG_ASSERT(sourceColumn >= 0);
 
-				// 5 parts
-				if (segment.getLength() > 0)
-				{
-					/* If present, the zero - based index into the “names” list associated with this segment.This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented.
-					*/
+                // 5 parts
+                if (segment.getLength() > 0)
+                {
+                    /* If present, the zero - based index into the “names” list associated with this segment.This field is a base 64 VLQ relative to the previous occurrence of this field, unless this is the first occurrence of this field, in which case the whole value is represented.
+                    */
 
-					Index nameDelta;
-					SLANG_RETURN_ON_FAIL(_decode(segment, nameDelta));
+                    Index nameDelta;
+                    SLANG_RETURN_ON_FAIL(_decode(segment, nameDelta));
 
-					nameIndex += nameDelta;
-					SLANG_ASSERT(nameIndex >= 0);
-				}
-			}
+                    nameIndex += nameDelta;
+                    SLANG_ASSERT(nameIndex >= 0);
+                }
+            }
 
-			Entry entry;
-			entry.generatedColumn = generatedColumn;
-			entry.sourceColumn = sourceColumn;
-			entry.sourceLine = sourceLine;
-			entry.sourceFileIndex = sourceFileIndex;
+            Entry entry;
+            entry.generatedColumn = generatedColumn;
+            entry.sourceColumn = sourceColumn;
+            entry.sourceLine = sourceLine;
+            entry.sourceFileIndex = sourceFileIndex;
 
-			m_lineEntries.add(entry);
-		}
-	}
+            m_lineEntries.add(entry);
+        }
+    }
 
-	// Mark the end
-	m_lineStarts[linesCount] = m_lineEntries.getCount();
+    // Mark the end
+    m_lineStarts[linesCount] = m_lineEntries.getCount();
 
-	return SLANG_OK;
+    return SLANG_OK;
 }
 
 
--- source/compiler-core/slang-source-map.h
+++ source/compiler-core/slang-source-map.h
@@ -38,78 +38,78 @@ Example...
 
 struct JSONSourceMap
 {
-		/// File version (always the first entry in the object) and must be a positive integer.
-	int32_t version = 3;					
-		/// An optional name of the generated code that this source map is associated with.
-	String file;							
-		/// An optional source root, useful for relocating source files on a server or removing repeated values in 
-		/// the “sources” entry.  This value is prepended to the individual entries in the “source” field.
-	String sourceRoot;						
-		/// A list of original sources used by the “mappings” entry.
-	List<UnownedStringSlice> sources;			
-		/// An optional list of source content, useful when the “source” can’t be hosted. The contents are listed in the same order as the sources in line 5. 
-		/// “null” may be used if some original sources should be retrieved by name.
-		/// Because could be a string or nullptr, we use JSONValue to hold value.
-	List<JSONValue> sourcesContent;	
-		/// A list of symbol names used by the “mappings” entry.
-	List<UnownedStringSlice> names;
-		/// A string with the encoded mapping data.
-	UnownedStringSlice mappings;
+        /// File version (always the first entry in the object) and must be a positive integer.
+    int32_t version = 3;                    
+        /// An optional name of the generated code that this source map is associated with.
+    String file;                            
+        /// An optional source root, useful for relocating source files on a server or removing repeated values in 
+        /// the “sources” entry.  This value is prepended to the individual entries in the “source” field.
+    String sourceRoot;                        
+        /// A list of original sources used by the “mappings” entry.
+    List<UnownedStringSlice> sources;            
+        /// An optional list of source content, useful when the “source” can’t be hosted. The contents are listed in the same order as the sources in line 5. 
+        /// “null” may be used if some original sources should be retrieved by name.
+        /// Because could be a string or nullptr, we use JSONValue to hold value.
+    List<JSONValue> sourcesContent;    
+        /// A list of symbol names used by the “mappings” entry.
+    List<UnownedStringSlice> names;
+        /// A string with the encoded mapping data.
+    UnownedStringSlice mappings;
 
-	static const StructRttiInfo g_rttiInfo;
+    static const StructRttiInfo g_rttiInfo;
 };
 
 struct SourceMap
 {
-	struct Entry
-	{
-		// Note! All column/line are zero indexed
+    struct Entry
+    {
+        // Note! All column/line are zero indexed
 
-		Index generatedColumn;		///< The generated column
-		Index sourceFileIndex;		///< The index into the source name/contents
-		Index sourceLine;			///< The line number in the originating source
-		Index sourceColumn;			///< The column number in the originating source
-	};
+        Index generatedColumn;        ///< The generated column
+        Index sourceFileIndex;        ///< The index into the source name/contents
+        Index sourceLine;            ///< The line number in the originating source
+        Index sourceColumn;            ///< The column number in the originating source
+    };
 
-	SlangResult decode(JSONContainer* container, const JSONSourceMap& src);
+    SlangResult decode(JSONContainer* container, const JSONSourceMap& src);
 
-		/// Get the total number of generated lines
-	Count getGeneratedLineCount() const { return m_lineStarts.getCount() - 1; }
-		/// Get the entries on the line
-	SLANG_FORCE_INLINE ConstArrayView<Entry> getEntriesForLine(Index generatedLine) const;
-	
-		/// Ctor
-	SourceMap():
-		m_slicePool(StringSlicePool::Style::Default)
-	{
-	}
+        /// Get the total number of generated lines
+    Count getGeneratedLineCount() const { return m_lineStarts.getCount() - 1; }
+        /// Get the entries on the line
+    SLANG_FORCE_INLINE ConstArrayView<Entry> getEntriesForLine(Index generatedLine) const;
+    
+        /// Ctor
+    SourceMap():
+        m_slicePool(StringSlicePool::Style::Default)
+    {
+    }
 
-	String m_file;
-	String m_sourceRoot;
+    String m_file;
+    String m_sourceRoot;
 
-	List<StringSlicePool::Handle> m_sources;
+    List<StringSlicePool::Handle> m_sources;
 
-		/// Storage for the contents. Can be unset null to indicate not set. 
-	List<StringSlicePool::Handle> m_sourcesContent;
-	
-	List<Index> m_lineStarts;
-	List<Entry> m_lineEntries;
+        /// Storage for the contents. Can be unset null to indicate not set. 
+    List<StringSlicePool::Handle> m_sourcesContent;
+    
+    List<Index> m_lineStarts;
+    List<Entry> m_lineEntries;
 
-	StringSlicePool m_slicePool;
+    StringSlicePool m_slicePool;
 };
 
 // -------------------------------------------------------------
 SLANG_FORCE_INLINE ConstArrayView<SourceMap::Entry> SourceMap::getEntriesForLine(Index generatedLine) const
 {
-	const Index start = m_lineStarts[generatedLine];
-	const Index end = m_lineStarts[generatedLine + 1];
-	
-	const auto entries = m_lineEntries.begin();
+    const Index start = m_lineStarts[generatedLine];
+    const Index end = m_lineStarts[generatedLine + 1];
+    
+    const auto entries = m_lineEntries.begin();
 
-	SLANG_ASSERT(start >= 0 && start < m_lineEntries.getCount());
-	SLANG_ASSERT(end >= start && end >= 0 && end < m_lineEntries.getCount());
+    SLANG_ASSERT(start >= 0 && start < m_lineEntries.getCount());
+    SLANG_ASSERT(end >= start && end >= 0 && end < m_lineEntries.getCount());
 
-	return ConstArrayView<SourceMap::Entry>(entries + start, end - start);
+    return ConstArrayView<SourceMap::Entry>(entries + start, end - start);
 }
 
 
