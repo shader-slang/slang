af04202fdf5d2a84435b069fdbc1e39486b04c21
jsmall-nvidia <jsmall@nvidia.com>
2023-06-29 17:13:31 -0400

    Fix a typo.

--- source/slang/slang-ir-lower-l-value-cast.h
+++ source/slang/slang-ir-lower-l-value-cast.h
@@ -6,7 +6,7 @@
 // 
 // Depending on the target this could produce
 // 
-// * Nothing - some kinds of casts are implicit for some targets. Such as conversions between uint/int on c like languges
+// * Nothing - some kinds of casts are implicit for some targets such as HLSL on out parameters for same sized integer types
 // * A reinterpret cast. On targets with pointers, such as C++/CUDA we can fix the problem by just casting to the appropriate pointer (for some kinds of conversions)
 // * Creating a temporary of the right type and calling the function, and *converting* to the target (say an out parameter)
 // * Creating a temporary, converting the value into the temporary, calling the function, and converting back to the source
