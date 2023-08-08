0ea3d5bea057933eeb6461d0676bec03f89d82c8
Ellie Hermaszewska <ellieh@nvidia.com>
2023-08-08 16:04:30 +0800

    Only specialize intrinsic functions with predicates

--- source/slang/slang-ir.cpp
+++ source/slang/slang-ir.cpp
@@ -7704,8 +7704,7 @@ namespace Slang
         {
         case kIROp_Func:
         case kIROp_Generic:
-            // TODO: We should probably take into account our target to see if the intrinsic applies
-            return val->getFirstChild() != nullptr || val->findDecoration<IRTargetIntrinsicDecoration>();
+            return val->getFirstChild() != nullptr;
 
         case kIROp_GlobalConstant:
             return cast<IRGlobalConstant>(val)->getValue() != nullptr;
