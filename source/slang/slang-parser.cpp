d4e72a93e13ab880b2f355fb7fb5d55cf0113c3d
Yong He <yonghe@outlook.com>
2023-08-09 21:52:13 -0700

    Support nested structs in function bodies. (#3078)

--- source/slang/slang-parser.cpp
+++ source/slang/slang-parser.cpp
@@ -4581,6 +4581,25 @@ namespace Slang
         Token closingBraceToken;
         while (!AdvanceIfMatch(this, MatchedTokenType::CurlyBraces, &closingBraceToken))
         {
+            if (LookAheadToken("struct"))
+            {
+                auto structDecl = ParseStruct();
+                AddMember(scopeDecl, structDecl);
+                continue;
+            }
+            else if (AdvanceIf(this, "typedef"))
+            {
+                auto typeDefDecl = parseTypeDef(this, nullptr);
+                AddMember(scopeDecl, (Decl*)typeDefDecl);
+                continue;
+            }
+            else if (AdvanceIf(this, "typealias"))
+            {
+                auto typeDefDecl = parseTypeAliasDecl(this, nullptr);
+                AddMember(scopeDecl, (Decl*)typeDefDecl);
+                continue;
+            }
+
             auto stmt = ParseStatement();
             if(stmt)
             {
