ea53d704736a2b8dae9c2b6ffd36a64daf4211b9
Ellie Hermaszewska <ellieh@nvidia.com>
2023-08-24 16:02:35 +0800

    Allow requiring glsl language extensions on structs

--- source/slang/slang-emit-c-like.cpp
+++ source/slang/slang-emit-c-like.cpp
@@ -3720,10 +3720,6 @@ void CLikeSourceEmitter::emitSimpleFuncImpl(IRFunc* func)
         emitEntryPointAttributes(func, entryPointDecor);
     }
 
-    // Deal with required features/capabilities of the function
-    //
-    handleRequiredCapabilitiesImpl(func);
-
     emitFunctionPreambleImpl(func);
 
     emitFuncDecorations(func);
@@ -4882,6 +4878,10 @@ static bool _shouldSkipFuncEmit(IRInst* func)
 void CLikeSourceEmitter::emitGlobalInstImpl(IRInst* inst)
 {
     m_writer->advanceToSourceLocation(inst->sourceLoc);
+
+    // Deal with required features/capabilities of the global inst
+    //
+    handleRequiredCapabilitiesImpl(inst);
 
     switch (inst->getOp())
     {
