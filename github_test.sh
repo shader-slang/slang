50eb33b339c6c9beca6fd50efe6beb22c060fb8d
Yong He <yhe@nvidia.com>
2023-09-01 01:22:10 -0700

    Add gfx-smoke to linux expected failure list.

--- github_test.sh
+++ github_test.sh
@@ -37,4 +37,4 @@ fi
 SLANG_TEST=${OUTPUTDIR}slang-test
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OUTPUTDIR
-${SLANG_TEST} -bindir ${OUTPUTDIR} -travis -category ${SLANG_TEST_CATEGORY} ${SLANG_TEST_FLAGS} -api all-vk
+${SLANG_TEST} -bindir ${OUTPUTDIR} -travis -category ${SLANG_TEST_CATEGORY} ${SLANG_TEST_FLAGS} -api all-vk -expected-failure-list tests/expected-failure-linux.txt
