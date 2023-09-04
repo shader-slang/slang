c99f53e3750b57d49c182adc5cd159498ffe4092
Ellie Hermaszewska <ellieh@nvidia.com>
2023-09-04 16:38:19 +0800

    Better 1-vector legalization

--- source/slang/slang-ir-legalize-vector-types.cpp
+++ source/slang/slang-ir-legalize-vector-types.cpp
@@ -32,13 +32,120 @@ namespace Slang
             workListSet.add(inst);
         }
 
-        void process1VectorType(IRVectorType* inst)
+        void process1VectorPtrType(IRPtrTypeBase* ptr)
+        {
+            IRBuilder builder(module);
+            const auto vec1Type = cast<IRVectorType>(ptr->getValueType());
+            const auto scalarType = vec1Type->getElementType();
+            const auto scalarPtr = builder.getPtrType(ptr->getOp(), scalarType, ptr->getAddressSpace());
+
+            traverseUses(ptr, [&](IRUse* use){
+                const auto user = use->user;
+
+                // Types will be updated later when we replaceUsesWith
+                if(as<IRType>(user))
+                    return;
+
+                InstHashSet vec1PtrConsumers(module);
+
+                if(user->getDataType() == ptr)
+                {
+                    for(auto u = user->firstUse; u; u = u->nextUse)
+                        if(!vec1PtrConsumers.contains(u->getUser()))
+                            vec1PtrConsumers.add(u->getUser());
+                }
+
+                switch(user->getOp())
+                {
+                case kIROp_FieldAddress:
+                {
+                    // const auto fa = cast<IRFieldAddress>(user);
+                    // const auto scalar = fa->getBase();
+                    // user->replaceUsesWith(scalar);
+                    // user->removeAndDeallocate();
+                }
+                break;
+                case kIROp_GetElementPtr:
+                // {
+                //     const auto fa = cast<IRGetElementPtr>(user);
+                //     const auto scalar = fa->getBase();
+                //     user->replaceUsesWith(scalar);
+                //     user->removeAndDeallocate();
+                // }
+                break;
+                case kIROp_Var:
+                {
+                    builder.setInsertBefore(user);
+                    const auto scalarVar = builder.emitVar(scalarType);
+                    user->replaceUsesWith(scalarVar);
+                    user->removeAndDeallocate();
+                }
+                break;
+
+                // Turn this on to easily find more cases to add
+#               if 1
+                default:
+                {
+                    String e = "Missing case in process1PtrVectorType:\n"
+                        + dumpIRToString(user, {IRDumpOptions::Mode::Detailed, 0});
+                    SLANG_UNIMPLEMENTED_X(e.begin());
+                }
+#               endif
+                }
+
+                //
+                // Now we handle the uses of all ptr<vec1> typed things
+                //
+                for(const auto vec1PtrConsumer : *vec1PtrConsumers.set)
+                {
+                    switch(vec1PtrConsumer->getOp())
+                    {
+                    case kIROp_GetElementPtr:
+                    {
+                        const auto fa = cast<IRGetElementPtr>(vec1PtrConsumer);
+                        const auto scalar = fa->getBase();
+                        vec1PtrConsumer->replaceUsesWith(scalar);
+                        vec1PtrConsumer->removeAndDeallocate();
+                    }
+
+                    // Turn this on to easily find more cases to add
+#               if 0
+                    default:
+                    {
+                        String e = "Missing case in process1VectorPtrType vec1PtrValUser:\n"
+                            + dumpIRToString(vec1PtrConsumer, {IRDumpOptions::Mode::Detailed, 0});
+                        SLANG_UNIMPLEMENTED_X(e.begin());
+                    }
+#endif
+                    }
+                }
+            });
+        }
+
+        void process1VectorType(IRType* vecTy)
         {
             InstHashSet vec1Consumers(module);
 
-            const auto scalarType = inst->getElementType();
-            traverseUses(inst, [&](IRUse* use){
+            const auto vecDataTy
+                = as<IRVectorType>(vecTy) ? cast<IRVectorType>(vecTy)
+                : as<IRRateQualifiedType>(vecTy) ? as<IRVectorType>(cast<IRRateQualifiedType>(vecTy)->getValueType())
+                : nullptr;
+
+            SLANG_ASSERT(vecDataTy);
+
+            IRBuilder builder(module);
+            auto scalarType = vecDataTy->getElementType();
+            if(const auto r = as<IRRateQualifiedType>(vecTy))
+                scalarType = builder.getRateQualifiedType(r->getRate(), scalarType);
+
+            traverseUses(vecTy, [&](IRUse* use){
                 const auto user = use->user;
+
+                if(const auto v1Ptr = as<IRPtrTypeBase>(user))
+                    process1VectorPtrType(v1Ptr);
+
+                if(const auto rateType = as<IRRateQualifiedType>(user))
+                    return process1VectorType(rateType);
 
                 // Types will be updated when we replaceUsesWith
                 if(as<IRType>(user))
@@ -48,7 +155,7 @@ namespace Slang
                 // If this instruction produces a 1-vector, add its uses to the
                 // consumer list
                 //
-                if(user->getDataType() == inst)
+                if(user->getDataType() == vecTy)
                 {
                     for(auto u = user->firstUse; u; u = u->nextUse)
                         if(!vec1Consumers.contains(u->getUser()))
@@ -69,9 +176,9 @@ namespace Slang
                 case kIROp_MakeVectorFromScalar:
                 case kIROp_MakeVector:
                 {
-                    const auto scalar = use->user->getOperand(0);
-                    use->user->replaceUsesWith(scalar);
-                    use->user->removeAndDeallocate();
+                    const auto scalar = user->getOperand(0);
+                    user->replaceUsesWith(scalar);
+                    user->removeAndDeallocate();
                 }
                 break;
 
@@ -87,7 +194,6 @@ namespace Slang
                 }
             });
 
-            IRBuilder builder(module);
             //
             // Now we handle the uses of all vec1 typed things
             //
@@ -101,9 +207,17 @@ namespace Slang
 
                 // TODO: handle things here such as getElement, swizzle etc...
                 case kIROp_GetElementPtr:
-                case kIROp_GetElement:
                 case kIROp_SwizzledStore:
                     SLANG_UNIMPLEMENTED_X("Vector user in 1-vector legalization");
+
+                case kIROp_GetElement:
+                {
+                    const auto ge = cast<IRGetElement>(vec1Consumer);
+                    const auto scalar = ge->getBase();
+                    ge->replaceUsesWith(scalar);
+                    ge->removeAndDeallocate();
+                }
+                break;
 
                 case kIROp_swizzle:
                 {
@@ -117,7 +231,9 @@ namespace Slang
                     {
                         builder.setInsertBefore(swizzle);
                         // TODO: This isn't type-correct at this stage...
-                        const auto v = builder.emitMakeVectorFromScalar(scalarType, swizzle->getBase());
+                        const auto v = builder.emitMakeVectorFromScalar(
+                            vec1Consumer->getFullType(),
+                            swizzle->getBase());
                         swizzle->replaceUsesWith(v);
                     }
                     swizzle->removeAndDeallocate();
@@ -136,8 +252,8 @@ namespace Slang
                 }
             }
 
-            inst->replaceUsesWith(scalarType);
-            inst->removeAndDeallocate();
+            vecTy->replaceUsesWith(scalarType);
+            vecTy->removeAndDeallocate();
         }
 
         void processInst(IRInst* inst)
