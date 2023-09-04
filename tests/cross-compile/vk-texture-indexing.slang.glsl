67e01432eac8328e6049e2007d06bf86938bb230
Ellie Hermaszewska <ellieh@nvidia.com>
2023-09-04 23:46:26 +0800

    Inst numbering changes to cross compile tests

--- tests/cross-compile/vk-texture-indexing.slang.glsl
+++ tests/cross-compile/vk-texture-indexing.slang.glsl
@@ -3,31 +3,23 @@
 #extension GL_EXT_nonuniform_qualifier : require
 layout(row_major) uniform;
 layout(row_major) buffer;
-
 layout(binding = 0)
 uniform texture2D  gParams_textures_0[10];
 
-
 float fetchData_0(uvec2 coords_0, uint index_0)
 {
-    float _S1 = (texelFetch((gParams_textures_0[nonuniformEXT(index_0)]), ivec2((coords_0)), 0).x);
-
-    return _S1;
+    return (texelFetch((gParams_textures_0[nonuniformEXT(index_0)]), ivec2((coords_0)), 0).x);
 }
 
 layout(location = 0)
-out vec4 _S2;
-
+out vec4 _S1;
 
 flat layout(location = 0)
-in uvec3 _S3;
-
+in uvec3 _S2;
 
 void main()
 {
-
-    _S2 = vec4(fetchData_0(_S3.xy, _S3.z));
-
+    _S1 = vec4(fetchData_0(_S2.xy, _S2.z));
     return;
 }
 
--- tests/vkray/anyhit.slang.glsl
+++ tests/vkray/anyhit.slang.glsl
@@ -1,4 +1,3 @@
-// anyhit.slang.glsl
 #version 460
 #extension GL_EXT_ray_tracing : require
 layout(row_major) uniform;
@@ -12,8 +11,7 @@ layout(binding = 0)
 layout(std140) uniform _S1
 {
     int mode_0;
-} gParams_0;
-
+}gParams_0;
 layout(binding = 1)
 uniform texture2D gParams_alphaMap_0;
 
@@ -24,24 +22,21 @@ struct SphereHitAttributes_0
 {
     vec3 normal_0;
 };
+
 hitAttributeEXT SphereHitAttributes_0 _S2;
 
 struct ShadowRay_0
 {
     vec4 hitDistance_0;
 };
+
 rayPayloadInEXT ShadowRay_0 _S3;
 
 void main()
 {
     if(gParams_0.mode_0 != 0)
     {
-        float val_0 = textureLod(
-            sampler2D(gParams_alphaMap_0, gParams_sampler_0),
-            _S2.normal_0.xy,
-            (0.0)).x;
-
-        if(val_0 > 0.0)
+        if((textureLod(sampler2D(gParams_alphaMap_0,gParams_sampler_0), (_S2.normal_0.xy), (0.0)).x) > 0.0)
         {
             terminateRayEXT;;
         }
@@ -50,7 +45,6 @@ void main()
             ignoreIntersectionEXT;;
         }
     }
-
     return;
 }
 
