80e5872182d0bb616e539fc9c73606cdd9501a57
Ellie Hermaszewska <ellieh@nvidia.com>
2023-10-30 16:10:37 +0800

    tests working with cmake build

--- cmake/SlangTarget.cmake
+++ cmake/SlangTarget.cmake
@@ -217,4 +217,14 @@ function(slang_add_target dir type)
     if(DEFINED ARG_REQUIRES)
         add_dependencies(${target} ${ARG_REQUIRES})
     endif()
+
+    #
+    # Since we do a lot of dynamic loading, unconditionally set the build rpath
+    # to find our libraries. Ordinarily CMake would sort this out, but we do
+    # have libraries which at build time don't depend on any other shared
+    # libraries of ours but which do load them at runtime, hence the need to do
+    # this explicitly here.
+    #
+    set_property(TARGET ${target} APPEND PROPERTY BUILD_RPATH "$ORIGIN/../lib")
+    set_property(TARGET ${target} APPEND PROPERTY INSTALL_RPATH "$ORIGIN/../lib")
 endfunction()
