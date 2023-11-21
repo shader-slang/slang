4819e1c6b1d594dac176f44d0dbe2ddb3a273376
Ellie Hermaszewska <ellieh@nvidia.com>
2023-11-21 10:15:57 +0000

    Neaten llvm building

--- cmake/LLVM.cmake
+++ cmake/LLVM.cmake
@@ -1,110 +1,97 @@
-macro(find_llvm)
-    if(SLANG_USE_SYSTEM_LLVM)
-        #
-        # Very easy if we get LLVM from the system
-        #
-        find_package(LLVM 13.0 REQUIRED CONFIG)
-        find_package(Clang REQUIRED CONFIG)
-    else()
-        #
-        # Otherwise, do this awkward dance of downloading and building LLVM at
-        # the last minute
-        #
+macro(build_llvm)
+    #
+    # Settings one might wish to change for this build
+    #
+    set(llvm_version 13.0.1)
+    set(llvm_url
+        https://github.com/llvm/llvm-project/releases/download/llvmorg-${llvm_version}/llvm-project-${llvm_version}.src.tar.xz
+    )
+    set(llvm_sha256
+        326335a830f2e32d06d0a36393b5455d17dc73e0bd1211065227ee014f92cbf8
+    )
+    set(llvm_install_root "${CMAKE_BINARY_DIR}/llvm-project")
+    set(llvm_config Release)
 
-        set(llvm_version 13.0.1)
-        set(llvm_url
-            https://github.com/llvm/llvm-project/releases/download/llvmorg-${llvm_version}/llvm-project-${llvm_version}.src.tar.xz
-        )
-        set(llvm_sha256
-            326335a830f2e32d06d0a36393b5455d17dc73e0bd1211065227ee014f92cbf8
-        )
-        set(llvm_install_root "${CMAKE_BINARY_DIR}/llvm-project")
-        set(llvm_config Release)
-
-        if(CMAKE_CROSSCOMPILING)
-            message(
-                FATAL_ERROR
-                "Cross building LLVM isn't supported here, please enhance find_llvm() or use set SLANG_USE_SYSTEM_LLVM"
-            )
-        endif()
-
-        fetchcontent_declare(
-            llvm-project
-            URL ${llvm_url}
-            URL_HASH SHA256=${llvm_sha256}
-            PREFIX
-            llvm-project
-        )
-        fetchcontent_makeavailable(llvm-project)
-
-        set(LLVM_CMAKE_ARGS
-            # General CMake settings
-            -G${CMAKE_GENERATOR}
-            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-            -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
-            -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
-            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-            -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
-            -DCMAKE_INSTALL_PREFIX=${llvm_install_root}
-            -DCMAKE_BUILD_TYPE=${llvm_config}
-            # Don't build unnecessary things
-            -DLLVM_BUILD_LLVM_C_DYLIB=0
-            -DLLVM_INCLUDE_BENCHMARKS=0
-            -DLLVM_INCLUDE_DOCS=0
-            -DLLVM_INCLUDE_EXAMPLES=0
-            -DLLVM_INCLUDE_TESTS=0
-            -DLLVM_ENABLE_TERMINFO=0
-            -DCLANG_BUILD_TOOLS=0
-            -DCLANG_ENABLE_STATIC_ANALYZER=0
-            -DCLANG_ENABLE_ARCMT=0
-            -DCLANG_INCLUDE_DOCS=0
-            -DCLANG_INCLUDE_TESTS=0
-            # Requirements for Slang
-            -DCMAKE_CXX_VISIBILITY_PRESET=hidden
-            -DLLVM_ENABLE_PROJECTS=clang
-            -DLLVM_TARGETS_TO_BUILD=X86\\\;ARM\\\;AArch64
-            -DLLVM_BUILD_TOOLS=1
-        )
-
-        # Configure, build and install a CMake project
-        execute_process(
-            COMMAND
-                ${CMAKE_COMMAND} -S ${llvm-project_SOURCE_DIR}/llvm -B
-                ${llvm-project_BINARY_DIR} ${LLVM_CMAKE_ARGS}
-            WORKING_DIRECTORY ${llvm-project_SOURCE_DIR}
-            COMMAND_ERROR_IS_FATAL ANY
-        )
-        execute_process(
-            COMMAND
-                ${CMAKE_COMMAND} --build ${llvm-project_BINARY_DIR} -j
-                --config=${llvm_config}
-            WORKING_DIRECTORY ${llvm-project_SOURCE_DIR}
-            COMMAND_ERROR_IS_FATAL ANY
-        )
-        execute_process(
-            COMMAND
-                ${CMAKE_COMMAND} --install ${llvm-project_BINARY_DIR}
-                --config=${llvm_config}
-            WORKING_DIRECTORY ${llvm-project_SOURCE_DIR}
-            COMMAND_ERROR_IS_FATAL ANY
-        )
-
-        find_package(
-            LLVM
-            REQUIRED
-            CONFIG
-            PATHS ${llvm_install_root}
-            NO_DEFAULT_PATH
-        )
-        find_package(
-            Clang
-            REQUIRED
-            CONFIG
-            PATHS ${llvm_install_root}
-            NO_DEFAULT_PATH
+    #
+    # Some validation
+    #
+    if(CMAKE_CROSSCOMPILING)
+        message(
+            FATAL_ERROR
+            "Cross building LLVM isn't supported here, please enhance find_llvm() or use set SLANG_USE_SYSTEM_LLVM"
         )
     endif()
+    if (CMAKE_SYSTEM_NAME MATCHES "Windows" AND NOT CMAKE_BUILD_TYPE MATCHES "^Release.*")
+        message(
+            WARNING
+            "Building LLVM in a Release configuration for a debug or multi-config Slang build "
+            "will likely lead to problems linking a Debug slang-llvm on Windows, where incompatible "
+            "runtime libraries may be used.\n"
+            "SLANG_BUILD_LLVM is only intended to be used for release creation. On Windows it's "
+            "recommended to use SLANG_USE_BINARY_SLANG_LLVM and on other platforms it's "
+            "recommended to use SLANG_USE_SYSTEM_LLVM for local development. "
+        )
+    endif()
+
+    #
+    # Fetch LLVM
+    #
+    fetchcontent_declare(
+        llvm-project
+        URL ${llvm_url}
+        URL_HASH SHA256=${llvm_sha256}
+        PREFIX
+        llvm-project
+    )
+    fetchcontent_makeavailable(llvm-project)
+
+    #
+    # Configure, build and install a CMake project
+    #
+    BuildNestedCMakeProject(
+      SOURCE_DIR "${llvm-project_SOURCE_DIR}/llvm"
+      BINARY_DIR "${llvm-project_BINARY_DIR}"
+      BUILD_TYPE ${llvm_config}
+      INSTALL_PREFIX ${llvm_install_root}
+      EXTRA_CMAKE_ARGS
+        # Don't build unnecessary things
+        -DLLVM_BUILD_LLVM_C_DYLIB=0
+        -DLLVM_INCLUDE_BENCHMARKS=0
+        -DLLVM_INCLUDE_DOCS=0
+        -DLLVM_INCLUDE_EXAMPLES=0
+        -DLLVM_INCLUDE_TESTS=0
+        -DLLVM_ENABLE_TERMINFO=0
+        -DCLANG_BUILD_TOOLS=0
+        -DCLANG_ENABLE_STATIC_ANALYZER=0
+        -DCLANG_ENABLE_ARCMT=0
+        -DCLANG_INCLUDE_DOCS=0
+        -DCLANG_INCLUDE_TESTS=0
+        # Requirements for Slang
+        -DCMAKE_CXX_VISIBILITY_PRESET=hidden
+        -DLLVM_ENABLE_PROJECTS=clang
+        "-DLLVM_TARGETS_TO_BUILD=X86\\\;ARM\\\;AArch64"
+        -DLLVM_BUILD_TOOLS=1
+        "-DCMAKE_CXX_FLAGS=-ffunction-sections -fdata-sections -Wl,--gc-sections"
+    )
+
+    #
+    # Don't require a specific version here, whatever we've build is
+    # clearly what we want
+    #
+    find_package(
+        LLVM
+        REQUIRED
+        CONFIG
+        PATHS ${llvm_install_root}
+        NO_DEFAULT_PATH
+    )
+    find_package(
+        Clang
+        REQUIRED
+        CONFIG
+        PATHS ${llvm_install_root}
+        NO_DEFAULT_PATH
+    )
 endmacro()
 
 # A convenience on top of the llvm package's cmake files, this creates a target
---
+++ cmake/Nested.cmake
@@ -0,0 +1,70 @@
+#
+# Configure, build and install a CMake project,
+#
+# For example, LLVM, or Slang itself (for building native generators in a cross
+# build)
+#
+function(BuildNestedCMakeProject)
+    set(no_value_args)
+    set(single_value_args
+        # The directory in which to find the project's top level CMakeLists.txt
+        SOURCE_DIR
+        # A directory in which to build the project
+        BINARY_DIR
+        # The build type, Debug, Release etc..
+        BUILD_TYPE
+        # Where to place the finished project
+        INSTALL_PREFIX
+    )
+    set(multi_value_args
+        # Extra arguments to pass to the CMake configure stage
+        EXTRA_CMAKE_ARGS
+    )
+    cmake_parse_arguments(
+        ARG
+        "${no_value_args}"
+        "${single_value_args}"
+        "${multi_value_args}"
+        "${ARGN}"
+    )
+    set(nested_cmake_args
+        # General CMake settings, copy settings from the parent project, as
+        # they're going to be available
+        -G${CMAKE_GENERATOR}
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
+        -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+        -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
+        -DCMAKE_BUILD_TYPE=${ARG_BUILD_TYPE}
+        "${ARG_EXTRA_CMAKE_ARGS}"
+    )
+
+    # Configure, build and install a CMake project
+    execute_process(
+        COMMAND
+            ${CMAKE_COMMAND} 
+            -S ${ARG_SOURCE_DIR} 
+            -B ${ARG_BINARY_DIR} 
+            ${nested_cmake_args}
+        WORKING_DIRECTORY ${ARG_SOURCE_DIR}
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+    execute_process(
+        COMMAND
+            ${CMAKE_COMMAND} --build ${ARG_BINARY_DIR} -j
+            --config=${ARG_BUILD_TYPE}
+        WORKING_DIRECTORY ${ARG_SOURCE_DIR}
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+    execute_process(
+        COMMAND
+            ${CMAKE_COMMAND} 
+            --install ${ARG_BINARY_DIR}
+            --prefix ${ARG_INSTALL_PREFIX}
+            --config ${ARG_BUILD_TYPE}
+        WORKING_DIRECTORY ${ARG_SOURCE_DIR}
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endfunction()
