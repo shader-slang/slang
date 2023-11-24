3f76cb2ec019a0021115e1a52f5ad1b21a902ac2
Ellie Hermaszewska <ellieh@nvidia.com>
2023-11-24 16:19:05 +0000

    Correct logic around disabling problematic debug info for ccache

--- cmake/CCacheDebugInfoWorkaround.cmake
+++ cmake/CCacheDebugInfoWorkaround.cmake
@@ -1,7 +1,7 @@
 # Make sure that shared debug info doesn't intefere with caching
 # See the sccache readme
 if(MSVC
-    AND NOT DEFINED CMAKE_MSVC_DEBUG_INFORMATION_FORMAT
+    AND (NOT DEFINED CMAKE_MSVC_DEBUG_INFORMATION_FORMAT)
     AND (CMAKE_C_COMPILER_LAUNCHER MATCHES "ccache" OR CMAKE_CXX_COMPILER_LAUNCHER MATCHES "ccache")
 )
     message(NOTICE "Setting embedded debug info for MSVC to work around (s)ccache's inability to cache shared debug info files, Note that this requires CMake 3.25 or greater")
