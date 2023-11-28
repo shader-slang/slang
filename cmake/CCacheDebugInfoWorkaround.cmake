1cc98bda8c243306ca07cab7bbc540fcc858a919
Ellie Hermaszewska <ellieh@nvidia.com>
2023-11-28 19:43:01 +0800

    ci wobble

--- cmake/CCacheDebugInfoWorkaround.cmake
+++ cmake/CCacheDebugInfoWorkaround.cmake
@@ -1,17 +1,5 @@
 # Make sure that shared debug info doesn't intefere with caching
 # See the sccache readme
-message(NOTICE "MSVC: ${MSVC}")
-if(MSVC)
-    message(NOTICE "MSVC true")
-endif()
-if(DEFINED CMAKE_MSVC_DEBUG_INFORMATION_FORMAT)
-    message(NOTICE "CMAKE_MSVC_DEBUG_INFORMATION_FORMAT is defined")
-    message(NOTICE "CMAKE_MSVC_DEBUG_INFORMATION_FORMAT ${CMAKE_MSVC_DEBUG_INFORMATION_FORMAT}")
-endif()
-message(NOTICE "CMAKE_C_COMPILER_LAUNCHER: ${CMAKE_C_COMPILER_LAUNCHER}")
-message(NOTICE "CMAKE_CXX_COMPILER_LAUNCHER: ${CMAKE_CXX_COMPILER_LAUNCHER}")
-message(NOTICE "ENV:CMAKE_C_COMPILER_LAUNCHER: $ENV{CMAKE_C_COMPILER_LAUNCHER}")
-message(NOTICE "ENV:CMAKE_CXX_COMPILER_LAUNCHER: $ENV{CMAKE_CXX_COMPILER_LAUNCHER}")
 if(
     MSVC
     AND (NOT DEFINED CMAKE_MSVC_DEBUG_INFORMATION_FORMAT)
@@ -25,9 +13,11 @@ if(
         "Setting embedded debug info for MSVC to work around (s)ccache's inability to cache shared debug info files, Note that this requires CMake 3.25 or greater"
     )
     cmake_minimum_required(VERSION 3.25)
-    cmake_policy(SET CMP0141 NEW)
+    cmake_policy(GET CMP0141 cmp0141)
+    if(NOT cmp0141 STREQUAL "NEW")
+        message(WARNING "Need CMake policy 0141 enabled")
+    endif()
     set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT
         "$<$<CONFIG:Debug,RelWithDebInfo>:Embedded>"
     )
-    enable_language(C CXX)
 endif()
