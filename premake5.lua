81481ff7cc4f788bba768620353e0bca9deb848b
Ellie Hermaszewska <ellieh@nvidia.com>
2024-01-19 22:48:45 +0800

    use slang_add_target for capability generated targets

--- premake5.lua
+++ premake5.lua
@@ -1568,7 +1568,7 @@ if enableEmbedStdLib then
             "SLANG_WITHOUT_EMBEDDED_STD_LIB"
         }
 
-        includedirs { "external/spirv-headers/include" }
+        includedirs { "external/spirv-headers/include", "source" }
 
         -- Add all of the slang source
         addSourceDir "source/slang"
@@ -1686,7 +1686,7 @@ standardProject("slang", "source/slang")
         defines { "SLANG_WITHOUT_EMBEDDED_STD_LIB" }
     end
 
-    includedirs { "external/spirv-headers/include" }
+    includedirs { "external/spirv-headers/include", "source" }
 
     -- On some tests with MSBuild disabling these made build work.
     -- flags { "NoIncrementalLink", "NoPCH", "NoMinimalRebuild" }
