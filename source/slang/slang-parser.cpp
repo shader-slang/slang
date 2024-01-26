85d6b5e5942088b611f5bdffd555562bdd4f8285
Yong He <yhe@nvidia.com>
2024-01-26 15:14:22 -0800

    [LSP] Fix semantic highlighting of cbuffer types.

--- source/slang/slang-parser.cpp
+++ source/slang/slang-parser.cpp
@@ -3119,8 +3119,6 @@ namespace Slang
         // the second is a variable declaration that uses the buffer type.
         StructDecl* bufferDataTypeDecl = parser->astBuilder->create<StructDecl>();
 
-        addModifier(bufferDataTypeDecl, parser->astBuilder->create<PublicModifier>());
-
         VarDecl* bufferVarDecl = parser->astBuilder->create<VarDecl>();
 
         // Both declarations will have a location that points to the name
