4d3d4395ae37b2875b22c5ac1f7663421c5e2034
Simon Kallweit <simon.kallweit@gmail.com>
2024-02-25 23:25:13 +0100

    switch to vkCreateMetalSurfaceEXT and create metal layer in swapchain

--- tools/gfx/apple/cocoa-util.h
+++ tools/gfx/apple/cocoa-util.h
@@ -7,6 +7,9 @@ struct CocoaUtil {
 
     static void getNSViewRectSize(void* nsview, int* widthOut, int* heightOut);
 
+    static void* createMetalLayer(void* nswindow);
+    static void destroyMetalLayer(void* metalLayer);
+
 };
 
 }
--- tools/gfx/apple/cocoa-util.mm
+++ tools/gfx/apple/cocoa-util.mm
@@ -1,6 +1,7 @@
 #include "cocoa-util.h"
 
 #import <Cocoa/Cocoa.h>
+#import <QuartzCore/CAMetalLayer.h>
 
 namespace gfx {
 
@@ -10,6 +11,21 @@ void CocoaUtil::getNSViewRectSize(void* nsview, int* widthOut, int* heightOut)
     NSRect rect = [view frame];
     *widthOut = rect.size.width;
     *heightOut = rect.size.height;
+}
+
+void* CocoaUtil::createMetalLayer(void* nswindow)
+{
+    CAMetalLayer *layer = [CAMetalLayer layer];
+    NSWindow* window = (NSWindow*)nswindow;
+    window.contentView.layer = layer;
+    window.contentView.wantsLayer = YES;
+    return layer;
+}
+
+void CocoaUtil::destroyMetalLayer(void* metalLayer)
+{
+    CAMetalLayer* layer = (CAMetalLayer*)metalLayer;
+    [layer release];
 }
 
 }
