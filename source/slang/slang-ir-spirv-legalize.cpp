6f8a20688e0c4b989db152b4d06aeab04fac0567
Yong He <yonghe@outlook.com>
2024-03-08 15:13:08 -0800

    [SPIRV] fix code gen for `SV_Coverage`. (#3718)
    * [SPIRV] fix code gen for `SV_Coverage`.
    
    * Fix #3714, #3699.
    
    * Fix.

--- source/slang/slang-ir-spirv-legalize.cpp
+++ source/slang/slang-ir-spirv-legalize.cpp
@@ -1934,7 +1934,7 @@ struct SPIRVLegalizationContext : public SourceEmitterBase
                 // After legalizing the control flow, we need to sort our blocks to ensure this is true.
                 sortBlocksInFunc(func);
             }
-
+            
             if (isInlinableGlobalInst(globalInst))
             {
                 for (auto use = globalInst->firstUse; use; use = use->nextUse)
