b33bf37a5d778c76c45c0d1270832b391bc11a2e
kaizhangNV <kazhang@nvidia.com>
2024-04-04 13:30:36 -0700

    Categorize the unstorable and storable instructs
    1. When checking the loop regions, there could be multi-levels nested
       loops, so we should use a list to store the loopHeaders.
    
    2. Categorize the instructs based on storable and non-storable, because
       we only have to duplicate the non-storable instructs. Note pointer
       type instruct is also belonged to non-storable class because we can
       not store a pointer in local variable.

--- source/slang/slang-ir-variable-scope-correction.cpp
+++ source/slang/slang-ir-variable-scope-correction.cpp
@@ -1,5 +1,6 @@
 #include "slang-ir-insts.h"
 #include "slang-ir.h"
+#include "slang-ir-clone.h"
 
 #include "slang-ir-dominators.h"
 #include "slang-ir-variable-scope-correction.h"
@@ -20,6 +21,11 @@ struct VariableScopeCorrectionContext
 
     /// Process a function in the module
     void _processFunction(IRFunc* funcInst);
+    void _processInstructions(IRFunc* funcInst, const Dictionary<IRInst*, List<IRInst*>>& workListMap);
+    void _processStorableInst(IRInst* insertLoc, IRInst* inst, IRInst* user);
+    void _processUnstorableInst(IRInst* funcInst, IRInst* inst, IRInst* user);
+    void _replaceOperand(IRInst* inst, IRInst* oldOperand, IRInst* newOperand);
+    bool _isStorableInst(IRInst* inst);
 
     IRModule* m_module;
     IRBuilder m_builder;
@@ -33,87 +39,106 @@ void VariableScopeCorrectionContext::processModule()
         // We want to find all of the functions, and process them
         if (auto funcInst = as<IRFunc>(child))
         {
-            _processFunction(funcInst);
+            if (funcInst->getFirstBlock())
+            {
+                _processFunction(funcInst);
+            }
         }
     }
 }
 
-static void debugPrint(uint32_t indent, IRInst* inst, const char* message)
+static void debugPrint(uint32_t indent, IRInst* inst, const char* message, bool enable)
 {
+    if (!enable)
+        return;
+
     uint32_t debugid = 0;
     for (uint32_t i = 0; i < indent; i++)
     {
         printf("    ");
     }
+
 #ifdef SLANG_ENABLE_IR_BREAK_ALLOC
+    if (inst)
         debugid = inst->_debugUID;
 #endif
-    printf("%s, id: %d\n", message, debugid);
+
+    if (debugid != 0)
+        printf("%s, id: %d\n", message, debugid);
+    else
+        printf("\n");
 }
 
 void VariableScopeCorrectionContext::_processFunction(IRFunc* funcInst)
 {
     IRDominatorTree* dominatorTree = m_module->findOrCreateDominatorTree(funcInst);
     Dictionary<IRInst*, List<IRInst*>> workListMap;
-    Dictionary<IRBlock*, IRLoop> loopHeaderMap;
+    Dictionary<IRBlock*, List<IRLoop*>> loopHeaderMap;
 
+    bool enableLog = false;
     // traverse all blocks in the function
     for (auto block = funcInst->getFirstBlock(); block; block = block->getNextBlock())
     {
         uint32_t indent = 0;
-        debugPrint(indent++, block, "processing block");
+        debugPrint(indent++, block, "processing block", enableLog);
 
         // Traverse all the dominators of a given block to check whether this given block is in a loop region.
         // Loop region blocks are the blocks that are dominated by the loop header block
         // but not dominated by the loop break block.
         auto dominatorBlock = dominatorTree->getImmediateDominator(block);
+        List<IRLoop*> loopHeaderList;
         for (; dominatorBlock; dominatorBlock = dominatorTree->getImmediateDominator(dominatorBlock))
         {
-            debugPrint(indent++, dominatorBlock, "dominator block");
+            debugPrint(indent++, dominatorBlock, "dominator block", enableLog);
 
             // Find if the block is loop header block
             if (auto loopHeader = as<IRLoop>(dominatorBlock->getTerminator()))
             {
-                debugPrint(indent, loopHeader, "loop header");
+                debugPrint(indent, loopHeader, "loop header", enableLog);
                 // Get the break block of the loop and check if such block
                 auto breakBlock = loopHeader->getBreakBlock();
-                debugPrint(indent++, breakBlock, "break block");
+                debugPrint(indent++, breakBlock, "break block", enableLog);
 
                 // Check if the current block is dominated by the break block. If so, it means that the block is in the loop region.
                 if (!dominatorTree->dominates(breakBlock, block))
                 {
-                    debugPrint(indent++, block, "block is not dominated by break block");
-                    loopHeaderMap.add(block, *loopHeader);
+                    debugPrint(indent++, block, "block is not dominated by break block", enableLog);
+                    loopHeaderList.add(loopHeader);
                 }
             }
         }
+        loopHeaderMap.add(block, loopHeaderList);
     }
 
-    printf("\n");
+    debugPrint(0, nullptr, "", enableLog);
     // Traverse all the instructions in function.
     for (auto block = funcInst->getFirstBlock(); block; block = block->getNextBlock())
     {
-        if(auto loopHeader = loopHeaderMap.tryGetValue(block))
+        if(auto loopHeaderList = loopHeaderMap.tryGetValue(block))
         {
             for (auto inst = block->getFirstChild(); inst; inst = inst->getNextInst())
             {
                 List<IRInst*> instList;
-                uint32_t indent = 0;
-                auto breakBlock = loopHeader->getBreakBlock();
                 // traverse all uses of this instruction
+                debugPrint(0, getBlock(inst), "inst is defined in block", enableLog);
                 for (auto use = inst->firstUse; use; use=use->nextUse)
                 {
-                    debugPrint(indent++, getBlock(use->getUser()), "inst's use block is");
+                    uint32_t indent = 0;
+                    debugPrint(indent++, getBlock(use->getUser()), "inst's use block is", enableLog);
                     if (auto userBlock = getBlock(use->getUser()))
                     {
                         // If the use site of this instruction is dominated by the break block, it means that the
                         // instruction is used after the break block, so we need to make that instruction available globally.
                         // By doing so, we record all the users of this instructions.
-                        if (dominatorTree->dominates(breakBlock, userBlock))
+                        for(auto loopHeader : *loopHeaderList)
                         {
-                            debugPrint(indent, inst, "inst is defined in loop but used outside of loop");
-                            debugPrint(indent++, use->getUser(), "The user is");
-                            instList.add(use->getUser());
+                            auto breakBlock = loopHeader->getBreakBlock();
+                            if (dominatorTree->dominates(breakBlock, userBlock))
+                            {
+                                debugPrint(indent, block, "inst is defined in loop but used outside of loop", enableLog);
+                                instList.add(use->getUser());
+                                break;
+                            }
                         }
                     }
                 }
@@ -125,19 +150,149 @@ void VariableScopeCorrectionContext::_processFunction(IRFunc* funcInst)
         }
     }
 
-    printf("\n");
-    // By duplicating the instructions right before their users, we can make the instructions available globally.
+    debugPrint(0, nullptr, "", enableLog);
+    _processInstructions(funcInst, workListMap);
+}
+
+void VariableScopeCorrectionContext::_processInstructions(IRFunc* funcInst, const Dictionary<IRInst*, List<IRInst*>>& workListMap)
+{
+    bool enableLog = false;
+
+    // The first instruction in the function is the parameter list, so we need to skip it.
+    auto instAfterParam = funcInst->getFirstBlock()->getFirstChild();
+    while(instAfterParam->getOp() == kIROp_Param)
+    {
+        instAfterParam = instAfterParam->getNextInst();
+    }
+
     for(auto it = workListMap.begin(); it != workListMap.end(); it++)
     {
         auto inst = it->first;
         auto list = it->second;
-        debugPrint(0, inst, "inst");
+        debugPrint(0, inst, "inst", enableLog);
+        bool isStoable = _isStorableInst(inst);
+
         for(auto user : list)
         {
-            debugPrint(1, user, "The user");
-            inst->insertAt(IRInsertLoc::before(user));
+            debugPrint(1, user, "The user", enableLog);
+            if(isStoable)
+            {
+                _processStorableInst(instAfterParam, inst, user);
+            }
+            else
+            {
+                _processUnstorableInst(instAfterParam, inst, user);
+            }
         }
     }
+}
+
+// For unstorable instructions, we need to duplicate the instruction right before the use site.
+// For the operands of the instruction, if the operand is in the same block as the instruction,
+// we need to declare a new variable at the beginning of the function
+void VariableScopeCorrectionContext::_processUnstorableInst(IRInst* insertLoc, IRInst* inst, IRInst* user)
+{
+    IRCloneEnv cloneEnv;
+    m_builder.setInsertBefore(user);
+
+    // duplicate the invisible instruction and insert it right before the use site
+    auto clonedInst = cloneInst(&cloneEnv, &m_builder, inst);
+    clonedInst->insertAt(IRInsertLoc::before(user));
+
+    // take care the operands of the duplicated instruction because they could also be invisible at use site
+    uint32_t operandCount = inst->getOperandCount();
+    for (uint32_t i = 0; i < operandCount; i++)
+    {
+        auto operand = inst->getOperand(i);
+
+        // if the operand is the same block as the instruction, we need special handling for the operand
+        // because the operand is still not available in the block where the instruction is used.
+        if (getBlock(operand) == getBlock(inst))
+        {
+            // 1. Add a store instruction after this operand, e.g.: store(dstPtr, operand)
+            // 2. Declare the dstPtr at beginning of the function, so it's globally available
+            // 3. Replace the operand with the dstPtr
+            if (auto type = operand->getDataType())
+            {
+                m_builder.setInsertBefore(insertLoc);
+                auto dstPtr = m_builder.emitVar(type);
+
+                m_builder.setInsertAfter(operand);
+                m_builder.emitStore(dstPtr, operand);
+
+                clonedInst->setOperand(i, dstPtr);
+            }
+        }
+    }
+
+    // last, because the user is still referencing the original instruction, we need to
+    // replace operands in the use site instruction with the cloned instruction
+    _replaceOperand(user, inst, clonedInst);
+}
+
+// For storable instructions, we don't want to duplicate it at use site, so we just need to store the result after the instruction
+void VariableScopeCorrectionContext::_processStorableInst(IRInst* insertLoc, IRInst* inst, IRInst* user)
+{
+    auto type = inst->getDataType();
+    // store instruction must have a result type
+    SLANG_ASSERT(type);
+
+    // declare a new variable at the beginning of the function used to store the result of the instruction
+    m_builder.setInsertBefore(insertLoc);
+    auto dstPtr = m_builder.emitVar(type);
+
+    // insert a store instruction after the instruction
+    m_builder.setInsertAfter(inst);
+    m_builder.emitStore(dstPtr, inst);
+
+    // last, replace operands in the use site instruction with the new variable
+    _replaceOperand(user, inst,  dstPtr);
+}
+
+void  VariableScopeCorrectionContext::_replaceOperand(IRInst* inst, IRInst* oldOperand, IRInst* newOperand)
+{
+    // TODO: I'd like to use "user->replaceUsesWith(clonedInst);" here, but it seems not replace the operands at all.
+    uint32_t operandCount = inst->getOperandCount();
+
+    // Traverse all the operands to find out which one is the invisible instruction, and replace it with the new one.
+    for (uint32_t i = 0; i < operandCount; i++)
+    {
+        auto operand = inst->getOperand(i);
+
+        if (operand == oldOperand)
+        {
+            // We have to special handle the store instruction. Because in C++/CUDA, pointer is a valid type,
+            // "store(dstPtr, srcVal)" could generate "dstPtr = &srcVal" if we declare srcVal at beginning of the function
+            // by using emitVar(type). So we need to insert a load instruction before the store.
+            if (inst->getOp() == kIROp_Store && operand == inst->getOperand(1))
+            {
+                m_builder.setInsertBefore(inst);
+                auto loadVar = m_builder.emitLoad(newOperand->getDataType(), newOperand);
+                inst->setOperand(i, loadVar);
+            }
+            else
+            {
+                inst->setOperand(i, newOperand);
+            }
+        }
+    }
+}
+
+bool VariableScopeCorrectionContext::_isStorableInst(IRInst* inst)
+{
+    auto type = inst->getDataType();
+
+    // If the instruction has a result, and the result is not a void type, we consider it as a storable instruction
+    if (type)
+    {
+        // Take care of pointer type, because we can't store a pointer type, so pointer type is regarded as a non-storable instruction
+        if (type->getOp() == kIROp_PtrType)
+        {
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
 
 } // anonymous
