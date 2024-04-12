07cfdf56789eda4654cf86fb94a77fe4aadbfa9d
kaizhangNV <kazhang@nvidia.com>
2024-04-11 21:18:13 -0700

    Fix the lib compile issue in 'slangc'
    When using slangc, there is no way to specify container format,
    the only way is that slangc will infer from the extension name
    of the output file.
    
    We change that logic to set the container format as long as the output
    indicates the slang library.

--- source/slang/slang-options.cpp
+++ source/slang/slang-options.cpp
@@ -990,17 +990,11 @@ void OptionsParser::addOutputPath(char const* inPath)
         ext == toSlice("zip"))
     {
         // These extensions don't indicate a artifact container, just that we want to emit IR
-        if (ext == toSlice("slang-module") ||
-            ext == toSlice("slang-lib"))
-        {
-            // We want to emit IR 
-            m_requestImpl->m_emitIr = true;
-        }
-        else
-        {
-            // We want to write out in an artfact "container", that can hold multiple artifacts.
-            m_compileRequest->setOutputContainerFormat(SLANG_CONTAINER_FORMAT_SLANG_MODULE);
-        }
+        // We want to emit IR
+        m_requestImpl->m_emitIr = true;
+
+        // We want to write out in an artfact "container", that can hold multiple artifacts.
+        m_compileRequest->setOutputContainerFormat(SLANG_CONTAINER_FORMAT_SLANG_MODULE);
 
         m_requestImpl->m_containerOutputPath = path;
     }
