a5580ee160ba3f1e96914f24fbb4dc97de5dd9a8
Yong He <yonghe@outlook.com>
2024-04-19 15:44:53 -0700

    Add more comments.

--- source/compiler-core/slang-metal-compiler.cpp
+++ source/compiler-core/slang-metal-compiler.cpp
@@ -10,6 +10,10 @@ namespace Slang
     class MetalDownstreamCompiler : public DownstreamCompilerBase
     {
     public:
+        // Because the metal compiler shares the same commandline interface with clang,
+        // we will use GccDownstreamCompilerUtil, which implements both gcc and clang,
+        // to create the inner compiler and wrap it here.
+        //
         ComPtr<IDownstreamCompiler> cppCompiler;
         String executablePath;
 
@@ -24,16 +28,20 @@ namespace Slang
 
         virtual SLANG_NO_THROW SlangResult SLANG_MCALL compile(const CompileOptions& options, IArtifact** outArtifact)
         {
+            // All compile requests should be routed directly to the inner compiler.
             return cppCompiler->compile(options, outArtifact);
         }
 
         virtual SLANG_NO_THROW bool SLANG_MCALL canConvert(const ArtifactDesc& from, const ArtifactDesc& to) override
         {
+            // Report that we can convert Metal IR to disassembly.
             return ArtifactDescUtil::isDisassembly(from, to) && from.payload == ArtifactPayload::MetalAIR;
         }
 
         virtual SLANG_NO_THROW SlangResult SLANG_MCALL convert(IArtifact* from, const ArtifactDesc& to, IArtifact** outArtifact)
         {
+            // Use metal-objdump to disassemble the Metal IR.
+
             ExecutableLocation exeLocation(executablePath, "metal-objdump");
             CommandLine cmdLine;
             cmdLine.setExecutableLocation(exeLocation);
