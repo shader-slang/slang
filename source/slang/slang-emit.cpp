618428a87b8295347288262ea13eff63cc62aa56
Yong He <yonghe@outlook.com>
2024-05-06 14:53:27 -0700

    Delete `wrap-global-context` pass. (#4114)
    * Delete `wrap-global-context` pass.
    
    The pass was added for the metal backend without realizing that the existing `explicit-global-context` does 99% of the job. Instead of duplicating the logic in a different pass for metal, we extend same explicit-global-context pass to work for metal.
    
    * Fix build.

--- source/slang/slang-emit.cpp
+++ source/slang/slang-emit.cpp
@@ -70,7 +70,6 @@
 #include "slang-ir-synthesize-active-mask.h"
 #include "slang-ir-validate.h"
 #include "slang-ir-wrap-structured-buffers.h"
-#include "slang-ir-wrap-global-context.h"
 #include "slang-ir-liveness.h"
 #include "slang-ir-glsl-liveness.h"
 #include "slang-ir-translate-glsl-global-var.h"
@@ -886,9 +885,9 @@ Result linkAndOptimizeIR(
     case CodeGenTarget::GLSL:
     case CodeGenTarget::SPIRV:
     case CodeGenTarget::SPIRVAssembly:
-    case CodeGenTarget::Metal:
         moveGlobalVarInitializationToEntryPoints(irModule);
         break;
+    case CodeGenTarget::Metal:
     case CodeGenTarget::CPPSource:
     case CodeGenTarget::CUDASource:
         moveGlobalVarInitializationToEntryPoints(irModule);
@@ -1096,12 +1095,6 @@ Result linkAndOptimizeIR(
         applyVariableScopeCorrection(irModule, targetRequest);
         validateIRModuleIfEnabled(codeGenContext, irModule);
     }
-
-    // Metal does not allow global variables and global parameters, so
-    // we need to convert them into an explicit global context parameter
-    // passed around through a function parameter.
-    if (target == CodeGenTarget::Metal)
-        wrapGlobalScopeInContextType(irModule);
 
     auto metadata = new ArtifactPostEmitMetadata;
     outLinkedIR.metadata = metadata;
