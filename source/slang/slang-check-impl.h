f58a7bf1f94b3a3674cd96448823cd81fccc243a
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-05-16 19:04:58 -0400

    push attempt at language server fix

--- source/slang/slang-check-impl.h
+++ source/slang/slang-check-impl.h
@@ -645,19 +645,22 @@ namespace Slang
         template<typename InheritanceDeclBaseTypeToFilterOn>
         bool findInheritance(AggTypeDecl* targetToSearchThrough, Type* inheritanceToFilterOn)
         {
+            if (!targetToSearchThrough)
+                return false;
+
             for (auto member : targetToSearchThrough->getMembersOfType<InheritanceDecl>())
             {
                 if (!member->base || !member->base.type)
                     continue;
-                
                 if (member->base.type == inheritanceToFilterOn)
                     return true;
                 else
                 {
-                    if (auto baseDecl = as<InheritanceDeclBaseTypeToFilterOn>(as<DeclRefType>(member->base.type)->getDeclRef().getDecl()))
+                    auto baseDecl = as<InheritanceDeclBaseTypeToFilterOn>(as<DeclRefType>(member->base.type));
+
+                    if (findInheritance<InheritanceDeclBaseTypeToFilterOn>(baseDecl, inheritanceToFilterOn))
                     {
-                        if (findInheritance<InheritanceDeclBaseTypeToFilterOn>(baseDecl, inheritanceToFilterOn))
-                            return true;
+                        return true;
                     }
                 }
             }
