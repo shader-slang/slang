3e1c83ed12194d32c77a9d4c4892721ea06790c3
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-06-06 01:04:47 -0400

    test if `SLANG_ASSERT` really is causing a build issue for linux-arm

--- source/core/slang-uint-set.h
+++ source/core/slang-uint-set.h
@@ -22,8 +22,9 @@ constexpr Index intLog2(unsigned x)
 // if `in` is 0, result is undefined behavior
 static inline Index bitscanForward(uint64_t in)
 {
-    assert(in != 0);
+    SLANG_ASSERT(in != 0);
 #if defined(_MSC_VER)
+
 #ifdef _WIN64
     uint64_t out = 0;
     _BitScanForward64((unsigned long*)&out, in);
