1cc44374e952f0cc55bc969e0efb915a15a5c5e7
Simon Kallweit <skallweit@nvidia.com>
2024-06-06 09:31:55 +0200

    add adapter enumeration

--- tools/gfx/metal/metal-helper-functions.h
+++ tools/gfx/metal/metal-helper-functions.h
@@ -7,7 +7,7 @@ namespace gfx
 
 using namespace Slang;
 
-//Result SLANG_MCALL getMetalAdapters(List<AdapterInfo>& outAdapters);
+Result SLANG_MCALL getMetalAdapters(List<AdapterInfo>& outAdapters);
 Result SLANG_MCALL createMetalDevice(const IDevice::Desc* desc, IDevice** outRenderer);
 
 } // namespace gfx
