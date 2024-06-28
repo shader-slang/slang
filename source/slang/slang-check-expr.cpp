4bc8abb1f97de22a7f0f1898d6584297a63a4684
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-06-28 09:43:19 -0400

    early leave if already checked InitializerList term

--- source/slang/slang-check-expr.cpp
+++ source/slang/slang-check-expr.cpp
@@ -4176,6 +4176,10 @@ namespace Slang
 
     Expr* SemanticsExprVisitor::visitInitializerListExpr(InitializerListExpr* expr)
     {
+        // If we are assigned a type, expr has already been legalaized
+        if(expr->type)
+            return expr;
+        
         // When faced with an initializer list, we first just check the sub-expressions blindly.
         // Actually making them conform to a desired type will wait for when we know the desired
         // type based on context.
@@ -4185,8 +4189,7 @@ namespace Slang
             arg = CheckTerm(arg);
         }
 
-        if(!expr->type)
-            expr->type = m_astBuilder->getInitializerListType();
+        expr->type = m_astBuilder->getInitializerListType();
 
         return expr;
     }
