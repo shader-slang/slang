4cea07d8e6fe189db4d07b92645c17e965bcee78
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-07-16 09:51:40 -0400

    Allow CPP/CUDA/Metal to legalize their buffer-elements.
    Fixes: #4537
    
    Changes:
    1. Matrix inputs require legalization (pack/unpack) to ensure consistent row_major/column_major throughout entire shader, the following enabled legalization pass fixes this.
    2. Added missing CUDA intrinsic so CUDA can run more tests.
    3. Added a memory packing test since this still fails for cpp/cuda/metal (due to having no memory packing enforcement).

--- source/slang/slang-compiler.h
+++ source/slang/slang-compiler.h
@@ -1755,6 +1755,8 @@ namespace Slang
     /// Are we generating code for a CUDA API (CUDA / OptiX)?
     bool isCUDATarget(TargetRequest* targetReq);
 
+    // Are we generating code for a CPU target
+    bool isCPUTarget(TargetRequest* targetReq);
 
         /// A request to generate output in some target format.
     class TargetRequest : public RefObject
