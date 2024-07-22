0dbc788e044e461b7a854afaa806e5d658e987eb
kaizhangNV <kazhang@nvidia.com>
2024-07-22 11:11:28 -0700

    record/replay: name refactor change
    Refactor the naming.
    
    Change the name "encoder/capture" to "record".

--- tools/slang-replay/main.cpp
+++ tools/slang-replay/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <replay/captureFile-processor.h>
+#include <replay/recordFile-processor.h>
 #include <replay/decoder-consumer.h>
 
 int main(int argc, char *argv[])
 {
     // TODO: This is just a place holder binary
-    SlangCapture::CaptureFileProcessor captureFileProcessor("input.capture");
+    SlangRecord::RecordFileProcessor recordFileProcessor("input.capture");
     return 0;
 }
