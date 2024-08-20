a1d980b2658df9e241f735838839f88c1b57a628
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-08-20 16:34:02 -0400

    Exclude synthesized code from code auto documentation system
    Fixes #4888
    Changes to exclude synthesized code from code-auto-documentation system

--- source/slang/slang-doc-ast.cpp
+++ source/slang/slang-doc-ast.cpp
@@ -3,6 +3,7 @@
 
 #include "../core/slang-string-util.h"
 
+#include "slang/slang-ast-support-types.h"
 //#include "slang-ast-builder.h"
 //#include "slang-ast-print.h"
 
@@ -47,9 +48,14 @@ namespace Slang {
     }
 }
 
+bool doWeDocDecl(Decl* decl)
+{
+    return !getText(decl->getName()).startsWith("$__syn") && !decl->hasModifier<SynthesizedModifier>();
+}
+
 static void _addDeclRec(Decl* decl, List<Decl*>& outDecls)
 {
-    if (decl == nullptr)
+    if (decl == nullptr || !doWeDocDecl(decl))
     {
         return;
     }
@@ -109,7 +115,7 @@ SlangResult ASTMarkupUtil::extract(ModuleDecl* moduleDecl, SourceManager* source
             item.searchStyle = getSearchStyle(decl);
 
             // Don't generate documentation for synthesized members.
-            if (getText(decl->getName()).startsWith("$__syn"))
+            if (!doWeDocDecl(decl))
                 item.searchStyle = DocMarkupExtractor::SearchStyle::None;
         }
 
