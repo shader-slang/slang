ba467a5ff9ec6cbbe747549a325fd10b69eb648e
ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
2024-08-21 00:23:58 -0400

    reorganize code

--- source/slang/slang-legalize-types.h
+++ source/slang/slang-legalize-types.h
@@ -605,21 +605,6 @@ struct IRTypeLegalizationContext
     TargetProgram* targetProgram;
     IRBuilder builderStorage;
 
-    List<IRFunc*> entryPoints;
-    List<IRFunc*>& getEntryPoints()
-    {
-        if (entryPoints.getCount() == 0)
-        {
-            for (auto i : module->getGlobalInsts())
-            {
-                if (auto func = as<IRFunc>(i))
-                    if (i->findDecoration<IREntryPointDecoration>())
-                        entryPoints.add(func);
-            }
-        }
-        return entryPoints;
-    }
-
     IRTypeLegalizationContext(
         TargetProgram* target,
         IRModule* inModule);
@@ -663,6 +648,22 @@ struct IRTypeLegalizationContext
     };
 
     List<PointerValue> activePointerValues;
+
+    List<IRFunc*> entryPoints;
+
+    List<IRFunc*>& getEntryPoints()
+    {
+        if (entryPoints.getCount() == 0)
+        {
+            for (auto i : module->getGlobalInsts())
+            {
+                if (auto func = as<IRFunc>(i))
+                    if (i->findDecoration<IREntryPointDecoration>())
+                        entryPoints.add(func);
+            }
+        }
+        return entryPoints;
+    }
 
     IRBuilder* getBuilder() { return builder; }
 
