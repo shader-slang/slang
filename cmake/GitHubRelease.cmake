8d0229f6dd3e325a77d504b31dd2bd02c5cdd0a5
Ellie Hermaszewska <ellieh@nvidia.com>
2024-08-21 12:48:43 +0800

    Be a bit more careful dealing with release list fetching failure

--- cmake/GitHubRelease.cmake
+++ cmake/GitHubRelease.cmake
@@ -94,6 +94,7 @@ function(get_best_slang_binary_release_url out_var)
     set(repo "slang")
 
     check_release_and_get_latest(${owner} ${repo} ${SLANG_VERSION_NUMERIC} ${os} ${arch} release_version)
-
-    set(${out_var} "https://github.com/${owner}/${repo}/releases/download/v${release_version}/slang-${release_version}-${os}-${arch}.zip" PARENT_SCOPE)
+    if(DEFINED release_version)
+      set(${out_var} "https://github.com/${owner}/${repo}/releases/download/v${release_version}/slang-${release_version}-${os}-${arch}.zip" PARENT_SCOPE)
+    endif()
 endfunction()
