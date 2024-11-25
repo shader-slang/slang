d282701ba76e9883d2b7be39ee614fe3bb4f5165
Anders Leino <aleino@nvidia.com>
2024-11-25 16:05:13 +0200

    Support interpolation modifiers for WGSL (#5641)
    * wgsl: Support interpolation modifiers
    
    * Move struct key decorations to flattened structs.
    ** This includes interpolation mode decorations, which must be in the flattened struct.
    * Emit interpolation attribute.
    * Enable tests/render/nointerpolation.hlsl for WGSL, as a result.
    
    This closes #5625.
    
    * Add new expected output for 'nointerpolation' test

--- source/slang/slang-ir-wgsl-legalize.cpp
+++ source/slang/slang-ir-wgsl-legalize.cpp
@@ -629,7 +629,10 @@ struct LegalizeWGSLEntryPointContext
         // 2. If IRStructType:
         //  2a. Recurse this function with 'decorations that carry semantic info' from parent.
         // 3. If not IRStructType:
-        //  3a. Emit 'newField' equal to 'oldField', add 'decorations which carry semantic info'.
+        //  3a. Emit 'newField' with 'newKey' equal to 'oldField' and 'oldKey', respectively,
+        //      where 'oldKey' is the key corresponding to 'oldField'.
+        //      Add 'decorations which carry semantic info' to 'newField', and move all decorations
+        //      of 'oldKey' to 'newKey'.
         //  3b. Store a mapping from 'oldField' to 'newField' in 'mapFieldToField'. This info is
         //  needed to copy between types.
         for (auto oldField : src->getFields())
@@ -674,7 +677,7 @@ struct LegalizeWGSLEntryPointContext
 
             // step 3a
             auto newKey = builder.createStructKey();
-            copyNameHintAndDebugDecorations(newKey, oldKey);
+            oldKey->transferDecorationsTo(newKey);
 
             auto newField = builder.createStructField(dst, newKey, oldField->getFieldType());
             copyNameHintAndDebugDecorations(newField, oldField);
