eeb390a44214932b8d666dedb23ef175a0395cea
Anders Leino <aleino@nvidia.com>
2024-12-31 14:39:31 +0200

    examples: Print stdout file if there is any

--- .github/workflows/ci-examples.sh
+++ .github/workflows/ci-examples.sh
@@ -170,6 +170,9 @@ function run_sample {
   pushd "$bin_dir" 1>/dev/null 2>&1
   if [[ ! "$dry_run" = true ]]; then
     ./"$sample" "${args[@]}" || result=$?
+    if [[ -f ./"stdout-$sample.txt" ]]; then
+      cat ./"stdout-$sample.txt"
+    fi
   fi
   if [[ $result -eq 0 ]]; then
     summary=("${summary[@]}" "  success")
