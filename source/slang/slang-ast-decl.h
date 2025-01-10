11a4d39f6f1ad7666f909125a192983e4e95e84d
kaizhangNV <kazhang@nvidia.com>
2025-01-09 21:24:00 -0800

    Keep fixing test errors
    1. When extension of a struct defines __init, we should not synthesize
       __init anymore.
    
    2. For the synthesized __init signature, we correct the default value
       for the parameter. Figuring out what exactly is "default initialize
       type". If a type has a ctor can take 0 parameter, it's default
       initialize type.

--- source/slang/slang-ast-decl.h
+++ source/slang/slang-ast-decl.h
@@ -170,6 +170,7 @@ class StructDecl : public AggTypeDecl
     // We will use these auxiliary to help in synthesizing the member initialize constructor.
     Slang::HashSet<VarDeclBase*> m_membersVisibleInCtor;
     Dictionary<int, ConstructorDecl*> m_synthesizedCtorMap;
+    bool m_hasExplicitCtorInExtension = false;
 };
 
 class ClassDecl : public AggTypeDecl
