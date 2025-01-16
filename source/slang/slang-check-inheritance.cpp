36d6422dd62843ff68d2a2878531348198a999d9
Sai Praveen Bangaru <sbangaru@mit.edu>
2025-01-16 17:13:58 -0500

    Fix circularity issue with checking multiple generic interface constraints

--- source/slang/slang-check-inheritance.cpp
+++ source/slang/slang-check-inheritance.cpp
@@ -466,10 +466,12 @@ InheritanceInfo SharedSemanticsContext::_calcInheritanceInfo(
             if (constraintDeclRef.getDecl()->checkState.isBeingChecked())
                 continue;
 
-            ensureDecl(&visitor, constraintDeclRef.getDecl(), DeclCheckState::CanSpecializeGeneric);
+            ensureDecl(&visitor, constraintDeclRef.getDecl(), DeclCheckState::ScopesWired);
 
-            auto subType = getSub(astBuilder, constraintDeclRef);
-            auto superType = getSup(astBuilder, constraintDeclRef);
+            // Check only the sub-type.
+            visitor.CheckConstraintSubType(constraintDeclRef.getDecl()->sub);
+            auto sub = visitor.TranslateTypeNodeForced(constraintDeclRef.getDecl()->sub);
+            auto subType = constraintDeclRef.substitute(astBuilder, sub);
 
             // We only consider constraints where the type represented
             // by `declRef` is the subtype, since those
@@ -488,6 +490,11 @@ InheritanceInfo SharedSemanticsContext::_calcInheritanceInfo(
             }
             if (subDeclRefType->getDeclRef() != declRef)
                 continue;
+
+            // Further check the constraint, since we now need the sup-type.
+            ensureDecl(&visitor, constraintDeclRef.getDecl(), DeclCheckState::CanSpecializeGeneric);
+
+            auto superType = getSup(astBuilder, constraintDeclRef);
 
             // Because the constraint is a declared inheritance relationship,
             // adding the base to our list of direct bases is as straightforward
