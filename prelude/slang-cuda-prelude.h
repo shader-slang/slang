08cb560b7384820c86d2e9f352e92f989f09f8aa
Julius Ikkala <julius.ikkala@gmail.com>
2025-01-23 12:36:14 +0200

    Define intptr_t and uintptr_t with CUDACC_RTC

--- prelude/slang-cuda-prelude.h
+++ prelude/slang-cuda-prelude.h
@@ -205,11 +205,13 @@ typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
 typedef long long int64_t;
+typedef ptrdiff_t intptr_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
+typedef size_t uintptr_t;
 
 #endif
 
