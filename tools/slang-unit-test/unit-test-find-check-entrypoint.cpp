31548e1f236f9b5e4986eea25f33cc02aba54206
Anders Leino <aleino@nvidia.com>
2025-03-03 11:17:59 +0200

    Hack unit test to see the generated CUDA code

--- tools/slang-unit-test/unit-test-find-check-entrypoint.cpp
+++ tools/slang-unit-test/unit-test-find-check-entrypoint.cpp
@@ -18,10 +18,13 @@ SLANG_UNIT_TEST(findAndCheckEntryPoint)
 {
     // Source for a module that contains an undecorated entrypoint.
     const char* userSourceBody = R"(
-        float4 fragMain(float4 pos:SV_Position) : SV_Target
-        {
-            return pos;
-        }
+RWStructuredBuffer<float> outputBuffer;
+
+[numthreads(4, 1, 1)]
+void computeMain(uint3 dispatchThreadID : SV_DispatchThreadID)
+{
+    outputBuffer[dispatchThreadID.x] = float(dispatchThreadID.x);
+}
         )";
 
     auto moduleName = "moduleG" + String(Process::getId());
@@ -29,12 +32,23 @@ SLANG_UNIT_TEST(findAndCheckEntryPoint)
     ComPtr<slang::IGlobalSession> globalSession;
     SLANG_CHECK(slang_createGlobalSession(SLANG_API_VERSION, globalSession.writeRef()) == SLANG_OK);
     slang::TargetDesc targetDesc = {};
-    targetDesc.format = SLANG_CUDA_SOURCE;
-    targetDesc.profile = globalSession->findProfile("spirv_1_5");
+    targetDesc.format = SLANG_PTX;
     slang::SessionDesc sessionDesc = {};
     sessionDesc.targetCount = 1;
     sessionDesc.targets = &targetDesc;
     ComPtr<slang::ISession> session;
+    List<slang::CompilerOptionEntry> sessionOptionEntries;
+    {
+        slang::CompilerOptionEntry entry = {};
+        entry.name = slang::CompilerOptionName::DumpIntermediates;
+        entry.value.kind = slang::CompilerOptionValueKind::Int;
+        entry.value.intValue0 = int(false);
+        sessionOptionEntries.add(entry);
+    }
+    sessionDesc.compilerOptionEntries = sessionOptionEntries.getBuffer();
+    sessionDesc.compilerOptionEntryCount= sessionOptionEntries.getCount();
+    targetDesc.compilerOptionEntries = sessionOptionEntries.getBuffer();
+    targetDesc.compilerOptionEntryCount = sessionOptionEntries.getCount();
     SLANG_CHECK(globalSession->createSession(sessionDesc, session.writeRef()) == SLANG_OK);
 
     ComPtr<slang::IBlob> diagnosticBlob;
@@ -47,7 +61,7 @@ SLANG_UNIT_TEST(findAndCheckEntryPoint)
 
     ComPtr<slang::IEntryPoint> entryPoint;
     module->findAndCheckEntryPoint(
-        "fragMain",
+        "computeMain",
         SLANG_STAGE_COMPUTE,
         entryPoint.writeRef(),
         diagnosticBlob.writeRef());
@@ -67,7 +81,9 @@ SLANG_UNIT_TEST(findAndCheckEntryPoint)
     SLANG_CHECK(linkedProgram != nullptr);
 
     ComPtr<slang::IBlob> code;
-    linkedProgram->getEntryPointCode(0, 0, code.writeRef(), diagnosticBlob.writeRef());
+    auto res = linkedProgram->getEntryPointCode(0, 0, code.writeRef(), diagnosticBlob.writeRef());
+	if (res != SLANG_OK)
+		std::cout << "diagnostic: " << (char*)diagnosticBlob->getBufferPointer() << std::endl;
     SLANG_CHECK(code != nullptr);
     SLANG_CHECK(code->getBufferSize() != 0);
 
