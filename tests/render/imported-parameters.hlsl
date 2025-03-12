f4d5372d3354e62770b076b47892b5172223e98a
Anders Leino <aleino@nvidia.com>
2025-03-12 13:44:57 +0200

    Migrate render-test away from deprecated compile request API (#6514)
    * Add a simple interface parameter test
    
    Since there's no documentation, it's nice to have a simple test case in order to
    experiment with this feature of the testing framework.
    
    * Add shader entry point attributes to tests
    
    * Fix specialization arguments for tests
    
    - Add some missing arguments
    - Rremove one extraneous argument.
    
    * Stop using deprecated compile request in render-test
    
    Use a session object instead of the deprecated compile request object.
    This closes issue #4760.

--- tests/render/imported-parameters.hlsl
+++ tests/render/imported-parameters.hlsl
@@ -38,6 +38,7 @@ struct VertexStageOutput
 	float4			sv_position		: SV_Position;
 };
 
+[shader("vertex")]
 VertexStageOutput vertexMain(VertexStageInput input)
 {
 	VertexStageOutput output;
@@ -64,6 +65,7 @@ struct FragmentStageOutput
 	Fragment fragment	: SV_Target;
 };
 
+[shader("fragment")]
 FragmentStageOutput fragmentMain(FragmentStageInput input)
 {
 	FragmentStageOutput output;
