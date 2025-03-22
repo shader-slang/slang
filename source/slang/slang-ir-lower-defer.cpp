cdfeae277ffde7201fa1239fc60fdf79dda81a89
Julius Ikkala <julius.ikkala@gmail.com>
2025-03-22 13:45:15 +0200

    Fix defer order & multi-block defers

--- source/slang/slang-ir-lower-defer.cpp
+++ source/slang/slang-ir-lower-defer.cpp
@@ -20,6 +20,62 @@ struct DeferLoweringContext : InstPassBase
     {
     }
 
+    void inlineSingleBlockDefer(IRInst* terminator, IRBlock* deferBlock, IRBuilder* builder)
+    {
+        // Insert to the end of the block right before the terminator.
+        builder->setInsertBefore(terminator);
+
+        IRCloneEnv env;
+        for(IRInst* inst: deferBlock->getChildren())
+            cloneInst(&env, builder, inst);
+    }
+
+    void inlineDefer(IRInst* terminator, IRBlock* targetBlock, IRDefer* defer, IRBuilder* builder)
+    {
+        // The single-block inlining case is simple, we can just dump the
+        // instructions at the target position, in the existing block.
+        auto firstBlock = defer->getFirstBlock();
+        if (!firstBlock->getNextBlock())
+        {
+            inlineSingleBlockDefer(terminator, firstBlock, builder);
+            return;
+        }
+
+        // Otherwise, we'll have to splice the blocks in.
+        IRCloneEnv env;
+
+        auto lastBlock = targetBlock;
+
+        // Clone blocks first
+        for (auto block : defer->getBlocks())
+        {
+            auto clonedBlock = builder->createBlock();
+            clonedBlock->insertAfter(lastBlock);
+            env.mapOldValToNew[block] = clonedBlock;
+            lastBlock = clonedBlock;
+        }
+
+        // Then, clone instructions, but mapping old blocks to new blocks.
+        for (auto block : defer->getBlocks())
+        {
+            auto clonedBlock = env.mapOldValToNew.getValue(block);
+            builder->setInsertInto(clonedBlock);
+            for (auto inst : block->getChildren())
+            {
+                cloneInst(&env, builder, inst);
+            }
+        }
+
+        // Move old terminator to afterBlock
+        terminator->removeFromParent();
+        terminator->insertAtEnd(lastBlock);
+
+        // Make target block jump to the cloned blocks.
+        builder->setInsertInto(targetBlock);
+        auto mainBlock = as<IRBlock>(env.mapOldValToNew.getValue(firstBlock));
+        builder->emitBranch(mainBlock);
+    }
+
     void processModule()
     {
         List<IRDefer*> unhandledDefers;
@@ -30,13 +86,10 @@ struct DeferLoweringContext : InstPassBase
                 unhandledDefers.add(defer);
             });
 
+        IRBuilder builder(module);
+
         // Iterating over `defer` instructions in reverse order allows us to
         // expand them in the correct order, including nested `defer`s.
-        unhandledDefers.reverse();
-
-        IRBuilder builder(module);
-        IRCloneEnv env;
-
         while (unhandledDefers.getCount() != 0)
         {
             IRDefer* defer = unhandledDefers.getLast();
@@ -59,42 +112,35 @@ struct DeferLoweringContext : InstPassBase
             for (IRBlock* block : dominatedBlocksSet)
             {
                 auto terminator = block->getTerminator();
+                SLANG_ASSERT(terminator);
                 bool exits = false;
-                if (!terminator)
-                    exits = true;
-                else
+                switch(terminator->getOp())
                 {
-                    switch(terminator->getOp())
+                case kIROp_Return:
+                case kIROp_discard:
+                case kIROp_Throw:
+                    exits = true;
+                    break;
+                case kIROp_unconditionalBranch:
+                case kIROp_conditionalBranch:
+                    // Check through all Block operands and check that all
+                    // are dominated.
+                    for (UInt index = 0; index < terminator->getOperandCount(); ++index)
                     {
-                    case kIROp_Return:
-                    case kIROp_discard:
-                    case kIROp_Throw:
-                        exits = true;
-                        break;
-                    case kIROp_unconditionalBranch:
-                    case kIROp_conditionalBranch:
-                        // Check through all Block operands and check that all
-                        // are dominated.
-                        for (UInt index = 0; index < terminator->getOperandCount(); ++index)
+                        auto target = as<IRBlock>(terminator->getOperand(index));
+                        if (target && !dominatedBlocksSet.contains(target))
                         {
-                            auto target = as<IRBlock>(terminator->getOperand(index));
-                            if (target && !dominatedBlocksSet.contains(target))
-                            {
-                                exits = true;
-                            }
+                            exits = true;
                         }
-                        break;
-                    default:
-                        break;
                     }
+                    break;
+                default:
+                    break;
                 }
 
                 if (exits)
                 { // Duplicate child instructions to the end of this block.
-                    builder.setInsertBefore(terminator);
-                    auto childBlock = as<IRBlock>(defer->getFirstChild());
-                    for(IRInst* inst: childBlock->getChildren())
-                        cloneInst(&env, &builder, inst);
+                    inlineDefer(terminator, block, defer, &builder);
                 }
             }
 
