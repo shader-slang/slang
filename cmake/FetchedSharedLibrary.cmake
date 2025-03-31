c1a9f9a8a88e989ceb98fc17ddf8d62d1e5fd54f
Gangzheng Tong <gtong@nvidia.com>
2025-03-31 16:25:33 -0700

    Use GITHUB_TOKEN for fetching prebuilt
    This PR extends Commit c6b702c to use GITHUB_TOKEN if set for fetching
    prebuilt binaries.
    This allows webgpu-dawn and slang-tint to be downloaded for certain IPs
    where the github API rate is limited.
    
    Fixes #6689

--- cmake/FetchedSharedLibrary.cmake
+++ cmake/FetchedSharedLibrary.cmake
@@ -6,6 +6,7 @@ endif()
 
 # Helper function to download and extract an archive
 function(download_and_extract archive_name url)
+    cmake_parse_arguments(ARG "" "SLANG_GITHUB_TOKEN" "" ${ARGN})
     cmake_path(GET url FILENAME filename_with_ext)
     cmake_path(GET url STEM LAST_ONLY file_stem)
     set(archive_path "${CMAKE_CURRENT_BINARY_DIR}/${filename_with_ext}")
@@ -26,16 +27,29 @@ function(download_and_extract archive_name url)
                 "Using existing archive for ${archive_name}: ${archive_path}"
             )
         else()
-            message(STATUS "Fetching ${archive_name} from ${url}")
-            file(DOWNLOAD ${url} ${archive_path} STATUS status)
+            set(download_args
+                ${url}
+                ${archive_path}
+                STATUS
+                status
+            )
+            if (ARG_SLANG_GITHUB_TOKEN)
+                list(APPEND
+                     download_args
+                     HTTPHEADER
+                     "Authorization: token ${ARG_SLANG_GITHUB_TOKEN}")
+            endif()
+
+            file(DOWNLOAD ${download_args})
 
             list(GET status 0 status_code)
             list(GET status 1 status_string)
             if(NOT status_code EQUAL 0)
                 message(
                     WARNING
-                    "Failed to download ${archive_name} from ${url}: ${status_string}"
+                    "Failed to download ${archive_name} from ${url}: ${status_string} with status code ${status_code}"
                 )
+                message(WARNING "If API rate limit is exceeded, Github allows a higher limit when you use token. Try a cmake option -DSLANG_GITHUB_TOKEN=your_token_here")
                 return()
             endif()
         endif()
@@ -56,6 +70,7 @@ endfunction()
 # for the shared library to produce the install rule.
 function(copy_fetched_shared_library library_name url)
     cmake_parse_arguments(ARG "IGNORE_FAILURE" "" "" ${ARGN})
+    cmake_parse_arguments(ARG "" "SLANG_GITHUB_TOKEN" "" ${ARGN})
     if(ARG_IGNORE_FAILURE)
         set(error_type STATUS)
     else()
@@ -95,7 +110,8 @@ function(copy_fetched_shared_library library_name url)
         set(source_object "${url}")
     else()
         # Otherwise, download and extract from whatever URL we have
-        download_and_extract("${library_name}" "${url}")
+        download_and_extract("${library_name}" "${url}"
+                             SLANG_GITHUB_TOKEN ${ARG_SLANG_GITHUB_TOKEN})
         if(DEFINED ${library_name}_SOURCE_DIR)
             from_glob(${${library_name}_SOURCE_DIR})
         elseif(ARG_IGNORE_FAILURE)
