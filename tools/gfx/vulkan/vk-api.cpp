591affaf733ec82d7b38a7bf9c4d2f49a69a2c66
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-04-18 16:18:02 -0700

    Check the available VK extensions before using CoopVec APIs in GFX (#6849)
    * Check the available VK extensions before using CoopVec APIs in GFX
    
    * Remove a redundant request for cooperative vector extension for vk

--- tools/gfx/vulkan/vk-api.cpp
+++ tools/gfx/vulkan/vk-api.cpp
@@ -86,17 +86,6 @@ Slang::Result VulkanApi::initPhysicalDevice(VkPhysicalDevice physicalDevice)
     vkGetPhysicalDeviceFeatures(m_physicalDevice, &m_deviceFeatures);
     vkGetPhysicalDeviceMemoryProperties(m_physicalDevice, &m_deviceMemoryProperties);
 
-    if (vkGetPhysicalDeviceCooperativeVectorPropertiesNV)
-    {
-        uint32_t nProps = 0;
-        vkGetPhysicalDeviceCooperativeVectorPropertiesNV(m_physicalDevice, &nProps, nullptr);
-        m_cooperativeVectorProperties.setCount(nProps);
-        vkGetPhysicalDeviceCooperativeVectorPropertiesNV(
-            m_physicalDevice,
-            &nProps,
-            m_cooperativeVectorProperties.getBuffer());
-    }
-
     return SLANG_OK;
 }
 
