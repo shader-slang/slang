56bf59f2a48dc1eaf8293293781f01ffce127b93
Julius Ikkala <julius.ikkala@gmail.com>
2025-04-30 20:10:10 +0300

    Lower result types only after specialization

--- source/slang/slang-emit.cpp
+++ source/slang/slang-emit.cpp
@@ -890,10 +890,6 @@ Result linkAndOptimizeIR(
         break;
     }
 
-    // Lower `Result<T,E>` types into ordinary struct types.
-    if (requiredLoweringPassSet.resultType)
-        lowerResultType(irModule, sink);
-
 #if 0
     dumpIRIfEnabled(codeGenContext, irModule, "UNIONS DESUGARED");
 #endif
@@ -1034,6 +1030,12 @@ Result linkAndOptimizeIR(
         if (!changed)
             break;
     }
+
+    // Lower `Result<T,E>` types into ordinary struct types. This must happen
+    // after specialization, since otherwise incompatible copies of the lowered
+    // result structure are generated.
+    if (requiredLoweringPassSet.resultType)
+        lowerResultType(irModule, sink);
 
     // Report checkpointing information
     if (codeGenContext->shouldReportCheckpointIntermediates())
