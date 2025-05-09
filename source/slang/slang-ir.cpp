029672ee08f5ecb710e84cf1ccc625e826ff9a29
Julius Ikkala <julius.ikkala@gmail.com>
2025-05-10 01:50:14 +0300

    Fix various intptr_t issues by defining its width in `getIntTypeInfo` (#6786)
    * Define a bit size for the intptr types
    
    * Fix intptr_t sign
    
    * Extend intptr test to check for previously broken operations
    
    * Fix intptr vector test on CUDA
    
    * Handle intptr size in getAnyValueSize
    
    * Fix formatting
    
    * Try with __ARM_ARCH_ISA_64
    
    * On macs, int64_t != intptr_t
    
    Yikes
    
    * Move define to prelude header
    
    * Also check apple in host-prelude
    
    * Fix define location

--- source/slang/slang-ir.cpp
+++ source/slang/slang-ir.cpp
@@ -7769,6 +7769,12 @@ IntInfo getIntTypeInfo(const IRType* intType)
         return {32, false};
     case kIROp_UInt64Type:
         return {64, false};
+    case kIROp_UIntPtrType:
+#if SLANG_PTR_IS_32
+        return {32, false};
+#else
+        return {64, false};
+#endif
     case kIROp_Int8Type:
         return {8, true};
     case kIROp_Int16Type:
@@ -7777,9 +7783,12 @@ IntInfo getIntTypeInfo(const IRType* intType)
         return {32, true};
     case kIROp_Int64Type:
         return {64, true};
-
-    case kIROp_IntPtrType:  // target platform dependent
-    case kIROp_UIntPtrType: // target platform dependent
+    case kIROp_IntPtrType:
+#if SLANG_PTR_IS_32
+        return {32, true};
+#else
+        return {64, true};
+#endif
     default:
         SLANG_UNEXPECTED("Unhandled type passed to getIntTypeInfo");
     }
