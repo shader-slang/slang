74b506d9dd63cfa4db6e3f280fd2e080a07bab19
Julius Ikkala <julius.ikkala@gmail.com>
2025-05-10 00:17:04 +0300

    Fix define location

--- prelude/slang-cpp-prelude.h
+++ prelude/slang-cpp-prelude.h
@@ -16,18 +16,18 @@
 #define SLANG_PRELUDE_STD
 #endif
 
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#endif // SLANG_LLVM
+
 // Is intptr_t not equal to equal-width sized integer type?
 #if defined(__APPLE__)
 #define SLANG_INTPTR_TYPE_IS_DISTINCT 1
 #else
 #define SLANG_INTPTR_TYPE_IS_DISTINCT 0
 #endif
-
-#include <assert.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#endif // SLANG_LLVM
 
 #if defined(_MSC_VER)
 #define SLANG_PRELUDE_SHARED_LIB_EXPORT __declspec(dllexport)
