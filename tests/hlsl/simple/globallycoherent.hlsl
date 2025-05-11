8e1ac9e96e1273a507db60ac6e6cadabd41b4c8a
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-05-11 00:32:41 -0700

    Add tests for vk_mem_model

--- tests/hlsl/simple/globallycoherent.hlsl
+++ tests/hlsl/simple/globallycoherent.hlsl
@@ -1,6 +1,14 @@
 //TEST:COMPARE_HLSL:-profile cs_5_0
+//TEST:SIMPLE(filecheck=SPIRV_GL_MEM_MODEL): -target spirv-asm
+//TEST:SIMPLE(filecheck=SPIRV_VK_MEM_MODEL): -target spirv-asm -emit-spirv-directly -capability vk_mem_model
 
 // Check output for `globallycoherent`
+
+//SPIRV_GL_MEM_MODEL: OpDecorate {{.*}} Coherent
+
+//SPIRV_VK_MEM_MODEL-NOT: OpDecorate {{.*}} Coherent
+//SPIRV_VK_MEM_MODEL: OpLoad {{.*}} MakePointerVisible|NonPrivatePointer
+//SPIRV_VK_MEM_MODEL: OpStore {{.*}} MakePointerAvailable|NonPrivatePointer
 
 #ifndef __SLANG__
 #define gBuffer gBuffer_0
