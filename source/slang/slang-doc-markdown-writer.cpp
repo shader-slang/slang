db64088e5487578b3098dfbf12e97fc1cb55ae38
Aidan Foster <aidanf@nvidia.com>
2025-05-13 12:54:22 -0700

    Do not escape markdown between backticks

--- source/slang/slang-doc-markdown-writer.cpp
+++ source/slang/slang-doc-markdown-writer.cpp
@@ -2036,7 +2036,7 @@ void DocMarkdownWriter::writeAggType(
                 ASTPrinter printer(m_astBuilder);
                 printer.addDeclPath(aggTypeDecl->getDefaultDeclRef());
                 out << "`" << printer.getString() << "` additionally conforms to `";
-                out << escapeMarkdownText(inheritanceDecl->base.type->toString());
+                out << inheritanceDecl->base.type->toString();
                 if (nonEmptyLines.getCount() != 0)
                 {
                     out << "` when the following conditions are met:\n\n";
