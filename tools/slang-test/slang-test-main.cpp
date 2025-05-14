03be954d1277eaddc82c270ef1da2d0e44f8e4ec
slangbot <186143334+slangbot@users.noreply.github.com>
2025-05-14 05:20:22 +0000

    format code

--- tools/slang-test/slang-test-main.cpp
+++ tools/slang-test/slang-test-main.cpp
@@ -5077,7 +5077,11 @@ SlangResult innerMain(int argc, char** argv)
                     testOptions.categories.add(unitTestCategory);
                     testOptions.categories.add(smokeTestCategory);
                     // Use default spawn type for unit tests as the test server one is unstable
-                    runUnitTestModule(&context, testOptions, SpawnType::Default, "slang-unit-test-tool");
+                    runUnitTestModule(
+                        &context,
+                        testOptions,
+                        SpawnType::Default,
+                        "slang-unit-test-tool");
                 }
 
                 {
