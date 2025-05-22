8fd6dfa6cda8ca13d490a6cd3553afc938d9216c
Lujin Wang <luwang@nvidia.com>
2025-05-21 21:37:04 -0700

    Fix lua header file path
    Add two missed files in #7167

--- tools/slang-fiddle/slang-fiddle-script.h
+++ tools/slang-fiddle/slang-fiddle-script.h
@@ -1,8 +1,8 @@
 // slang-fiddle-script.h
 #pragma once
 
-#include "../external/lua/lapi.h"
-#include "../external/lua/lauxlib.h"
+#include "lapi.h"
+#include "lauxlib.h"
 #include "compiler-core/slang-source-loc.h"
 #include "core/slang-list.h"
 #include "core/slang-string.h"
