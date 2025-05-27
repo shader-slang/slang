f570e109c4039e15526af38e17f350c115327489
Mukund Keshava <mkeshava@nvidia.com>
2025-05-27 17:46:04 +0530

    Add LSS intrinsics (#7200)
    * WiP: LSS intrinsics: initial commit
    
    * format code
    
    * Fix CI failures
    
    * Address review comment
    
    ---------
    
    Co-authored-by: slangbot <186143334+slangbot@users.noreply.github.com>

--- prelude/slang-cuda-prelude.h
+++ prelude/slang-cuda-prelude.h
@@ -31,6 +31,7 @@
 
 #ifdef SLANG_CUDA_ENABLE_OPTIX
 #include <optix.h>
+#include <optix_device.h>
 #endif
 
 // Define slang offsetof implementation
@@ -3219,6 +3220,54 @@ __forceinline__ __device__ void* optixTrace(
         MissShaderIndex,
         r0,
         r1);
+}
+
+__forceinline__ __device__ float4 optixGetSpherePositionAndRadius()
+{
+    float4 data[1];
+    optixGetSphereData(data);
+    return data;
+}
+
+__forceinline__ __device__ float4 optixHitObjectGetSpherePositionAndRadius()
+{
+    float4 data[1];
+    optixHitObjectGetSphereData(data);
+    return data;
+}
+
+__forceinline__ __device__ Matrix<float, 2, 4> optixGetSpherePositionAndRadius()
+{
+    float4 data[2];
+    optixGetLinearCurveVertexData(data);
+    return Matrix<float, 2, 4>(data[0], data[1]);
+}
+
+__forceinline__ __device__ float2x4 optixHitObjectGetSpherePositionAndRadius()
+{
+    float4 data[2];
+    optixHitObjectGetLinearCurveVertexData(data);
+    return Matrix<float, 2, 4>(data[0], data[1]);
+}
+
+__forceinline__ __device__ bool optixIsSphereHit()
+{
+    return optixGetPrimitiveType() == OPTIX_PRIMITIVE_TYPE_SPHERE;
+}
+
+__forceinline__ __device__ bool optixHitObjectIsSphereHit()
+{
+    return optixGetPrimitiveType(optixHitObjectGetHitKind()) == OPTIX_PRIMITIVE_TYPE_SPHERE;
+}
+
+__forceinline__ __device__ bool optixIsLSSHit()
+{
+    return optixGetPrimitiveType() == OPTIX_PRIMITIVE_TYPE_ROUND_LINEAR;
+}
+
+__forceinline__ __device__ bool optixHitObjectIsLSSHit()
+{
+    return optixGetPrimitiveType(optixHitObjectGetHitKind()) == OPTIX_PRIMITIVE_TYPE_ROUND_LINEAR;
 }
 
 template<typename T>
