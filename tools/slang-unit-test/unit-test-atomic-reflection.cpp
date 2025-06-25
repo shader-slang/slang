76d1e8b4b72f4636be2b0e1d960742c32f9a7695
James Helferty <jhelferty@nvidia.com>
2025-06-25 16:51:14 -0400

    Clean up unit test a bit
    - rename bar to buf (since one of them is not Atomic and thus not a
      barrier)
    - validate deeper into the fields of bufC
    - remove debug code

--- tools/slang-unit-test/unit-test-atomic-reflection.cpp
+++ tools/slang-unit-test/unit-test-atomic-reflection.cpp
@@ -14,14 +14,14 @@ using namespace Slang;
 SLANG_UNIT_TEST(atomicReflection)
 {
     const char* userSourceBody = R"(
-        RWStructuredBuffer<Atomic<uint>> barA;
-        RWStructuredBuffer<uint> barB;
+        RWStructuredBuffer<uint> bufA;  // for reference
+        RWStructuredBuffer<Atomic<uint>> bufB;
         struct TestStruct {
             Atomic<uint> fieldA;
             Atomic<uint> fieldB[2];
             vector<Atomic<uint>, 2> fieldC;
         };
-        RWStructuredBuffer<TestStruct> barC;
+        RWStructuredBuffer<TestStruct> bufC;
 
         [shader("compute")]
         [numthreads(64, 1, 1)]
@@ -51,56 +51,62 @@ SLANG_UNIT_TEST(atomicReflection)
 
     auto reflection = module->getLayout();
 
-    auto barAVariableLayout = reflection->getParameterByIndex(0);
-    auto barBVariableLayout = reflection->getParameterByIndex(1);
-    auto barCVariableLayout = reflection->getParameterByIndex(2);
+    auto bufAVariableLayout = reflection->getParameterByIndex(0);
+    auto bufBVariableLayout = reflection->getParameterByIndex(1);
+    auto bufCVariableLayout = reflection->getParameterByIndex(2);
 
-    SLANG_CHECK(barAVariableLayout != nullptr);
-    SLANG_CHECK(barBVariableLayout != nullptr);
-    SLANG_CHECK(barCVariableLayout != nullptr);
+    SLANG_CHECK(bufAVariableLayout != nullptr);
+    SLANG_CHECK(bufBVariableLayout != nullptr);
+    SLANG_CHECK(bufCVariableLayout != nullptr);
 
-    auto barAVariable = barAVariableLayout->getVariable();
-    auto barBVariable = barBVariableLayout->getVariable();
-    auto barCVariable = barCVariableLayout->getVariable();
+    // This test walks down the parameters in a different way from the JSON
+    // option. The slangc option "-reflection-json" walks:
+    //   param->getTypeLayout()->getElementTypeLayout()->getType()->getKind()
+    // This test uses an alternate walk:
+    //   param->getVariable()->getType()->getElementType()->getKind()
 
-    SLANG_CHECK(barAVariable != nullptr);
-    SLANG_CHECK(barBVariable != nullptr);
-    SLANG_CHECK(barCVariable != nullptr);
+    auto bufAVariable = bufAVariableLayout->getVariable();
+    auto bufBVariable = bufBVariableLayout->getVariable();
+    auto bufCVariable = bufCVariableLayout->getVariable();
 
-    auto barAType = barAVariable->getType();
-    auto barBType = barBVariable->getType();
-    auto barCType = barCVariable->getType();
+    SLANG_CHECK(bufAVariable != nullptr);
+    SLANG_CHECK(bufBVariable != nullptr);
+    SLANG_CHECK(bufCVariable != nullptr);
 
-    SLANG_CHECK(barAType->getKind() == slang::TypeReflection::Kind::Resource);
-    SLANG_CHECK(barBType->getKind() == slang::TypeReflection::Kind::Resource);
-    SLANG_CHECK(barCType->getKind() == slang::TypeReflection::Kind::Resource);
+    auto bufAType = bufAVariable->getType();
+    auto bufBType = bufBVariable->getType();
+    auto bufCType = bufCVariable->getType();
 
-    auto barAElementType = barAType->getElementType();
-    auto barBElementType = barBType->getElementType();
-    auto barCElementType = barCType->getElementType();
+    SLANG_CHECK(bufAType->getKind() == slang::TypeReflection::Kind::Resource);
+    SLANG_CHECK(bufBType->getKind() == slang::TypeReflection::Kind::Resource);
+    SLANG_CHECK(bufCType->getKind() == slang::TypeReflection::Kind::Resource);
 
-    SLANG_CHECK(barAElementType->getKind() == slang::TypeReflection::Kind::Scalar);
-    SLANG_CHECK(barBElementType->getKind() == slang::TypeReflection::Kind::Scalar);
-    SLANG_CHECK(barAElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
-    SLANG_CHECK(barBElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
+    auto bufAElementType = bufAType->getElementType();
+    auto bufBElementType = bufBType->getElementType();
+    auto bufCElementType = bufCType->getElementType();
 
-    auto barAResourceResultType = barAType->getResourceResultType();
-    auto barBResourceResultType = barBType->getResourceResultType();
+    SLANG_CHECK(bufAElementType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(bufBElementType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(bufAElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
+    SLANG_CHECK(bufBElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
 
-    SLANG_CHECK(barAResourceResultType->getKind() == slang::TypeReflection::Kind::Scalar);
-    SLANG_CHECK(barBResourceResultType->getKind() == slang::TypeReflection::Kind::Scalar);
-    SLANG_CHECK(barAResourceResultType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
-    SLANG_CHECK(barBResourceResultType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
+    auto bufAResResultType = bufAType->getResourceResultType();
+    auto bufBResResultType = bufBType->getResourceResultType();
+
+    SLANG_CHECK(bufAResResultType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(bufBResResultType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(bufAResResultType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
+    SLANG_CHECK(bufBResResultType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
 
     // Atomics embedded in structs require traversing the fields
-    SLANG_CHECK(barCElementType->getKind() == slang::TypeReflection::Kind::Struct);
+    SLANG_CHECK(bufCElementType->getKind() == slang::TypeReflection::Kind::Struct);
 
-    auto barCFieldCount = barCElementType->getFieldCount();
-    SLANG_CHECK(barCFieldCount == 3);
+    auto bufCFieldCount = bufCElementType->getFieldCount();
+    SLANG_CHECK(bufCFieldCount == 3);
 
-    auto fieldA = barCElementType->getFieldByIndex(0);
-    auto fieldB = barCElementType->getFieldByIndex(1);
-    auto fieldC = barCElementType->getFieldByIndex(2);
+    auto fieldA = bufCElementType->getFieldByIndex(0);
+    auto fieldB = bufCElementType->getFieldByIndex(1);
+    auto fieldC = bufCElementType->getFieldByIndex(2);
 
     SLANG_CHECK(fieldA != nullptr);
     SLANG_CHECK(fieldB != nullptr);
@@ -110,12 +116,20 @@ SLANG_UNIT_TEST(atomicReflection)
     auto fieldBType = fieldB->getType();
     auto fieldCType = fieldC->getType();
 
-    std::cout << "fieldAType: " << (int) fieldAType->getKind() << std::endl;
-    std::cout << "fieldBType: " << (int) fieldBType->getKind() << std::endl;
-    std::cout << "fieldCType: " << (int) fieldCType->getKind() << std::endl;
-
     SLANG_CHECK(fieldAType->getKind() == slang::TypeReflection::Kind::Scalar);
-    SLANG_CHECK(fieldBType->getKind() == slang::TypeReflection::Kind::Array);
-    SLANG_CHECK(fieldCType->getKind() == slang::TypeReflection::Kind::Vector);
+    SLANG_CHECK(fieldAType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
 
+    SLANG_CHECK(fieldBType->getKind() == slang::TypeReflection::Kind::Array);
+    SLANG_CHECK(fieldBType->getElementCount() == 2);
+    auto fieldBElementType = fieldBType->getElementType();
+    SLANG_CHECK(fieldBElementType != nullptr);
+    SLANG_CHECK(fieldBElementType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(fieldBElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
+
+    SLANG_CHECK(fieldCType->getKind() == slang::TypeReflection::Kind::Vector);
+    SLANG_CHECK(fieldCType->getElementCount() == 2);
+    auto fieldCElementType = fieldCType->getElementType();
+    SLANG_CHECK(fieldCElementType != nullptr);
+    SLANG_CHECK(fieldCElementType->getKind() == slang::TypeReflection::Kind::Scalar);
+    SLANG_CHECK(fieldCElementType->getScalarType() == slang::TypeReflection::ScalarType::UInt32);
 }
