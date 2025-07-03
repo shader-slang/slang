63d721390447437d4d251d5bb09d3c8f50845e92
slangbot <186143334+slangbot@users.noreply.github.com>
2025-07-03 00:33:41 +0000

    format code

--- source/slang/slang-emit-metal.cpp
+++ source/slang/slang-emit-metal.cpp
@@ -138,8 +138,8 @@ void MetalSourceEmitter::_emitHLSLTextureType(IRTextureTypeBase* texType)
     {
     case SLANG_RESOURCE_ACCESS_READ:
         {
-            // Metal does not support access::sample for texture buffers and multisampled textures, so we need to emit
-            // access::read instead.
+            // Metal does not support access::sample for texture buffers and multisampled textures,
+            // so we need to emit access::read instead.
             if (texType->GetBaseShape() == SLANG_TEXTURE_BUFFER || texType->isMultisample())
                 m_writer->emit("access::read");
             else
