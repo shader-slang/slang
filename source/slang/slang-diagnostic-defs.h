fe038b751b9bd046842954c006ceccaa880a94a9
slangbot <ellieh+slangbot@nvidia.com>
2025-07-16 04:56:30 +0800

    format code (#7772)
    Co-authored-by: slangbot <186143334+slangbot@users.noreply.github.com>

--- source/slang/slang-diagnostic-defs.h
+++ source/slang/slang-diagnostic-defs.h
@@ -700,7 +700,11 @@ DIAGNOSTIC(
     Error,
     argumentExpectedLValue,
     "argument passed to parameter '$0' must be l-value.")
-DIAGNOSTIC(30078, Error, cannotTakeConstantPointers, "Not allowed to take pointer of an immutable object")
+DIAGNOSTIC(
+    30078,
+    Error,
+    cannotTakeConstantPointers,
+    "Not allowed to take pointer of an immutable object")
 DIAGNOSTIC(
     30048,
     Error,
