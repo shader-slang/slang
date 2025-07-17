ab5a815297e57f579b15023cd2ebe97db6bd33eb
Dennis Brakhane <brakhane@gmail.com>
2025-07-17 13:13:13 +0200

    fix typo (#7794)
    Co-authored-by: Ellie Hermaszewska <ellieh@nvidia.com>
    Co-authored-by: Yong He <yonghe@outlook.com>

--- prelude/slang-cpp-prelude.h
+++ prelude/slang-cpp-prelude.h
@@ -1,7 +1,7 @@
 #ifndef SLANG_CPP_PRELUDE_H
 #define SLANG_CPP_PRELUDE_H
 
-// Because the signiture of isnan, isfinite, and is isinf changed in C++, we use the macro
+// Because the signature of isnan, isfinite, and is isinf changed in C++, we use the macro
 // to use the version in the std namespace.
 // https://stackoverflow.com/questions/39130040/cmath-hides-isnan-in-math-h-in-c14-c11
 
