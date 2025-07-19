88c557d8ef3278efe17b8dca8dbb630ff0631220
slangbot <ellieh+slangbot@nvidia.com>
2025-07-19 11:40:58 +0800

    format code (#77)
    Co-authored-by: slangbot <186143334+slangbot@users.noreply.github.com>

--- examples/example-base/test-base.cpp
+++ examples/example-base/test-base.cpp
@@ -219,8 +219,8 @@ void TestBase::printUsage(const char* programName) const
 #else
     printOutput("                                Use a given rendering API (Default: vulkan)\n");
 #endif
-    printOutput(
-        " -test-mode                     Print hash values of compiled shader entry points and skip rendering\n");
+    printOutput(" -test-mode                     Print hash values of compiled shader entry points "
+                "and skip rendering\n");
     printOutput("\n");
     printOutput("Supported APIs:\n");
 #ifdef _WIN32
