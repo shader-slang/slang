aa4e12735157c60f6094a0fb69a423b1d7badedd
Sam Estep <sam@samestep.com>
2025-07-23 14:44:28 -0400

    Fix clangd

--- flake.nix
+++ flake.nix
@@ -16,15 +16,20 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        # For debugging with LLDB, Clang seems to behave better than GCC about
-        # respecting breakpoint locations and not having missing variables.
-        devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+        # We want to use Clang instead of GCC because it seems to behave better
+        # with LLDB, so we use `mkShellNoCC` here instead of `mkShell` because
+        # the latter brings in GCC by default on Linux.
+        devShell = pkgs.mkShellNoCC {
           buildInputs = [
+            # Must put `clang-tools` before `clang` for clangd to work properly.
+            # We use `llvmPackages_17.clang-tools` instead of just `clang-tools`
+            # to match the `clang-format` version used in CI.
+            pkgs.llvmPackages_17.clang-tools
+
+            pkgs.clang
             pkgs.cmake
             pkgs.gersemi
             pkgs.lldb
-            pkgs.llvm
-            pkgs.llvmPackages_17.clang-tools
             pkgs.ninja
             pkgs.nixfmt-rfc-style
             pkgs.prettier
