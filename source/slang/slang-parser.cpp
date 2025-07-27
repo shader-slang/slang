219c8b86710469704f8ce487a35d981b9e8a59f2
Nathan V. Morrical <natemorrical@gmail.com>
2025-07-27 16:54:42 -0700

    Reverting copilot change in slang-parser.cpp

--- source/slang/slang-parser.cpp
+++ source/slang/slang-parser.cpp
@@ -1359,23 +1359,6 @@ static NodeBase* parseStaticAssertDecl(Parser* parser, void* /*userData*/)
     return decl;
 }
 
-// Add a special parser for compound static_assert when it appears as two tokens
-static NodeBase* parseStaticKeyword(Parser* parser, void* /*userData*/)
-{
-    // Check if we have "static" followed by "assert" to handle static_assert compound keyword
-    if (parser->LookAheadToken(TokenType::Identifier, 1) && 
-        parser->LookAheadToken("assert", 1))
-    {
-        // Consume both tokens and parse as static_assert
-        parser->ReadToken("static");
-        parser->ReadToken("assert");
-        return parseStaticAssertDecl(parser, userData);
-    }
-    
-    // Otherwise, fall back to regular static parsing (this will likely cause an error)
-    return nullptr;
-}
-
 static NodeBase* parseModuleDeclarationDecl(Parser* parser, void* /*userData*/)
 {
     auto decl = parser->astBuilder->create<ModuleDeclarationDecl>();
