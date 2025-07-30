bb78bdffd3caed0de411fc8b30d114894f1bc784
Yong He <yonghe@outlook.com>
2025-07-30 15:42:23 -0700

    Fix.

--- source/slang/slang-ir-glsl-legalize.cpp
+++ source/slang/slang-ir-glsl-legalize.cpp
@@ -2561,6 +2561,7 @@ static void consolidateParameters(GLSLLegalizationContext* context, List<IRParam
 
         // Replace parameter uses with field address
         _param->replaceUsesWith(fieldAddr);
+        _param->removeAndDeallocate();
     }
 }
 
@@ -2578,6 +2579,9 @@ void consolidateRayTracingParameters(GLSLLegalizationContext* context, IRFunc* f
 
     for (auto param = firstBlock->getFirstParam(); param; param = param->getNextParam())
     {
+        auto paramLayout = findVarLayout(param);
+        if (!isVaryingParameter(paramLayout))
+            continue;
         builder->setInsertBefore(firstBlock->getFirstOrdinaryInst());
         if (as<IROutType>(param->getDataType()) || as<IRInOutType>(param->getDataType()))
         {
@@ -2592,12 +2596,6 @@ void consolidateRayTracingParameters(GLSLLegalizationContext* context, IRFunc* f
         for (auto param : params)
         {
             auto paramLayoutDecoration = param->findDecoration<IRLayoutDecoration>();
-            if (!paramLayoutDecoration)
-            {
-                // Some parameters (like ParameterBlock types) may not have layout decorations
-                // in ray tracing shaders, so we skip them here.
-                continue;
-            }
             auto paramLayout = as<IRVarLayout>(paramLayoutDecoration->getLayout());
             handleSingleParam(context, func, param, paramLayout);
         }
@@ -2613,12 +2611,6 @@ void consolidateRayTracingParameters(GLSLLegalizationContext* context, IRFunc* f
                 continue;
             }
             auto paramLayoutDecoration = param->findDecoration<IRLayoutDecoration>();
-            if (!paramLayoutDecoration)
-            {
-                // Some parameters (like ParameterBlock types) may not have layout decorations
-                // in ray tracing shaders, so we skip them here.
-                continue;
-            }
             auto paramLayout = as<IRVarLayout>(paramLayoutDecoration->getLayout());
             handleSingleParam(context, func, param, paramLayout);
         }
@@ -4192,10 +4184,6 @@ void legalizeEntryPointForGLSL(
     {
         for (auto pp = firstBlock->getFirstParam(); pp; pp = pp->getNextParam())
         {
-            if (isRayTracingShader)
-            {
-                continue;
-            }
             // Any initialization code we insert for parameters needs
             // to be at the start of the "ordinary" instructions in the block:
             builder.setInsertBefore(firstBlock->getFirstOrdinaryInst());
