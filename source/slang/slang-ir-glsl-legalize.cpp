9b76a9116486a74d4211f596073dace35d6a61f3
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-08-04 09:23:52 -0700

    Handle array and scalar

--- source/slang/slang-ir-glsl-legalize.cpp
+++ source/slang/slang-ir-glsl-legalize.cpp
@@ -2212,9 +2212,9 @@ void assign(
                     auto adaptedRight = adaptType(
                         builder,
                         typeAdapter->val,
-                        typeAdapter->actualType,
-                        typeAdapter->pretendType);
-                    assign(builder, left, adaptedRight, index);
+                        typeAdapter->pretendType,
+                        typeAdapter->actualType);
+                    assign(builder, left, adaptedRight);
                     break;
                 }
 
@@ -3567,10 +3567,7 @@ void legalizeEntryPointParameterForGLSL(
     }
     else if (auto ptrType = as<IRPtrTypeBase>(paramType))
     {
-        // This is the case where the parameter is passed by const
-        // reference. For struct types, we create a local variable to replace 
-        // the parameter and populate it from the global variable for better 
-        // debug info. For other types, we use the original behavior.
+        // This is the case where the parameter is passed by const reference.
         SLANG_ASSERT(
             ptrType->getOp() == kIROp_ConstRefType ||
             ptrType->getAddressSpace() == AddressSpace::Input ||
@@ -3591,14 +3588,91 @@ void legalizeEntryPointParameterForGLSL(
         auto localVariable = builder->emitVar(valueType);
         auto localVal = ScalarizedVal::address(localVariable);
 
-        // Copy name hint and other debug decorations from the parameter
-        if (auto nameHint = pp->findDecoration<IRNameHintDecoration>())
+        bool needToAssign = true;
+        if (globalValue.flavor == ScalarizedVal::Flavor::tuple)
         {
-            builder->addNameHintDecoration(localVariable, nameHint->getName());
+            if (auto arrayType = as<IRArrayType>(valueType))
+            {
+                auto elementType = arrayType->getElementType();
+                auto elementCount = arrayType->getElementCount();
+                
+                if (auto structType = as<IRStructType>(elementType))
+                {
+                    // Copy name hint and other debug decorations from the parameter
+                    if (auto nameHint = pp->findDecoration<IRNameHintDecoration>())
+                    {
+                        builder->addNameHintDecoration(localVariable, nameHint->getName());
+                    }
+                    
+                    // Convert from SOA (global) to AOS (local) by initializing each array element
+                    auto globalTuple = as<ScalarizedTupleValImpl>(globalValue.impl);
+                    auto arrayElementCount = (UInt)getIntVal(elementCount);
+                    
+                    for (UInt arrayIndex = 0; arrayIndex < arrayElementCount; arrayIndex++)
+                    {
+                        // Get the address of the array element: localArray[arrayIndex]
+                        auto indexVal = builder->getIntValue(builder->getIntType(), (IRIntegerValue)arrayIndex);
+                        auto elementAddr = builder->emitElementAddress(
+                            builder->getPtrType(elementType),
+                            localVariable,
+                            indexVal);
+                        
+                        // For each struct field, get the value from the global SOA and store to local AOS
+                        for (auto field : structType->getFields())
+                        {
+                            // Find the corresponding global field array
+                            IRInst* globalFieldValue = nullptr;
+                            for (auto& globalElement : globalTuple->elements)
+                            {
+                                if (globalElement.key == field->getKey())
+                                {
+                                    // Extract the value at arrayIndex from the global field array
+                                    if (globalElement.val.flavor == ScalarizedVal::Flavor::address)
+                                    {
+                                        auto fieldElementAddr = builder->emitElementAddress(
+                                            builder->getPtrType(field->getFieldType()),
+                                            globalElement.val.irValue,
+                                            indexVal);
+                                        globalFieldValue = builder->emitLoad(fieldElementAddr);
+                                    }
+                                    else if (globalElement.val.flavor == ScalarizedVal::Flavor::value)
+                                    {
+                                        globalFieldValue = builder->emitElementExtract(
+                                            field->getFieldType(),
+                                            globalElement.val.irValue,
+                                            indexVal);
+                                    }
+                                    break;
+                                }
+                            }
+                            
+                            if (globalFieldValue)
+                            {
+                                // Store the field value to the local struct element
+                                auto localFieldAddr = builder->emitFieldAddress(
+                                    builder->getPtrType(field->getFieldType()),
+                                    elementAddr,
+                                    field->getKey());
+                                builder->emitStore(localFieldAddr, globalFieldValue);
+                            }
+                        }
+                    }
+                    needToAssign = false;
+                }
+            }
         }
 
-        // Assign from the global variable to the local variable
-        assign(builder, localVal, globalValue);
+        if (needToAssign)
+        {
+            // Copy name hint and other debug decorations from the parameter
+            if (auto nameHint = pp->findDecoration<IRNameHintDecoration>())
+            {
+                builder->addNameHintDecoration(localVariable, nameHint->getName());
+            }
+            
+            // Assign from the global variable to the local variable
+            assign(builder, localVal, globalValue);
+        }
 
         // Replace uses of the original parameter with the local variable
         pp->replaceUsesWith(localVariable);
