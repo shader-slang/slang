4d0a650a169feffdcd00e60a24ff87c180ac4290
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-08-21 21:48:37 -0700

    Retry CI test 30 times

--- tools/slang-unit-test/unit-test-record-replay.cpp
+++ tools/slang-unit-test/unit-test-record-replay.cpp
@@ -457,7 +457,7 @@ SLANG_UNIT_TEST(RecordReplay_ray_tracing)
 
 // This causes a Windows Graphics driver crash.
 // Temporarily disabled; issue #8022
-#if 0
+#if 1
 SLANG_UNIT_TEST(RecordReplay_ray_tracing_pipeline)
 {
     SLANG_CHECK(SLANG_SUCCEEDED(runTest(unitTestContext, "ray-tracing-pipeline")));
