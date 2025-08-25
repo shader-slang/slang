e36bbc4b4862793431bb2eee5c1a00f102c2a41a
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-08-25 11:16:12 -0700

    adding a test to expected-failure-github.txt

--- tools/gfx/vulkan/vk-shader-object.cpp
+++ tools/gfx/vulkan/vk-shader-object.cpp
@@ -504,7 +504,7 @@ void ShaderObjectImpl::writeAccelerationStructureDescriptor(
             static_cast<AccelerationStructureImpl*>(resourceViews[i].Ptr());
         VkWriteDescriptorSetAccelerationStructureKHR writeAS = {};
         writeAS.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR;
-        VkAccelerationStructureKHR nullHandle = VK_NULL_HANDLE;
+        static const VkAccelerationStructureKHR nullHandle = VK_NULL_HANDLE;
         if (accelerationStructure)
         {
             writeAS.accelerationStructureCount = 1;
@@ -512,6 +512,12 @@ void ShaderObjectImpl::writeAccelerationStructureDescriptor(
         }
         else
         {
+            // The Vulkan spec states: If the nullDescriptor feature is not enabled, each element of
+            // pAccelerationStructures must not be VK_NULL_HANDLE
+            if (!context.device->m_api.m_extendedFeatures.robustness2Features.nullDescriptor)
+            {
+                continue;
+            }
             writeAS.accelerationStructureCount = 1;
             writeAS.pAccelerationStructures = &nullHandle;
         }
