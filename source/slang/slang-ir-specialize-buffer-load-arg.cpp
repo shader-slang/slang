955bc834b09fca3167be144b57bbc36064da4e00
Yong He <yonghe@outlook.com>
2025-09-26 21:43:45 -0700

    Specialize buffer load.

--- source/slang/slang-ir-specialize-buffer-load-arg.cpp
+++ source/slang/slang-ir-specialize-buffer-load-arg.cpp
@@ -1,8 +1,11 @@
 // slang-ir-specialize-buffer-load-arg.cpp
 #include "slang-ir-specialize-buffer-load-arg.h"
 
+#include "slang-ir-defer-buffer-load.h"
 #include "slang-ir-insts.h"
+#include "slang-ir-layout.h"
 #include "slang-ir-specialize-function-call.h"
+#include "slang-ir-util.h"
 #include "slang-ir.h"
 
 namespace Slang
@@ -17,43 +20,87 @@ namespace Slang
 // As swith most of our IR passes, we encapsulate the logic here in a context
 // type so that the data that needs to be shared throughout the pass can
 // be conveniently scoped.
+//
+
+// Note that this pass also ensures other more contrived cases are properly
+// handled. For example:
+//
+// * A load of a large structure from field in a constant buffer, so that
+//   the value loaded is not the entire buffer contents.
+//
+// * A load of a large structure from a structured buffer, or any other kind
+//   of buffer that requires an index.
+//
 
 struct FuncBufferLoadSpecializationCondition : FunctionCallSpecializeCondition
 {
     typedef FunctionCallSpecializeCondition Super;
 
+    // Generally, we want to specialize arguments that are large in size, or arguments that
+    // are arrays or composite type that contains arrays.
+    // This is because:
+    // 1. Struct types without arrays will eventually be SROA's into registers and then effectively
+    //    DCE'd, so they usually won't cause performance issues. In fact, front loading structs
+    //    and reusing the loaded value instead of repetitively loading from constant memory is
+    //    usually beneficial to performance. However large struct values can be SROA'd into a large
+    //    number of registers, causing slow downstream compilation. Therefore we should avoid/defer
+    //    loading them into registers if we can.
+    // 2. Arrays usually cannot be SROA'd into individual registers, which usually leads to
+    //    large register consumption if they ever get loaded, so we want to defer loading array
+    //    typed values as much as possible.
+
+    // If the argument data is bigger than this threshold, it is considered a large object
+    // and we will try to specialize it even if it doesn't contain arrays.
+    static const int kBufferLoadElementSizeSpecializationThreshold = 256;
+
+    // If the argument data is smaller than this threshold, it is considered a tiny object
+    // and we will not consider specializing it, even if it contains arrays.
+    static const int kBufferLoadElementSizeSpecializationMinThreshold = 16;
+
+    CodeGenContext* codegenContext;
+
     virtual bool doesParamWantSpecialization(IRParam* param, IRInst* arg)
     {
         // We only want to specialize for `struct` types and not base types.
         //
-        // TODO: We might want to consider some criteria here for the "large-ness"
-        // of a structure (in terms of bytes and/or fields), so that we don't
-        // eliminate loads of sufficiently small types (which are cheap to pass
-        // by value).
-        //
-        auto paramType = param->getDataType();
-        if (!as<IRStructType>(paramType))
+        auto paramType = (IRType*)unwrapAttributedType(param->getDataType());
+        if (!isTypePreferrableToDeferLoad(codegenContext, paramType))
             return false;
 
-        // We also only want to specialize for arguments that are a load
-        // from some kind of global shader parameter.
+        // We want to handle loads from arbitrary access chains rooting from a shader parameter.
         //
         IRInst* a = arg;
-        if (auto argLoad = as<IRLoad>(arg))
+        bool seenLoad = false;
+        for (;;)
         {
-            a = argLoad->getPtr();
-        }
-        else
-        {
-            return false;
-        }
-
-        // We want to handle loads from a shader parameter that is an array
-        // of buffers, and not just a single global buffer.
-        //
-        while (auto argGetElement = as<IRGetElement>(a))
-        {
-            a = argGetElement->getBase();
+            if (auto argGetElement = as<IRGetElement>(a))
+            {
+                a = argGetElement->getBase();
+            }
+            else if (auto argFieldExtract = as<IRFieldExtract>(a))
+            {
+                a = argFieldExtract->getBase();
+            }
+            else if (auto argGetElementPtr = as<IRGetElementPtr>(a))
+            {
+                a = argGetElementPtr->getBase();
+            }
+            else if (auto argFieldAddr = as<IRFieldAddress>(a))
+            {
+                a = argFieldAddr->getBase();
+            }
+            else if (auto argLoad = as<IRLoad>(a))
+            {
+                // For now, we can only handle one level of dereference.
+                if (seenLoad)
+                    return false;
+                a = argLoad->getPtr();
+                seenLoad = true;
+            }
+            else
+            {
+                break;
+            }
         }
 
         // The "root" of the parameter must be a reference to a global-scope
@@ -61,32 +108,18 @@ struct FuncBufferLoadSpecializationCondition : FunctionCallSpecializeCondition
         //
         if (const auto argGlobalParam = as<IRGlobalParam>(a))
         {
-            return true;
+            // We can only specialize if the buffer is immutable.
+            if (isImmutableLocation(argGlobalParam))
+                return true;
         }
-        else
-        {
-            return false;
-        }
-
-        // TODO: There are other patterns that we could attempt to optimize here.
-        // For example, this logic only handles loads of the *entire* contents of
-        // a buffer, so it would miss:
-        //
-        // * A load of a large structure from field in a constant buffer, so that
-        //   the value loaded is not the entire buffer contents.
-        //
-        // * A load of a large structure from a structured buffer, or any other kind
-        //   of buffer that requires an index.
-        //
-        // * Any resource load that is not expressed at the IR level with a `load`
-        //   instruction (e.g., those that might use an intrinsic function).
-        //
+        return false;
     }
 };
 
 void specializeFuncsForBufferLoadArgs(CodeGenContext* codegenContext, IRModule* module)
 {
     FuncBufferLoadSpecializationCondition condition;
+    condition.codegenContext = codegenContext;
     specializeFunctionCalls(codegenContext, module, &condition);
 }
 
