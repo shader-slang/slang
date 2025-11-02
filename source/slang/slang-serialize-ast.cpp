861018b65b0722939685c27253468b98f469d18e
Yong He <yonghe@outlook.com>
2025-11-01 18:12:33 -0700

    Fix.

--- source/slang/slang-serialize-ast.cpp
+++ source/slang/slang-serialize-ast.cpp
@@ -1680,11 +1680,11 @@ ModuleDecl* ASTSerialReadContext::_readImportedModule(ASTSerializer const& seria
     //
     Name* moduleName = nullptr;
     serialize(serializer, moduleName);
-    auto module = _linkage->findOrImportModule(moduleName, SourceLoc(), _sink);
+    auto module = _linkage->findOrImportModule(moduleName, _requestingSourceLoc, _sink);
     if (!module)
     {
         if (_sink)
-            _sink->diagnose(SourceLoc(), Diagnostics::importFailed, moduleName);
+            _sink->diagnose(_requestingSourceLoc, Diagnostics::importFailed, moduleName);
         return nullptr;
     }
     return module->getModuleDecl();
