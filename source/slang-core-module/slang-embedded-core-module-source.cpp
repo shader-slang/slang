70a047264adb390d2699211fdf45f8205731f24c
GinShio <ginshio78@gmail.com>
2025-11-03 13:52:59 +0800

    WIP: MLElement in CoopMat

--- source/slang-core-module/slang-embedded-core-module-source.cpp
+++ source/slang-core-module/slang-embedded-core-module-source.cpp
@@ -23,12 +23,12 @@ enum
     BOOL_RESULT = 1 << 2,
     BOOL_MASK = 1 << 3,
     UINT_MASK = 1 << 4,
-    BFLOAT_MASK = 1 << 5,
+    ML_ELEMENT_MASK = 1 << 5,
 
     INT_MASK = SINT_MASK | UINT_MASK,
-    ARITHMETIC_MASK = INT_MASK | FLOAT_MASK | BFLOAT_MASK,
+    ARITHMETIC_MASK = INT_MASK | FLOAT_MASK,
     LOGICAL_MASK = INT_MASK | BOOL_MASK,
-    ANY_MASK = INT_MASK | FLOAT_MASK | BOOL_MASK | BFLOAT_MASK,
+    ANY_MASK = INT_MASK | FLOAT_MASK | BOOL_MASK | ML_ELEMENT_MASK,
 };
 
 // We are going to declare initializers that allow for conversion between
@@ -47,7 +47,6 @@ enum BaseTypeConversionKind : uint8_t
     kBaseTypeConversionKind_Signed,
     kBaseTypeConversionKind_Unsigned,
     kBaseTypeConversionKind_Float,
-    kBaseTypeConversionKind_BFloat,
     kBaseTypeConversionKind_Error,
 };
 enum BaseTypeConversionRank : uint8_t
@@ -117,8 +116,8 @@ static const BaseTypeConversionInfo kBaseTypes[] = {
      kBaseTypeConversionRank_Int16},
     {"bfloat16_t",
      BaseType::BFloat16,
-     BFLOAT_MASK,
-     kBaseTypeConversionKind_BFloat,
+     ML_ELEMENT_MASK,
+     kBaseTypeConversionKind_Float,
      kBaseTypeConversionRank_Int16},
     {"float",
      BaseType::Float,
@@ -308,7 +307,7 @@ struct IntrinsicOpInfo
     {kIROp_Mul, "mul", "*", "__BuiltinArithmeticType", ARITHMETIC_MASK},
     {kIROp_Div, "div", "/", "__BuiltinArithmeticType", ARITHMETIC_MASK},
     {kIROp_IRem, "irem", "%", "__BuiltinIntegerType", INT_MASK},
-    {kIROp_FRem, "frem", "%", "__BuiltinFloatingPointType", FLOAT_MASK | BFLOAT_MASK},
+    {kIROp_FRem, "frem", "%", "__BuiltinFloatingPointType", FLOAT_MASK},
     {kIROp_And, "logicalAnd", "&&", nullptr, BOOL_MASK | BOOL_RESULT},
     {kIROp_Or, "logicalOr", "||", nullptr, BOOL_MASK | BOOL_RESULT},
     {kIROp_BitAnd, "and", "&", "__BuiltinLogicalType", LOGICAL_MASK},
