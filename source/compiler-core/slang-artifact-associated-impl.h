942c4f38008fe7fa0910a0957f078bef561a3e00
Ellie Hermaszewska <git@monoid.al>
2025-11-13 16:41:30 +0800

    Neaten variable names

--- source/compiler-core/slang-artifact-associated-impl.h
+++ source/compiler-core/slang-artifact-associated-impl.h
@@ -101,9 +101,11 @@ struct ShaderBindingRange
     slang::ParameterCategory category = slang::ParameterCategory::None;
     UInt spaceIndex = 0;
     UInt registerIndex = 0;
-    UInt registerCount = 0; // 0 for unsized
+    UInt registerCount = k_unsizedRegisterCount; // 0 for unsized
 
-    bool isInfinite() const { return registerCount == 0; }
+    const static UInt k_unsizedRegisterCount = 0;
+
+    bool isInfinite() const { return registerCount == k_unsizedRegisterCount; }
 
     bool containsBinding(slang::ParameterCategory _category, UInt _spaceIndex, UInt _registerIndex)
         const
@@ -144,7 +146,7 @@ struct ShaderBindingRange
         UInt newRegisterIndex = Math::Min(registerIndex, other.registerIndex);
 
         if (other.isInfinite())
-            registerCount = 0;
+            registerCount = k_unsizedRegisterCount;
         else if (!isInfinite())
             registerCount = Math::Max(
                                 registerIndex + registerCount,
