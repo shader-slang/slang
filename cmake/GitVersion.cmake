6ed4611bbb2e92d9d6f8676143b17de2b7552b31
James Helferty <jhelferty@nvidia.com>
2025-11-14 15:42:29 -0500

    Embed VERSION in source archives, give priority
    Fixes #9019
    
    Up until now, we've used git describe as the primary source of truth
    for the version number. To be clear, it still is, but we need to
    consider what downstream consumers and packagers of Slang may be
    doing. e.g., the following use cases:
    
    - Rebuilding from source archives
    
    - Rebuilding from source archives ingested into a separate version
      control system
    
    To that end, we define a VERSION file, and enable git export-subst on
    it. For source releases created using git archive, this will replace
    the contents of VERSION with the most recent version number, as
    expressed relative to the most recent version tag in the Slang git
    repository (our source of truth).
    
    The cmake rule is updated to prioritize the VERSION file (if and only
    if export-subst has been applied to the file) higher than the git
    describe contents, since the source package may have been ingested
    into another VCS (possibly also git). It's possible this won't be
    accurate--additional patches may have been applied on top--but we're
    doing what we can here to ensure the versioning on the filenames is
    correct. Not every eventuality can be forseen.

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -5,40 +5,64 @@ function(get_git_version var_numeric var dir)
     if(NOT DEFINED ${var})
         set(version_numeric "0.0.0")
         set(version "0.0.0-unknown")
-        if(GIT_EXECUTABLE)
-            set(command
-                "${GIT_EXECUTABLE}"
-                -C
-                "${dir}"
-                describe
-                --tags
-                --match
-                v20[2-9][0-9].[0-9]*
-            )
-            execute_process(
-                COMMAND ${command}
-                RESULT_VARIABLE result
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                OUTPUT_VARIABLE version_out
-            )
-            if(NOT result EQUAL 0)
-                message(
-                    WARNING
-                    "Getting ${var} failed: ${command} returned ${result}\nIs this a Git repo with tags?\nConsider settings -D${var} to specify a version manually"
+        set(version_out "")
+
+        # First, try reading from VERSION file
+        set(version_file "${dir}/VERSION")
+        if(EXISTS "${version_file}")
+            file(READ "${version_file}" version_file_content)
+            string(STRIP "${version_file_content}" version_file_content)
+            # Check if it's not a git archive format string and matches expected pattern
+            if(NOT version_file_content MATCHES "^\\$Format:" AND
+               version_file_content MATCHES "^v20[2-9][0-9]\\.[0-9]")
+                set(version_out "${version_file_content}")
+                message(STATUS "Using version from VERSION file: ${version_out}")
+            endif()
+        endif()
+
+        # If VERSION file didn't provide a valid version, try git describe
+        if(NOT version_out)
+            if(GIT_EXECUTABLE AND EXISTS "${dir}/.git")
+                set(command
+                    "${GIT_EXECUTABLE}"
+                    -C
+                    "${dir}"
+                    describe
+                    --tags
+                    --match
+                    v20[2-9][0-9].[0-9]*
                 )
-            elseif("${version_out}" MATCHES "^v(([0-9]+(\\.[0-9]+)*).*)")
-                set(version "${CMAKE_MATCH_1}")
-                set(version_numeric "${CMAKE_MATCH_2}")
-            else()
+                execute_process(
+                    COMMAND ${command}
+                    RESULT_VARIABLE result
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    OUTPUT_VARIABLE git_describe_out
+                )
+                if(result EQUAL 0)
+                    set(version_out "${git_describe_out}")
+                    message(STATUS "Using version from git describe: ${version_out}")
+                else()
+                    message(
+                        WARNING
+                        "Getting ${var} failed: ${command} returned ${result}\nIs this a Git repo with tags?\nConsider settings -D${var} to specify a version manually"
+                    )
+                endif()
+            elseif(NOT GIT_EXECUTABLE)
                 message(
                     WARNING
-                    "Couldn't parse version (like v2025.21 or v2025.21-foo) from ${version_out}, using ${version} for now"
+                    "Couldn't find git executable to get ${var}, please use -D${var}, using ${version} for now"
                 )
             endif()
-        else()
+        endif()
+
+        # Parse the version string (from either VERSION file or git describe)
+        if(version_out AND "${version_out}" MATCHES "^v(([0-9]+(\\.[0-9]+)*).*)")
+            set(version "${CMAKE_MATCH_1}")
+            set(version_numeric "${CMAKE_MATCH_2}")
+        elseif(version_out)
             message(
                 WARNING
-                "Couldn't find git executable to get ${var}, please use -D${var}, using ${version} for now"
+                "Couldn't parse version (like v2025.21 or v2025.21-foo) from ${version_out}, using ${version} for now"
             )
         endif()
     endif()
