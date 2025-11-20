aa24a9c4a97f6d5ff8f9de13d13ca1f8fb883a04
Theresa Foley <tfoley@nvidia.com>
2025-11-20 11:55:08 -0800

    fixup: shift stable opcode number for __undefined

--- source/slang/slang-ir-insts-stable-names.lua
+++ source/slang/slang-ir-insts-stable-names.lua
@@ -685,5 +685,5 @@ return {
 	["StoreBase.copyLogical"] = 681,
 	["MakeStorageTypeLoweringConfig"] = 682,
 	["Decoration.experimentalModule"] = 683,
-	["Undefined.DeliberatelyUninitialized"] = 680,
+	["Undefined.DeliberatelyUninitialized"] = 684,
 }
