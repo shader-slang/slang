47844f37e15b6a23228b1079471f83c7c4bfe63b
Sai Praveen Bangaru <31557731+saipraveenb25@users.noreply.github.com>
2025-12-12 16:52:36 -0500

    fix formatting

--- source/slang/slang-ir-check-specialize-generic-with-existential.h
+++ source/slang/slang-ir-check-specialize-generic-with-existential.h
@@ -8,8 +8,6 @@ struct IRModule;
 
 /// This IR check pass is will diagnose error when a generic is specialized with
 // an existential type.
-void addDecorationsForGenericsSpecializedWithExistentials(
-    IRModule* module,
-    DiagnosticSink* sink);
+void addDecorationsForGenericsSpecializedWithExistentials(IRModule* module, DiagnosticSink* sink);
 
 } // namespace Slang
