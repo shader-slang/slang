84cac4ca38e26790f87808af3b7aad96ba440eea
James Helferty <jhelferty@nvidia.com>
2025-12-12 16:38:55 -0500

    Fix fd leak

--- source/core/unix/slang-unix-process.cpp
+++ source/core/unix/slang-unix-process.cpp
@@ -75,6 +75,8 @@ public:
     {
     }
 
+    ~UnixPipeStream() { close(); }
+
 protected:
     /// This read file descriptor non blocking. Doing so will change the behavior of
     /// read - it can fail and return an error indicating there is no data, instead of blocking.
