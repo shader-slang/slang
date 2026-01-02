6c37a908dcd3c04354099c527c3f5baa5bd064ad
Ellie Hermaszewska <git@monoid.al>
2026-01-02 14:40:20 +0800

    tweak render header

--- source/compiler-core/slang-rich-diagnostics-render.h
+++ source/compiler-core/slang-rich-diagnostics-render.h
@@ -1,13 +1,45 @@
 #pragma once
 
-#include "core/slang-string.h"
+#include "../core/slang-list.h"
+#include "../core/slang-string.h"
+#include "compiler-core/slang-diagnostic-sink.h"
+#include "compiler-core/slang-source-loc.h"
 
 namespace Slang
 {
 
-struct GenericDiagnostic;
 struct SourceManager;
 
-String renderDiagnostic(const GenericDiagnostic& diagnostic, SourceManager& sourceManager);
+struct DiagnosticSpan
+{
+    Slang::SourceRange range;
+    String message;
+};
+
+struct DiagnosticNote
+{
+    String message;
+    DiagnosticSpan span;
+};
+
+//
+// A struct capable of representing any diagnostic we want to display
+//
+struct GenericDiagnostic
+{
+    int code;
+    Severity severity;
+    String message;
+    DiagnosticSpan primarySpan;
+    List<DiagnosticSpan> secondarySpans;
+    List<DiagnosticNote> notes;
+};
+
+String renderDiagnostic(SourceManager* sm, const GenericDiagnostic& diag);
+
+#define SLANG_ENABLE_DIAGNOSTIC_RENDER_UNIT_TESTS 1
+#ifdef SLANG_ENABLE_DIAGNOSTIC_RENDER_UNIT_TESTS
+int slangRichDiagnosticsUnitTest(int argc, char* argv[]);
+#endif
 
 } // namespace Slang
