2edd57a590922e44f273b257d5788551b9280615
Sai Praveen Bangaru <31557731+saipraveenb25@users.noreply.github.com>
2026-03-24 15:28:43 -0400

    Fix formatting

--- source/slang/slang-ast-val.cpp
+++ source/slang/slang-ast-val.cpp
@@ -2124,10 +2124,7 @@ Val* FuncCallIntVal::tryFoldImpl(
     }                                       \
     else
 
-#define TERMINATING_CASE(MATCH) \
-    {                           \
-        MATCH                   \
-    }
+#define TERMINATING_CASE(MATCH) {MATCH}
 
         // Handle the cases using the macros
         BINARY_OPERATOR_CASE(>=)
