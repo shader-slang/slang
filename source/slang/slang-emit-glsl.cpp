372d1fc890d8b935189d6a27e704caf22f0a4338
zangold <zangold@nvidia.com>
2026-04-06 02:07:51 -0400

    address automated code review feedback

--- source/slang/slang-emit-glsl.cpp
+++ source/slang/slang-emit-glsl.cpp
@@ -1065,8 +1065,7 @@ void GLSLSourceEmitter::_emitGLSLTextureOrTextureSamplerType(
         if (type->GetBaseShape() == SLANG_TEXTURE_CUBE)
         {
             // samplerCubeArray requires GL_ARB_texture_cube_map_array on GLSL < 4.00
-            _requireGLSLExtension(
-                UnownedStringSlice::fromLiteral("GL_ARB_texture_cube_map_array"));
+            _requireGLSLExtension(UnownedStringSlice::fromLiteral("GL_ARB_texture_cube_map_array"));
         }
     }
 
