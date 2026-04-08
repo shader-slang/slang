2dee663313cd788756e504edc6b8c83202df1cb1
slangbot <186143334+slangbot@users.noreply.github.com>
2026-04-08 07:07:13 +0000

    format code

--- source/slang/slang-check-expr.cpp
+++ source/slang/slang-check-expr.cpp
@@ -6295,8 +6295,7 @@ static bool _isTypeParametric(Type* type)
     if (auto declRefType = as<DeclRefType>(type))
     {
         auto decl = declRefType->getDeclRef().getDecl();
-        if (as<GenericTypeParamDeclBase>(decl) || as<AssocTypeDecl>(decl) ||
-            as<ThisTypeDecl>(decl))
+        if (as<GenericTypeParamDeclBase>(decl) || as<AssocTypeDecl>(decl) || as<ThisTypeDecl>(decl))
             return true;
         // Check generic arguments for parametric types.
         // E.g., Container<T> contains the generic param T.
