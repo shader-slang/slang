9ba1702fdbc3ac966844be4501744536502adf8e
Julius Ikkala <julius.ikkala@gmail.com>
2026-04-23 01:24:03 +0300

    Address more LLM feedback

--- prelude/slang-cuda-prelude.h
+++ prelude/slang-cuda-prelude.h
@@ -2547,6 +2547,8 @@ SLANG_FORCE_INLINE SLANG_CUDA_CALL uintptr_t UPTR_max(uintptr_t a, uintptr_t b)
 SLANG_FORCE_INLINE SLANG_CUDA_CALL void* __slang_cuda_realloc(void* oldPtr, size_t oldBytes, size_t newBytes)
 {
     void* newPtr = malloc(newBytes);
+    if (!newPtr)
+        return nullptr;
     memcpy(newPtr, oldPtr, oldBytes < newBytes ? oldBytes : newBytes);
     free(oldPtr);
     return newPtr;
