804977c1b54a6365be48f9a8e692ca2cd95d7f8d
Jussi Vepsalainen (NVIDIA) <161336110+jvepsalainen-nv@users.noreply.github.com>
2026-05-27 10:39:13 +0300

    Revert "Track carrying values through inout calls"
    This reverts commit 6f0fca4fdb8d2a5576e0d3329a5d0efb33f3c44c.

--- source/slang/slang-ir-check-differentiability.cpp
+++ source/slang/slang-ir-check-differentiability.cpp
@@ -458,24 +458,6 @@ public:
             }
         };
 
-        auto isAddressCarryingNonTrivialDiff = [&](IRInst* addr) -> bool
-        {
-            if (carryNonTrivialDiffSet.contains(addr))
-                return true;
-            if (!canAddressHoldDerivative(diffTypeContext, addr))
-                return false;
-
-            for (auto use = addr->firstUse; use; use = use->nextUse)
-            {
-                if (auto store = as<IRStore>(use->getUser()))
-                {
-                    if (store->getPtr() == addr && carryNonTrivialDiffSet.contains(store->getVal()))
-                        return true;
-                }
-            }
-            return false;
-        };
-
         auto isInstCarryingOverDiff = [&](IRInst* inst) -> bool
         {
             switch (inst->getOp())
@@ -519,10 +501,7 @@ public:
                             if (!diffTypeContext.isDifferentiableType(paramBaseType))
                                 continue;
                         }
-                        auto arg = callInst->getArg(i);
-                        if (carryNonTrivialDiffSet.contains(arg))
-                            return true;
-                        if (isAddressCarryingNonTrivialDiff(arg))
+                        if (carryNonTrivialDiffSet.contains(callInst->getArg(i)))
                             return true;
                     }
                     return false;
