a82a93e1060b21af69a8cea45001eecd5f92fd1f
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2026-05-28 04:52:20 -0700

    Fix default ctor synthesis for later member types

--- source/slang/slang-check-decl.cpp
+++ source/slang/slang-check-decl.cpp
@@ -3144,6 +3144,8 @@ static Expr* constructDefaultConstructorForType(
     {
         if (auto structDecl = as<StructDecl>(declRefType->getDeclRef().getDecl()))
         {
+            if (!structDecl->checkState.isBeingChecked())
+                visitor->ensureDecl(structDecl, DeclCheckState::AttributesChecked);
             defaultCtor = _getDefaultCtor(structDecl);
         }
     }
