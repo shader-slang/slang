shader-slang/slang

commit 45ccce9a376d48a7342615afb607d865cf973092

Refactor auto-diff implementation. (#9808)
This PR is a near-total overhaul of how auto-diff is handled in Slang. 

> [!NOTE]
> Due to this PR's size, we strongly recommend using **Github Dev**
(https://github.dev/shader-slang/slang/pull/9808) to review these
changes.

It replaces the previously ad-hoc, messy system with a much simpler
approach that integrates cleanly into Slang's trait-based type system.
Additionally, the refactored code is much less likely to get in the way
of implementing new type-system features in Slang.

This makes it much simpler to extend our auto-diff to handle
significantly more complex requirements.
For example:
   - Differentiability under generic constraints
- Fine-grained custom derivatives via extensions that can apply to
constrained cases
- User-defined context types to pass checkpointing information from
primal to differential code.
- Full integration with dynamic dispatch & other advanced type system
features (unified auto-diff behavior whether a method is invoked
indirectly or directly) without having to explicitly account for
auto-diff everywhere.
   - and more...

Fixes: https://github.com/shader-slang/slang/issues/6486

# Autodiff Overhaul Patch Notes

## Table of Contents
- <a href="#overview">Overview</a>
- <a href="#functions-as-types">Functions As Types</a>
- <a
href="#type-system-changes-to-support-functions-as-types">Type-System
Changes to Support Functions as Types</a>
- <a href="#auto-diff-function-interfaces">Auto-Diff Function
Interfaces</a>
- <a
href="#splitting-the-context-type-into-minimalcontext-and-bwdcallable">Splitting
the Context Type into `MinimalContext` and `BwdCallable`</a>
- <a href="#lowering-constraints-on-generic-requirements">Lowering
Constraints on Generic Requirements</a>
- <a href="#synthesized-decls">Synthesized Decls</a>
- <a
href="#synthesized-function-declarations-synthesizedfuncdecl">Synthesized
Function Declarations `SynthesizedFuncDecl`</a>
- <a

source/slang/slang-ir-link.cpp
@@ -10,11 +10,13 @@
 #include "slang-ir-specialize-target-switch.h"
 #include "slang-ir-specialize.h"
 #include "slang-ir-string-hash.h"
+#include "slang-ir-translate.h"
 #include "slang-ir.h"
 #include "slang-legalize-types.h"
 #include "slang-mangle.h"
 #include "slang-module-library.h"
 #include "slang-rich-diagnostics.h"
+
 
 namespace Slang
 {
@@ -151,15 +153,6 @@ void registerClonedValue(IRSpecContextBase* context, IRInst* clonedValue, IRInst
         context->deferredWitnessTableEntryKeys.add(
             getMangledName(as<IRLookupWitnessMethod>(clonedValue)->getRequirementKey()));
         break;
-    case kIROp_ForwardDerivativeDecoration:
-    case kIROp_BackwardDerivativeDecoration:
-    case kIROp_UserDefinedBackwardDerivativeDecoration:
-        if (context->getShared()->useAutodiff)
-        {
-            if (auto key = as<IRStructKey>(clonedValue->getOperand(0)))
-                context->deferredWitnessTableEntryKeys.add(getMangledName(key));
-        }
-        break;
     }
 }
 
@@ -200,31 +193,24 @@ IRInst* cloneInst(
     IRInst* originalInst,
     IROriginalValuesForClone const& originalValues);
 
-IRInst* cloneInst(IRSpecContextBase* context, IRBuilder* builder, IRInst* originalInst)
+static void cloneAnnotations(IRSpecContextBase* context, IRInst* clonedInst, IRInst* originalInst)
 {
-    return cloneInst(context, builder, originalInst, originalInst);
+    SLANG_UNUSED(clonedInst);
+    traverseUsers<IRAnnotation>(
+        originalInst,
+        [&](IRAnnotation* annotation)
+        {
+            if (annotation->getTarget() == originalInst)
+                cloneInst(context, context->builder, annotation, annotation);
+        });
 }
 
-bool isAutoDiffDecoration(IRInst* decor)
+IRInst* cloneInst(IRSpecContextBase* context, IRBuilder* builder, IRInst* originalInst)
 {
-    switch (decor->getOp())
-    {
-    case kIROp_ForwardDerivativeDecoration:
-    case kIROp_BackwardDerivativeIntermediateTypeDecoration:
-    case kIROp_BackwardDerivativePrimalDecoration:
-    case kIROp_BackwardDerivativePropagateDecoration:
-    case kIROp_BackwardDerivativePrimalContextDecoration:
-    case kIROp_BackwardDerivativePrimalReturnDecoration:
-    case kIROp_PrimalSubstituteDecoration:
-    case kIROp_BackwardDerivativeDecoration:
-    case kIROp_UserDefinedBackwardDerivativeDecoration:
-    case kIROp_DifferentiableTypeDictionaryDecoration:
-    case kIROp_ForwardDifferentiableDecoration:
-    case kIROp_BackwardDifferentiableDecoration:
-        return true;
-    default:
-        return false;
-    }
+    auto clonedInst = cloneInst(context, builder, originalInst, originalInst);
+    cloneAnnotations(context, clonedInst, originalInst);
+
+    return clonedInst;
 }
 
 /// Clone any decorations from `originalValue` onto `clonedValue`
@@ -243,8 +229,6 @@ void cloneDecorations(IRSpecContextBase* context, IRInst* clonedValue, IRInst* o
     SLANG_UNUSED(context);
     for (auto originalDecoration : originalValue->getDecorations())
     {
-        if (!context->shared->useAutodiff && isAutoDiffDecoration(originalDecoration))
-            continue;
         cloneInst(context, builder, originalDecoration);
     }
 
@@ -265,8 +249,6 @@ void cloneDecorationsAndChildren(
     SLANG_UNUSED(context);
     for (auto originalItem : originalValue->getDecorationsAndChildren())
     {
-        if (!context->shared->useAutodiff && isAutoDiffDecoration(originalItem))
-            continue;
         cloneInst(context, builder, originalItem);
     }
 
@@ -310,7 +292,11 @@ IRInst* IRSpecContext::maybeCloneValue(IRInst* originalValue)
     case kIROp_InterfaceType:
     case kIROp_EnumType:
     case kIROp_SymbolAlias:
-        return cloneGlobalValue(this, originalValue);
+        {
+            auto clonedInst = cloneGlobalValue(this, originalValue);
+            cloneAnnotations(this, clonedInst, originalValue);
+            return clonedInst;
+        }
 
     case kIROp_BoolLit:
         {
@@ -377,6 +363,7 @@ IRInst* IRSpecContext::maybeCloneValue(IRInst* originalValue)
 
             cloneDecorationsAndChildren(this, clonedValue, originalValue);
             addHoistableInst(builder, clonedValue);
+            cloneAnnotations(this, clonedValue, originalValue);
             return clonedValue;
         }
         break;
@@ -489,12 +476,6 @@ static void cloneExtraDecorationsFromInst(
         {
         default:
             break;
-        case kIROp_ForwardDerivativeDecoration:
-        case kIROp_UserDefinedBackwardDerivativeDecoration:
-        case kIROp_PrimalSubstituteDecoration:
-            if (!context->getShared()->useAutodiff)
-                break;
-            [[fallthrough]];
         case kIROp_HLSLExportDecoration:
         case kIROp_BindExistentialSlotsDecoration:
         case kIROp_LayoutDecoration:
@@ -688,6 +669,7 @@ IRGlobalGenericParam* cloneGlobalGenericParamImpl(
 
 bool shouldDeepCloneWitnessTable(IRSpecContextBase* context, IRWitnessTable* table)
 {
+    SLANG_UNUSED(context);
     for (auto decor : table->getDecorations())
     {
         switch (decor->getOp())
@@ -699,7 +681,6 @@ bool shouldDeepCloneWitnessTable(IRSpecContextBase* context, IRWitnessTable* tab
     }
 
     auto conformanceType = getResolvedInstForDecorations(table->getConformanceType());
-
     for (auto decor : conformanceType->getDecorations())
     {
         switch (decor->getOp())
@@ -709,9 +690,15 @@ bool shouldDeepCloneWitnessTable(IRSpecContextBase* context, IRWitnessTable* tab
         case kIROp_KnownBuiltinDecoration:
             {
                 auto name = as<IRKnownBuiltinDecoration>(decor)->getName();
-                if (name == KnownBuiltinDeclName::IDifferentiable ||
-                    name == KnownBuiltinDeclName::IDifferentiablePtr)
-                    return context->getShared()->useAutodiff;
+                switch (name)
+                {
+                case KnownBuiltinDeclName::IDifferentiable:
+                case KnownBuiltinDeclName::IDifferentiablePtr:
+                case KnownBuiltinDeclName::IForwardDifferentiable:
+                case KnownBuiltinDeclName::IBackwardDifferentiable:
+                case KnownBuiltinDeclName::IBwdCallable:
+                    return true;
+                }
                 break;
             }
         default:
@@ -916,9 +903,6 @@ void cloneGlobalValueWithCodeCommon(
                 }
                 else
                 {
-                    if (oi->getOp() == kIROp_DifferentiableTypeAnnotation &&
-                        !context->getShared()->useAutodiff)
-                        continue;
                     cloneInst(context, builder, oi);
                 }
             }
@@ -1482,6 +1466,7 @@ IRInst* cloneGlobalValueImpl(
     auto clonedValue =
         cloneInst(context, &context->shared->builderStorage, originalInst, originalValues);
     clonedValue->moveToEnd();
+    cloneAnnotations(context, clonedValue, originalInst);
     return clonedValue;
 }
 
@@ -1756,12 +1741,6 @@ static bool isFunctionDefinedOrImported(IRInst* inst)
         case kIROp_IntrinsicOpDecoration:
         case kIROp_TargetIntrinsicDecoration:
         case kIROp_SPIRVOpDecoration:
-        // Autodiff decorations - the function's implementation is provided by the derivative
-        // function
-        case kIROp_ForwardDerivativeDecoration:
-        case kIROp_BackwardDerivativeDecoration:
-        case kIROp_UserDefinedBackwardDerivativeDecoration:
-        case kIROp_PrimalSubstituteDecoration:
         // Explicitly external functions
         case kIROp_ExternCDecoration:
         case kIROp_ExternCppDecoration:
@@ -2041,64 +2020,15 @@ bool isDiffPairType(IRInst* type)
     return as<IRDifferentialPairTypeBase>(type) != nullptr;
 }
 
-bool doesModuleUseAutodiff(IRInst* inst)
+bool doesModuleUseAutodiff(IRModule* module)
 {
-    switch (inst->getOp())
+    for (auto globalInst : module->getGlobalInsts())
     {
-    case kIROp_Call:
-        if (auto callee = getResolvedInstForDecorations(inst->getOperand(0)))
-        {
-            switch (callee->getOp())
-            {
-            case kIROp_ForwardDifferentiate:
-            case kIROp_BackwardDifferentiate:
-            case kIROp_BackwardDifferentiatePrimal:
-            case kIROp_BackwardDifferentiatePropagate:
-                return true;
-            }
-        }
-        return false;
-    case kIROp_DifferentialPairGetDifferentialUserCode:
-    case kIROp_DifferentialPairGetPrimalUserCode:
-    case kIROp_DifferentialPtrPairGetPrimal:
-    case kIROp_DifferentialPtrPairGetDifferential:
-        return true;
-    case kIROp_StructField:
-        return isDiffPairType(as<IRStructField>(inst)->getFieldType());
-    case kIROp_Param:
-        return isDiffPairType(inst->getDataType());
-    default:
-        for (auto child : inst->getChildren())
-        {
-            bool isImported = false;
-            for (auto decor : child->getDecorations())
-            {
-                if (as<IRImportDecoration>(decor))
-                {
-                    isImported = true;
-                    break;
-                }
-                else if (as<IRAutoPyBindCudaDecoration>(decor))
-                {
-                    return true;
-                }
-                else if (as<IRAutoPyBindExportInfoDecoration>(decor))
-                {
-                    return true;
-                }
-            }
-            if (isImported)
-                continue;
-            for (auto decor : child->getDecorations())
-            {
-                if (isAutoDiffDecoration(decor))
-                    return true;
-            }
-            if (doesModuleUseAutodiff(child))
-                return true;
-        }
-        return false;
+        if (as<IRTranslateBase>(globalInst))
+            return true;
     }
+
+    return false;
 }
 
 void cloneUsedWitnessTableEntries(IRSpecContext* context)
@@ -2199,7 +2129,7 @@ LinkedIR linkIR(CodeGenContext* codeGenContext)
     {
         if (sharedContext->useAutodiff)
             break;
-        sharedContext->useAutodiff = doesModuleUseAutodiff(irModule->getModuleInst());
+        sharedContext->useAutodiff = doesModuleUseAutodiff(irModule);
     }
 
     auto context = state->getContext();
@@ -2232,6 +2162,8 @@ LinkedIR linkIR(CodeGenContext* codeGenContext)
     // arguments which might end up affecting the mangled
     // entry point name.
     //
+
+    initializeTranslationDictionary(context->getModule());
 
     List<IRFunc*> irEntryPoints;
     for (auto entryPointIndex : codeGenContext->getEntryPointIndices())
@@ -2303,16 +2235,42 @@ LinkedIR linkIR(CodeGenContext* codeGenContext)
     bool shouldCopyGlobalParams =
         linkage->m_optionSet.getBoolOption(CompilerOptionName::PreserveParameters);
 
+    auto shouldCopy = [&](IRInst* inst) -> bool
+    {
+        // We need to copy over exported symbols,
+        // and any global parameters if preserve-params option is set.
+        //
+        if (_isHLSLExported(inst))
+        {
+            return true;
+        }
+
+        if (shouldCopyGlobalParams && as<IRGlobalParam>(inst))
+        {
+            return true;
+        }
+
+        if (auto knownBuiltin = inst->findDecoration<IRKnownBuiltinDecoration>())
+        {
+            switch (knownBuiltin->getName())
+            {
+            case KnownBuiltinDeclName::NullDifferential:
+                return true;
+            default:
+                break;
+            }
+        }
+
+        return false;
+    };
+
     for (IRModule* irModule : irModules)
     {
         for (auto inst : irModule->getGlobalInsts())
         {
             // We need to copy over exported symbols,
             // and any global parameters if preserve-params option is set.
-            if (_isHLSLExported(inst) || shouldCopyGlobalParams && as<IRGlobalParam>(inst) ||
-                sharedContext->useAutodiff &&
-                    (as<IRDifferentiableTypeAnnotation>(inst) ||
-                     inst->findDecorationImpl(kIROp_AutoDiffBuiltinDecoration) != nullptr))
+            if (shouldCopy(inst))
             {
                 auto cloned = cloneValue(context, inst);
                 if (!cloned->findDecorationImpl(kIROp_KeepAliveDecoration))
@@ -2538,6 +2496,7 @@ void prelinkIR(Module* module, IRModule* irModule, const List<IRInst*>& external
     IRSharedSpecContext sharedContext;
     sharedContext.builderStorage = IRBuilder(irModule->getModuleInst());
     sharedContext.module = irModule;
+    sharedContext.useAutodiff = doesModuleUseAutodiff(irModule);
 
     IRPrelinkContext specContext;
     specContext.builder = &sharedContext.builderStorage;

commit 1d6c26ad1a4f4e30dacd65c12fed8f22e36a01d9

Restore auto-diff link gating to the final codegen link (perf regression from #9808)
Motivation
----------
PR #9808 (auto-diff overhaul) caused a broad compile-time perf regression
between releases 2026.5 and 2026.7, visible in the compile-perf suite
(tools/compile-perf). The "per-compile floor" workloads regressed sharply:
e.g. the `minimal` (empty shader) `linkIR` rose ~10-13x and `linkAndOptimizeIR`
rose similarly, and every compile pays this regardless of whether it uses
auto-diff.

Root cause
----------
#9808 removed the `useAutodiff` gating that the IR linker previously used to
avoid pulling auto-diff artifacts into programs that do not differentiate:

  * `shouldDeepCloneWitnessTable` used to return `useAutodiff` for the
    `IDifferentiable` family; #9808 made it return `true` unconditionally and
    also added the new `IForwardDifferentiable` / `IBackwardDifferentiable` /
    `IBwdCallable` interfaces to the always-deep-clone set. As a result every
    program deep-clones the differentiable-interface witness tables and all
    their entries (`Differential`, `dzero`, `dadd`, fwd/bwd methods).

  * The new `cloneAnnotations` step clones module-scope `IRAnnotation`s for
    every cloned inst. Every `AnnotationKind` is differentiability-related
    (DifferentialType/Zero/Add/PairType, Forward/BackwardDerivative, ...), so
    this links a differentiable builtin's derivative associations into programs
    that never use them.

This dead auto-diff IR is then carried through specialize / simplifyIR / DCE
before finally being eliminated, inflating link and optimization time.

Fix
---
Restore the `useAutodiff` gating, but apply it ONLY to the final per-target
code-generation link (`linkIR`). A new `IRSharedSpecContext::isFinalCodegenLink`
flag is set true only there. The flag is required because the same clone paths
run during `prelinkIR` and module precompilation, whose output module must stay

source/slang/slang-ir-link.cpp
@@ -60,6 +60,15 @@ struct IRSharedSpecContext
 
     bool useAutodiff = false;
 
+    // True only for the final code-generation link (`linkIR`), where the linked
+    // module is a throw-away copy built for one target and unused symbols are
+    // dropped on demand / by later DCE. It is false for `prelinkIR` and module
+    // precompilation, which mutate a module that must remain complete and
+    // self-consistent (it may be serialized, e.g. the core module). Auto-diff
+    // link-time pruning (see `cloneAnnotations` / `shouldDeepCloneWitnessTable`)
+    // is only safe in the former case.
+    bool isFinalCodegenLink = false;
+
     IRBuilder builderStorage;
 
     // The "global" specialization environment.
@@ -205,6 +214,21 @@ IRInst* cloneInst(
 static void cloneAnnotations(IRSpecContextBase* context, IRInst* clonedInst, IRInst* originalInst)
 {
     SLANG_UNUSED(clonedInst);
+
+    // `IRAnnotation`s exclusively carry auto-diff trait associations: a target's
+    // derivative functions and differential type/zero/add/pair witnesses (every
+    // `AnnotationKind` is differentiability-related). A program that does not use
+    // auto-diff never reads them, so cloning them here only bloats the linked
+    // module — pulling every differentiable builtin's derivative methods (e.g.
+    // `sin`/`cos`/`sqrt`) and `float`'s differential machinery through every
+    // downstream pass (specialize / simplifyIR / DCE) before they are finally
+    // eliminated as dead. Skip them for the final code-gen link when auto-diff is
+    // not in use; this restores the pre-PR-#9808 behavior where derivative info
+    // was never linked into non-differentiating modules. We must not skip them
+    // during prelink / precompilation, whose output module stays live (and may be
+    // serialized, e.g. the core module) and must keep its annotations.
+    if (context->getShared()->isFinalCodegenLink && !context->getShared()->useAutodiff)
+        return;
 
     // Local annotations will be cloned normally as part of cloning their parent function/generic
     // body. For module-scope annotations, we need to look them up since they won't get
@@ -686,7 +710,39 @@ IRGlobalGenericParam* cloneGlobalGenericParamImpl(
 
 bool shouldDeepCloneWitnessTable(IRSpecContextBase* context, IRWitnessTable* table)
 {
-    SLANG_UNUSED(context);
+    auto conformanceType = getResolvedInstForDecorations(table->getConformanceType());
+
+    // Differentiable-interface witness tables are decided first, *before* the
+    // generic `HLSLExport` / `KeepAlive` rule below. PR #9808 marks these tables
+    // (`IDifferentiable`, `IForwardDifferentiable`, ...) and their entries
+    // `[HLSLExport]` so they can be linked across modules for auto-diff, but that
+    // export would otherwise force a deep clone into *every* program. A shader
+    // that references a differentiable builtin (e.g. `sin`) would then drag the
+    // table's `fwd_diff` / `bwd_diff` / `dadd` / `dzero` entries — and the whole
+    // derivative-function closure they reference — through every downstream pass,
+    // even with no auto-diff in use (the PR #9808 codegen regression). For the
+    // final code-gen link we instead defer the entries and clone only those that
+    // are actually referenced, unless the program uses auto-diff. Prelink /
+    // precompilation must keep the output module complete, so it still deep-clones.
+    for (auto decor : conformanceType->getDecorations())
+    {
+        if (auto knownBuiltin = as<IRKnownBuiltinDecoration>(decor))
+        {
+            switch (knownBuiltin->getName())
+            {
+            case KnownBuiltinDeclName::IDifferentiable:
+            case KnownBuiltinDeclName::IDifferentiablePtr:
+            case KnownBuiltinDeclName::IForwardDifferentiable:
+            case KnownBuiltinDeclName::IBackwardDifferentiable:
+            case KnownBuiltinDeclName::IBwdCallable:
+                return !context->getShared()->isFinalCodegenLink ||
+                       context->getShared()->useAutodiff;
+            default:
+                break;
+            }
+        }
+    }
+
     for (auto decor : table->getDecorations())
     {
         switch (decor->getOp())
@@ -697,27 +753,12 @@ bool shouldDeepCloneWitnessTable(IRSpecContextBase* context, IRWitnessTable* tab
         }
     }
 
-    auto conformanceType = getResolvedInstForDecorations(table->getConformanceType());
     for (auto decor : conformanceType->getDecorations())
     {
         switch (decor->getOp())
         {
         case kIROp_ComInterfaceDecoration:
             return true;
-        case kIROp_KnownBuiltinDecoration:
-            {
-                auto name = as<IRKnownBuiltinDecoration>(decor)->getName();
-                switch (name)
-                {
-                case KnownBuiltinDeclName::IDifferentiable:
-                case KnownBuiltinDeclName::IDifferentiablePtr:
-                case KnownBuiltinDeclName::IForwardDifferentiable:
-                case KnownBuiltinDeclName::IBackwardDifferentiable:
-                case KnownBuiltinDeclName::IBwdCallable:
-                    return true;
-                }
-                break;
-            }
         default:
             break;
         }
@@ -2139,6 +2180,11 @@ LinkedIR linkIR(CodeGenContext* codeGenContext)
     // high-fanout use-list walks.
     for (auto irModule : irModules)
         irModule->_ensureLinkingInfo();
+
+    // This is the final per-target code-generation link, so it is safe to prune
+    // auto-diff artifacts that the program never uses (see `cloneAnnotations` /
+    // `shouldDeepCloneWitnessTable`).
+    sharedContext->isFinalCodegenLink = true;
 
     // Check if any user module uses auto-diff, if so we will need to link
     // additional witnesses and decorations.