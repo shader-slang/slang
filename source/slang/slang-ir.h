59437e0a800038e89db23de5a4c8880cb9233730
kaizhangNV <kazhang@nvidia.com>
2026-08-24 15:48:14 -0700

    (Checkpoint Commit) Preserve structural group stages

--- source/slang/slang-ir.h
+++ source/slang/slang-ir.h
@@ -2275,7 +2275,7 @@ public:
     // anything to do with serialization format
     //
     const static UInt k_minSupportedModuleVersion = 4;
-    const static UInt k_maxSupportedModuleVersion = 38;
+    const static UInt k_maxSupportedModuleVersion = 39;
     static_assert(k_minSupportedModuleVersion <= k_maxSupportedModuleVersion);
 
 private:
