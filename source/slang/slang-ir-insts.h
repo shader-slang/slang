21c76eb5cf5181d3fae5fa88fa4f007febeaac24
George Ouzounoudis <geothrock@gmail.com>
2026-09-14 23:06:56 +0300

    Decorate domain shader patch constants with Patch decoration
    This is needed to be emitted in SPIR-V on tessellation evaluation / domain shaders,
    for user-defined patch constant variables. Builtin patch variables, like SV_TessFactor, already
    get Patch decoration from the SPIRV emitter.
    Fixes part of #12726

--- source/slang/slang-ir-insts.h
+++ source/slang/slang-ir-insts.h
@@ -5024,11 +5024,13 @@ $(type_info.return_type) $(type_info.method_name)(
         addDecoration(value, kIROp_PatchConstantFuncDecoration, patchConstantFunc);
     }
 
+    /// A decoration that marks a global variable as belonging to
+    /// the patch constant interface of a tessellation shader:
+    /// - Hull (tessellation control) shader: patch constant function outputs
+    /// - Domain (tessellation evaluation) shader: patch constant inputs
+    /// This is needed for SPIR-V/GLSL where patch constant variables require the `Patch` decoration.
     void addGLSLPatchDecoration(IRInst* value)
     {
-        // A decoration that marks a global output variable as belonging to
-        // the patch constant function output of a hull (tessellation control) shader.
-        // This is needed for SPIR-V/GLSL where patch constant function outputs require the `Patch` decoration.
         addDecoration(value, kIROp_GLSLPatchDecoration);
     }
 
